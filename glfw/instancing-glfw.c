fc2a2b0ec9da55c46981701fd637889e138e326f
Andre Weissflog <floooh@gmail.com>
2017-07-29 12:09:53 +0200

    and another vs fix

--- glfw/instancing-glfw.c
+++ glfw/instancing-glfw.c
@@ -15,7 +15,7 @@
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
-#define MAX_PARTICLES = 524288; /* 512*1024, this is necessary for VS */
+#define MAX_PARTICLES 524288 /* 512*1024, this is necessary for VS */
 const int NUM_PARTICLES_EMITTED_PER_FRAME = 10;
 
 /* vertex shader uniform block */
