3eb89722130aaf93e5301c9cc59f2279478533f4
Andre Weissflog <floooh@gmail.com>
2017-08-12 15:45:27 +0200

    comment typos

--- osx-metal/clear-metal.c
+++ osx-metal/clear-metal.c
@@ -24,7 +24,7 @@ void init(const void* mtl_device) {
     };
     sg_setup(&desc);
 
-    /* setup pass action to clear to read */
+    /* setup pass action to clear to red */
     pass_action = (sg_pass_action) {
         .colors[0] = { .action = SG_ACTION_CLEAR, .val = { 1.0f, 0.0f, 0.0f, 1.0f } }
     };
