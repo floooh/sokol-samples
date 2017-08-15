9eb79c46cf800440da3f759dfb653256454b66f1
Andre Weissflog <floooh@gmail.com>
2017-08-15 19:43:40 +0200

    Metal: cube sample

--- osx-metal/cube-metal.c
+++ osx-metal/cube-metal.c
@@ -3,9 +3,23 @@
 //------------------------------------------------------------------------------
 #include "osxentry.h"
 #include "sokol_gfx.h"
+#define HANDMADE_MATH_IMPLEMENTATION
+#define HANDMADE_MATH_NO_SSE
+#include "HandmadeMath.h"
 
-const int msaa_sample_count = 4;
+const int WIDTH = 640;
+const int HEIGHT = 480;
+const int MSAA_SAMPLES = 4;
+
 sg_pass_action pass_action = {0};
+sg_draw_state draw_state = {0};
+float rx = 0.0f;
+float ry = 0.0f;
+hmm_mat4 view_proj;
+
+typedef struct {
+    hmm_mat4 mvp;
+} vs_params_t;
 
 void init(const void* mtl_device) {
     /* setup sokol */
@@ -15,10 +29,134 @@ void init(const void* mtl_device) {
         .mtl_drawable_cb = osx_mtk_get_drawable
     };
     sg_setup(&desc);
+
+    /* cube vertex buffer */
+    float vertices[] = {
+        -1.0, -1.0, -1.0,   1.0, 0.0, 0.0, 1.0, 
+         1.0, -1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
+         1.0,  1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
+        -1.0,  1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
+
+        -1.0, -1.0,  1.0,   0.0, 1.0, 0.0, 1.0,
+         1.0, -1.0,  1.0,   0.0, 1.0, 0.0, 1.0, 
+         1.0,  1.0,  1.0,   0.0, 1.0, 0.0, 1.0,
+        -1.0,  1.0,  1.0,   0.0, 1.0, 0.0, 1.0,
+
+        -1.0, -1.0, -1.0,   0.0, 0.0, 1.0, 1.0, 
+        -1.0,  1.0, -1.0,   0.0, 0.0, 1.0, 1.0, 
+        -1.0,  1.0,  1.0,   0.0, 0.0, 1.0, 1.0, 
+        -1.0, -1.0,  1.0,   0.0, 0.0, 1.0, 1.0,
+
+        1.0, -1.0, -1.0,   1.0, 0.5, 0.0, 1.0, 
+        1.0,  1.0, -1.0,   1.0, 0.5, 0.0, 1.0, 
+        1.0,  1.0,  1.0,   1.0, 0.5, 0.0, 1.0, 
+        1.0, -1.0,  1.0,   1.0, 0.5, 0.0, 1.0,
+
+        -1.0, -1.0, -1.0,   0.0, 0.5, 1.0, 1.0, 
+        -1.0, -1.0,  1.0,   0.0, 0.5, 1.0, 1.0, 
+         1.0, -1.0,  1.0,   0.0, 0.5, 1.0, 1.0, 
+         1.0, -1.0, -1.0,   0.0, 0.5, 1.0, 1.0,
+
+        -1.0,  1.0, -1.0,   1.0, 0.0, 0.5, 1.0, 
+        -1.0,  1.0,  1.0,   1.0, 0.0, 0.5, 1.0, 
+         1.0,  1.0,  1.0,   1.0, 0.0, 0.5, 1.0, 
+         1.0,  1.0, -1.0,   1.0, 0.0, 0.5, 1.0
+    };
+    draw_state.vertex_buffers[0] = sg_make_buffer(&(sg_buffer_desc){
+        .size = sizeof(vertices),
+        .data_ptr = vertices
+    });
+
+    /* create an index buffer for the cube */
+    uint16_t indices[] = {
+        0, 1, 2,  0, 2, 3,
+        6, 5, 4,  7, 6, 4,
+        8, 9, 10,  8, 10, 11,
+        14, 13, 12,  15, 14, 12,
+        16, 17, 18,  16, 18, 19,
+        22, 21, 20,  23, 22, 20
+    };
+    draw_state.index_buffer = sg_make_buffer(&(sg_buffer_desc){
+        .type = SG_BUFFERTYPE_INDEXBUFFER,
+        .size = sizeof(indices),
+        .data_ptr = indices,
+    });
+
+    /*
+        a shader, note that Metal only needs to know uniform block sizes, but
+        not their internal layout
+    */
+    sg_shader shd = sg_make_shader(&(sg_shader_desc){
+        .vs.uniform_blocks[0].size = sizeof(vs_params_t),
+        .vs.entry = "vs_main",
+        .fs.entry = "fs_main",
+        .source =
+            "#include <metal_stdlib>\n"
+            "#include <simd/simd.h>\n"
+            "using namespace metal;\n"
+            "struct params_t {\n"
+            "  float4x4 mvp;\n"
+            "};\n"
+            "struct vs_in {\n"
+            "  float4 position [[attribute(0)]];\n"
+            "  float4 color [[attribute(1)]];\n"
+            "};\n"
+            "struct vs_out {\n"
+            "  float4 pos [[position]];\n"
+            "  float4 color;\n"
+            "};\n"
+            "vertex vs_out vs_main(vs_in in [[stage_in]], constant params_t& params [[buffer(0)]]) {\n"
+            "  vs_out out;\n"
+            "  out.pos = params.mvp * in.position;\n"
+            "  out.color = in.color;\n"
+            "  return out;\n"
+            "}\n"
+            "fragment float4 fs_main(vs_out in [[stage_in]]) {\n"
+            "  return in.color;\n"
+            "};\n"
+    });
+
+    /* 
+        a pipeline object, note that we need to provide the
+        MSAA sample count of the default framebuffer
+    */
+    draw_state.pipeline = sg_make_pipeline(&(sg_pipeline_desc){
+        .vertex_layouts[0] = {
+            .stride = 28,
+            .attrs = {
+                [0] = { .name="position", .offset=0, .format=SG_VERTEXFORMAT_FLOAT3 },
+                [1] = { .name="color0", .offset=12, .format=SG_VERTEXFORMAT_FLOAT4 }
+            }
+        },
+        .shader = shd,
+        .index_type = SG_INDEXTYPE_UINT16,
+        .depth_stencil = {
+            .depth_compare_func = SG_COMPAREFUNC_LESS_EQUAL,
+            .depth_write_enabled = true
+        },
+        .rasterizer.cull_mode = SG_CULLMODE_BACK,
+        .rasterizer.sample_count = MSAA_SAMPLES
+    });
+
+    /* view-projection matrix */
+    hmm_mat4 proj = HMM_Perspective(60.0f, (float)WIDTH/(float)HEIGHT, 0.01f, 10.0f);
+    hmm_mat4 view = HMM_LookAt(HMM_Vec3(0.0f, 1.5f, 6.0f), HMM_Vec3(0.0f, 0.0f, 0.0f), HMM_Vec3(0.0f, 1.0f, 0.0f));
+    view_proj = HMM_MultiplyMat4(proj, view);
 }
 
 void frame() {
+    /* compute model-view-projection matrix for vertex shader */
+    vs_params_t vs_params;
+    rx += 1.0f; ry += 2.0f;
+    hmm_mat4 rxm = HMM_Rotate(rx, HMM_Vec3(1.0f, 0.0f, 0.0f));
+    hmm_mat4 rym = HMM_Rotate(ry, HMM_Vec3(0.0f, 1.0f, 0.0f));
+    hmm_mat4 model = HMM_MultiplyMat4(rxm, rym);
+    vs_params.mvp = HMM_MultiplyMat4(view_proj, model);
+
     sg_begin_default_pass(&pass_action, osx_width(), osx_height());
+    sg_apply_draw_state(&draw_state);
+    sg_apply_uniform_block(SG_SHADERSTAGE_VS, 0, &vs_params, sizeof(vs_params));
+    sg_draw(0, 36, 1);
     sg_end_pass();
     sg_commit();
 }
@@ -28,6 +166,6 @@ void shutdown() {
 }
 
 int main() {
-    osx_start(640, 480, msaa_sample_count, "Sokol Cube (Metal)", init, frame, shutdown);
+    osx_start(WIDTH, HEIGHT, MSAA_SAMPLES, "Sokol Cube (Metal)", init, frame, shutdown);
     return 0;
 }
