bb2aaae2b2ff9d3aa970f775e9782c56c6821411
Andre Weissflog <floooh@gmail.com>
2017-09-29 19:33:40 +0200

    D3D11: started with triangle sample

---
+++ d3d11/triangle-d3d11.c
@@ -0,0 +1,78 @@
+//------------------------------------------------------------------------------
+//  triangle-d3d11.c
+//------------------------------------------------------------------------------
+#include "d3d11entry.h"
+#define SOKOL_IMPL
+#define SOKOL_D3D11
+#include "sokol_gfx.h"
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    /* setup d3d11 app wrapper */
+    d3d11_init(640, 480, 1, L"Sokol Triangle D3D11");
+
+    /* setup sokol gfx */
+    sg_setup(&(sg_desc){
+        .d3d11_device = d3d11_device(),
+        .d3d11_device_context = d3d11_device_context(),
+        .d3d11_render_target_view_cb = d3d11_render_target_view,
+        .d3d11_depth_stencil_view_cb = d3d11_depth_stencil_view
+    });
+
+    /* default pass action (clear to grey) */
+    sg_pass_action pass_action = { 0 };
+
+    /* a vertex buffer with the triangle vertices */
+    const float vertices[] = {
+        /* positions            colors */
+         0.0f, 0.5f, 0.5f,      1.0f, 0.0f, 0.0f, 1.0f,
+         0.5f, -0.5f, 0.5f,     0.0f, 1.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, 0.5f,     0.0f, 0.0f, 1.0f, 1.0f
+    };
+    sg_buffer vbuf = sg_make_buffer(&(sg_buffer_desc){
+        .size = sizeof(vertices),
+            .content = vertices
+    });
+
+    /* a shader to render the triangle */
+    /*
+    sg_shader shd = sg_make_shader(&(sg_shader_desc){
+        0
+    });
+    */
+
+    /* a pipeline object */
+    /*
+    sg_pipeline pip = sg_make_pipeline(&(sg_pipeline_desc){
+        .vertex_layouts[0] = {
+            .stride = 28,
+            .attrs = {
+                [0] = { .offset = 0, .format = SG_VERTEXFORMAT_FLOAT3 },
+                [1] = { .offset = 12, .format = SG_VERTEXFORMAT_FLOAT4 }
+            }
+        },
+        .shader = shd
+    });
+    */
+
+    /* a draw state struct with all the resource bindings */
+    /*
+    sg_draw_state draw_state = {
+        .pipeline = pip,
+        .vertex_buffers[0] = vbuf
+    };
+    */
+
+    /* the draw loop */
+    while (d3d11_process_events()) {
+        sg_begin_default_pass(&pass_action, d3d11_width(), d3d11_height());
+        //sg_apply_draw_state(&draw_state);
+        //sg_draw(0, 3, 1);
+        sg_end_pass();
+        sg_commit();
+        d3d11_present();
+    }
+    /* shutdown everything */
+    sg_shutdown();
+    d3d11_shutdown();
+    return 0;
+}
