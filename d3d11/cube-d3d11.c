57a0d5dbe6fb2330ef2b3a51870eb4dbe49898d8
Andre Weissflog <floooh@gmail.com>
2017-10-03 15:03:28 +0200

    cube-d3d11 sample

---
+++ d3d11/cube-d3d11.c
@@ -0,0 +1,171 @@
+//------------------------------------------------------------------------------
+//  cube-d3d11.c
+//  Demonstrate uniform block updates.
+//------------------------------------------------------------------------------
+#include "d3d11entry.h"
+#define SOKOL_IMPL
+#define SOKOL_D3D11
+#define SOKOL_D3D11_SHADER_COMPILER
+#define SOKOL_LOG(s) OutputDebugStringA(s)
+#include "sokol_gfx.h"
+#define HANDMADE_MATH_IMPLEMENTATION
+#define HANDMADE_MATH_NO_SSE
+#include "HandmadeMath.h"
+
+/* a uniform block with a model-view-projection matrix */
+typedef struct {
+    hmm_mat4 mvp;
+} vs_params_t;
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    /* setup d3d11 app wrapper and sokol_gfx */
+    const int msaa_samples = 4;
+    const int width = 800;
+    const int height = 600;
+    d3d11_init(width, height, msaa_samples, L"Sokol Cube D3D11");
+    sg_setup(&(sg_desc){
+        .d3d11_device = d3d11_device(),
+        .d3d11_device_context = d3d11_device_context(),
+        .d3d11_render_target_view_cb = d3d11_render_target_view,
+        .d3d11_depth_stencil_view_cb = d3d11_depth_stencil_view
+    });
+    
+    /* cube vertex buffer */
+    float vertices[] = {
+        /* positions        colors */
+        -1.0, -1.0, -1.0,   1.0, 0.0, 0.0, 1.0, 
+         1.0, -1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
+         1.0,  1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
+        -1.0,  1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
+
+        -1.0, -1.0,  1.0,   0.0, 1.0, 0.0, 1.0,
+         1.0, -1.0,  1.0,   0.0, 1.0, 0.0, 1.0, 
+         1.0,  1.0,  1.0,   0.0, 1.0, 0.0, 1.0,
+        -1.0,  1.0,  1.0,   0.0, 1.0, 0.0, 1.0,
+
+        -1.0, -1.0, -1.0,   0.0, 0.0, 1.0, 1.0, 
+        -1.0,  1.0, -1.0,   0.0, 0.0, 1.0, 1.0, 
+        -1.0,  1.0,  1.0,   0.0, 0.0, 1.0, 1.0, 
+        -1.0, -1.0,  1.0,   0.0, 0.0, 1.0, 1.0,
+
+        1.0, -1.0, -1.0,   1.0, 0.5, 0.0, 1.0, 
+        1.0,  1.0, -1.0,   1.0, 0.5, 0.0, 1.0, 
+        1.0,  1.0,  1.0,   1.0, 0.5, 0.0, 1.0, 
+        1.0, -1.0,  1.0,   1.0, 0.5, 0.0, 1.0,
+
+        -1.0, -1.0, -1.0,   0.0, 0.5, 1.0, 1.0, 
+        -1.0, -1.0,  1.0,   0.0, 0.5, 1.0, 1.0, 
+         1.0, -1.0,  1.0,   0.0, 0.5, 1.0, 1.0, 
+         1.0, -1.0, -1.0,   0.0, 0.5, 1.0, 1.0,
+
+        -1.0,  1.0, -1.0,   1.0, 0.0, 0.5, 1.0, 
+        -1.0,  1.0,  1.0,   1.0, 0.0, 0.5, 1.0, 
+         1.0,  1.0,  1.0,   1.0, 0.0, 0.5, 1.0, 
+         1.0,  1.0, -1.0,   1.0, 0.0, 0.5, 1.0
+    };
+    sg_buffer vbuf = sg_make_buffer(&(sg_buffer_desc){
+        .size = sizeof(vertices),
+        .content = vertices
+    });
+
+    /* cube indices */
+    uint16_t indices[] = {
+        0, 1, 2,  0, 2, 3,
+        6, 5, 4,  7, 6, 4,
+        8, 9, 10,  8, 10, 11,
+        14, 13, 12,  15, 14, 12,
+        16, 17, 18,  16, 18, 19,
+        22, 21, 20, 23, 22, 20
+    };
+    sg_buffer ibuf = sg_make_buffer(&(sg_buffer_desc){
+        .type = SG_BUFFERTYPE_INDEXBUFFER,
+        .size = sizeof(indices),
+        .content = indices
+    });
+
+    /* create shader */
+    sg_shader shd = sg_make_shader(&(sg_shader_desc){
+        .vs.uniform_blocks[0].size = sizeof(vs_params_t),
+        .vs.source =
+            "cbuffer params: register(b0) {\n"
+            "  float4x4 mvp;\n"
+            "};\n"
+            "struct vs_in {\n"
+            "  float4 pos: POSITION;\n"
+            "  float4 color: COLOR;\n"
+            "};\n"
+            "struct vs_out {\n"
+            "  float4 color: COLOR0;\n"
+            "  float4 pos: SV_Position;\n"
+            "};\n"
+            "vs_out main(vs_in inp) {\n"
+            "  vs_out outp;\n"
+            "  outp.pos = mul(mvp, inp.pos);\n"
+            "  outp.color = inp.color;\n"
+            "  return outp;\n"
+            "};\n",
+        .fs.source =
+            "float4 main(float4 color: COLOR0): SV_Target0 {\n"
+            "  return color;\n"
+            "}\n"
+    });
+
+    /* a pipeline object */
+    sg_pipeline pip = sg_make_pipeline(&(sg_pipeline_desc){
+        .vertex_layouts[0] = {
+            .stride = 28,
+            .attrs = {
+                [0] = { .name = "POSITION", .offset = 0, .format = SG_VERTEXFORMAT_FLOAT3 }, 
+                [1] = { .name = "COLOR", .offset = 12, .format = SG_VERTEXFORMAT_FLOAT4 }
+            }
+        },
+        .shader = shd,
+        .index_type = SG_INDEXTYPE_UINT16,
+        .depth_stencil = {
+            .depth_compare_func = SG_COMPAREFUNC_LESS_EQUAL,
+            .depth_write_enabled = true
+        },
+        .rasterizer = {
+            .cull_mode = SG_CULLMODE_BACK,
+            .sample_count = msaa_samples
+        }
+    });
+
+    /* draw state with resource bindings */
+    sg_draw_state draw_state = {
+        .pipeline = pip,
+        .vertex_buffers[0] = vbuf,
+        .index_buffer = ibuf
+    };
+
+    /* default pass action (clear to gray) */
+    sg_pass_action pass_action = { 0 };
+
+    /* view-projection matrix */
+    hmm_mat4 proj = HMM_Perspective(60.0f, (float)width/(float)height, 0.01, 10.0f);
+    hmm_mat4 view = HMM_LookAt(HMM_Vec3(0.0f, 1.5f, 6.0f), HMM_Vec3(0.0f, 0.0f, 0.0f), HMM_Vec3(0.0f, 1.0f, 0.0f));
+    hmm_mat4 view_proj = HMM_MultiplyMat4(proj, view);
+
+    float rx = 0.0f, ry = 0.0f;
+    while (d3d11_process_events()) {
+        /* model-view-proj matrix for vertex shader */
+        vs_params_t vs_params;
+        rx += 1.0f; ry += 2.0f;
+        hmm_mat4 rxm = HMM_Rotate(rx, HMM_Vec3(1.0f, 0.0f, 0.0f));
+        hmm_mat4 rym = HMM_Rotate(ry, HMM_Vec3(0.0f, 1.0f, 0.0f));
+        hmm_mat4 model = HMM_MultiplyMat4(rxm, rym);
+        vs_params.mvp = HMM_MultiplyMat4(view_proj, model);
+
+        sg_begin_default_pass(&pass_action, d3d11_width(), d3d11_height());
+        sg_apply_draw_state(&draw_state);
+        sg_apply_uniform_block(SG_SHADERSTAGE_VS, 0, &vs_params, sizeof(vs_params));
+        sg_draw(0, 36, 1);
+        sg_end_pass();
+        sg_commit();
+        d3d11_present();
+    }
+    sg_shutdown();
+    d3d11_shutdown();
+    return 0;
+}
+
