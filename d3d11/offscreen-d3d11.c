5ae2689b94ee1f5eb97a3d76264dbd3d092a2244
Andre Weissflog <floooh@gmail.com>
2017-10-11 20:28:22 +0200

    new sample offscreen-d3d11, no MSAA offscreen rendering yet

---
+++ d3d11/offscreen-d3d11.c
@@ -0,0 +1,271 @@
+//------------------------------------------------------------------------------
+//  offscreen-d3d11.c
+//------------------------------------------------------------------------------
+#include "d3d11entry.h"
+#define SOKOL_IMPL
+#define SOKOL_D3D11
+#define SOKOL_D3D11_SHADER_COMPILER
+#define SOKOL_LOG(s) OutputDebugStringA(s)
+#include "sokol_gfx.h"
+#define HANDMADE_MATH_IMPLEMENTATION
+#define HANDMADE_MATH_NO_SSE
+#include "HandmadeMath.h"
+
+typedef struct {
+    hmm_mat4 mvp;
+} vs_params_t;
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
+    /* setup d3d11 app wrapper and sokol_gfx */
+    const int width = 800;
+    const int height = 600;
+    const int sample_count = 4;
+    d3d11_init(width, height, sample_count, L"Sokol Instancing D3D11");
+    sg_setup(&(sg_desc){
+        .d3d11_device = d3d11_device(),
+        .d3d11_device_context = d3d11_device_context(),
+        .d3d11_render_target_view_cb = d3d11_render_target_view,
+        .d3d11_depth_stencil_view_cb = d3d11_depth_stencil_view
+    });
+
+    /* create one color and one depth render target image */
+    const int rt_sample_count = 1; //sg_query_feature(SG_FEATURE_MSAA_RENDER_TARGETS) ? sample_count : 1;
+    sg_image_desc img_desc = {
+        .render_target = true,
+        .width = 512,
+        .height = 512,
+        .min_filter = SG_FILTER_LINEAR,
+        .mag_filter = SG_FILTER_LINEAR,
+        .sample_count = rt_sample_count
+    };
+    sg_image color_img = sg_make_image(&img_desc);
+    img_desc.pixel_format = SG_PIXELFORMAT_DEPTH;
+    sg_image depth_img = sg_make_image(&img_desc);
+
+    /* an offscreen render pass into those images */
+    sg_pass offscreen_pass = sg_make_pass(&(sg_pass_desc){
+        .color_attachments[0].image = color_img,
+        .depth_stencil_attachment.image = depth_img
+    });
+
+    /* pass action for offscreen pass, clearing to black */
+    sg_pass_action offscreen_pass_action = {
+        .colors[0] = { .action = SG_ACTION_CLEAR, .val = { 0.0f, 0.0f, 0.0f, 1.0f } }
+    };
+
+    /* pass action for default pass, clearing to blue-ish */
+    sg_pass_action default_pass_action = {
+        .colors[0] = { .action = SG_ACTION_CLEAR, .val = { 0.0f, 0.25f, 1.0f, 1.0f } }
+    };
+
+    /* cube vertex buffer with positions, colors and tex coords */
+    float vertices[] = {
+        /* pos                  color                       uvs */
+        -1.0f, -1.0f, -1.0f,    1.0f, 0.5f, 0.5f, 1.0f,     0.0f, 0.0f,
+         1.0f, -1.0f, -1.0f,    1.0f, 0.5f, 0.5f, 1.0f,     1.0f, 0.0f,
+         1.0f,  1.0f, -1.0f,    1.0f, 0.5f, 0.5f, 1.0f,     1.0f, 1.0f,
+        -1.0f,  1.0f, -1.0f,    1.0f, 0.5f, 0.5f, 1.0f,     0.0f, 1.0f,
+
+        -1.0f, -1.0f,  1.0f,    0.5f, 1.0f, 0.5f, 1.0f,     0.0f, 0.0f, 
+         1.0f, -1.0f,  1.0f,    0.5f, 1.0f, 0.5f, 1.0f,     1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f,    0.5f, 1.0f, 0.5f, 1.0f,     1.0f, 1.0f,
+        -1.0f,  1.0f,  1.0f,    0.5f, 1.0f, 0.5f, 1.0f,     0.0f, 1.0f,
+
+        -1.0f, -1.0f, -1.0f,    0.5f, 0.5f, 1.0f, 1.0f,     0.0f, 0.0f,
+        -1.0f,  1.0f, -1.0f,    0.5f, 0.5f, 1.0f, 1.0f,     1.0f, 0.0f,
+        -1.0f,  1.0f,  1.0f,    0.5f, 0.5f, 1.0f, 1.0f,     1.0f, 1.0f,
+        -1.0f, -1.0f,  1.0f,    0.5f, 0.5f, 1.0f, 1.0f,     0.0f, 1.0f,
+
+         1.0f, -1.0f, -1.0f,    1.0f, 0.5f, 0.0f, 1.0f,     0.0f, 0.0f,
+         1.0f,  1.0f, -1.0f,    1.0f, 0.5f, 0.0f, 1.0f,     1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f,    1.0f, 0.5f, 0.0f, 1.0f,     1.0f, 1.0f,
+         1.0f, -1.0f,  1.0f,    1.0f, 0.5f, 0.0f, 1.0f,     0.0f, 1.0f,
+
+        -1.0f, -1.0f, -1.0f,    0.0f, 0.5f, 1.0f, 1.0f,     0.0f, 0.0f,
+        -1.0f, -1.0f,  1.0f,    0.0f, 0.5f, 1.0f, 1.0f,     1.0f, 0.0f,
+         1.0f, -1.0f,  1.0f,    0.0f, 0.5f, 1.0f, 1.0f,     1.0f, 1.0f,
+         1.0f, -1.0f, -1.0f,    0.0f, 0.5f, 1.0f, 1.0f,     0.0f, 1.0f,
+
+        -1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.5f, 1.0f,     0.0f, 0.0f,
+        -1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.5f, 1.0f,     1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.5f, 1.0f,     1.0f, 1.0f,
+         1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.5f, 1.0f,     0.0f, 1.0f
+    };
+    sg_buffer_desc vbuf_desc = {
+        .size = sizeof(vertices),
+        .content = vertices,
+    };
+    sg_buffer vbuf = sg_make_buffer(&vbuf_desc);
+
+    /* an index buffer for the cube */
+    uint16_t indices[] = {
+        0, 1, 2,  0, 2, 3,
+        6, 5, 4,  7, 6, 4,
+        8, 9, 10,  8, 10, 11,
+        14, 13, 12,  15, 14, 12,
+        16, 17, 18,  16, 18, 19,
+        22, 21, 20,  23, 22, 20
+    };
+    sg_buffer_desc ibuf_desc = {
+        .type = SG_BUFFERTYPE_INDEXBUFFER,
+        .size = sizeof(indices),
+        .content = indices,
+    };
+    sg_buffer ibuf = sg_make_buffer(&ibuf_desc);
+
+    /* shader for non-textured cube, rendered in offscreen pass */
+    sg_shader offscreen_shd = sg_make_shader(&(sg_shader_desc){
+        .vs.uniform_blocks[0].size = sizeof(vs_params_t),
+        .vs.source = 
+            "cbuffer params: register(b0) {\n"
+            "  float4x4 mvp;\n"
+            "};\n"
+            "struct vs_in {\n"
+            "  float4 pos: POSITION;\n"
+            "  float4 color: COLOR0;\n"
+            "};\n"
+            "struct vs_out {\n"
+            "  float4 color: COLOR0;\n"
+            "  float4 pos: SV_Position;\n"
+            "};\n"
+            "vs_out main(vs_in inp) {\n"
+            "  vs_out outp;\n"
+            "  outp.pos = mul(mvp, inp.pos);\n"
+            "  outp.color = inp.color;\n"
+            "  return outp;\n"
+            "}\n",
+        .fs.source =
+            "float4 main(float4 color: COLOR0): SV_Target0 {\n"
+            "  return color;\n"
+            "}\n"
+    });
+
+    /* ...and a second shader for rendering a textured cube in the default pass */
+    sg_shader default_shd = sg_make_shader(&(sg_shader_desc){
+        .vs.uniform_blocks[0].size = sizeof(vs_params_t),
+        .fs.images[0].type = SG_IMAGETYPE_2D,
+        .vs.source =
+            "cbuffer params: register(b0) {\n"
+            "  float4x4 mvp;\n"
+            "};\n"
+            "struct vs_in {\n"
+            "  float4 pos: POSITION;\n"
+            "  float4 color: COLOR0;\n"
+            "  float2 uv: TEXCOORD0;\n"
+            "};\n"
+            "struct vs_out {\n"
+            "  float4 color: COLOR0;\n"
+            "  float2 uv: TEXCOORD0;\n"
+            "  float4 pos: SV_Position;\n"
+            "};\n"
+            "vs_out main(vs_in inp) {\n"
+            "  vs_out outp;\n"
+            "  outp.pos = mul(mvp, inp.pos);\n"
+            "  outp.color = inp.color;\n"
+            "  outp.uv = inp.uv;\n"
+            "  return outp;\n"
+            "}\n",
+        .fs.source =
+            "Texture2D<float4> tex: register(t0);\n"
+            "sampler smp: register(s0);\n"
+            "float4 main(float4 color: COLOR0, float2 uv: TEXCOORD0): SV_Target0 {\n"
+            "  return tex.Sample(smp, uv) + color * 0.5;\n"
+            "}\n"
+    });
+
+    /* pipeline object for offscreen rendering */
+    sg_pipeline offscreen_pip = sg_make_pipeline(&(sg_pipeline_desc){
+        .vertex_layouts[0] = {
+            .stride = 36,
+            .attrs = {
+                [0] = { .sem_name="POSITION", .offset=0, .format=SG_VERTEXFORMAT_FLOAT3 },
+                [1] = { .sem_name="COLOR", .sem_index=0, .offset=12, .format=SG_VERTEXFORMAT_FLOAT4 }
+            },
+        },
+        .shader = offscreen_shd,
+        .index_type = SG_INDEXTYPE_UINT16,
+        .depth_stencil = {
+            .depth_compare_func = SG_COMPAREFUNC_LESS_EQUAL,
+            .depth_write_enabled = true
+        },
+        .rasterizer = {
+            .cull_mode = SG_CULLMODE_BACK,
+            .sample_count = rt_sample_count
+        }
+    });
+
+    /* pipeline object for rendering textured cube in default pass */
+    sg_pipeline default_pip = sg_make_pipeline(&(sg_pipeline_desc){
+        .vertex_layouts[0] = {
+            .stride = 36,
+            .attrs = {
+                [0] = { .sem_name="POSITION", .offset=0, .format=SG_VERTEXFORMAT_FLOAT3 },
+                [1] = { .sem_name="COLOR", .sem_index=0, .offset=12, .format=SG_VERTEXFORMAT_FLOAT4 },
+                [2] = { .sem_name="TEXCOORD", .sem_index=0, .offset=28, .format=SG_VERTEXFORMAT_FLOAT2 }
+            }
+        },
+        .shader = default_shd,
+        .index_type = SG_INDEXTYPE_UINT16,
+        .depth_stencil = {
+            .depth_compare_func = SG_COMPAREFUNC_LESS_EQUAL,
+            .depth_write_enabled = true
+        },
+        .rasterizer = {
+            .cull_mode = SG_CULLMODE_BACK,
+            .sample_count = sample_count
+        }
+    });
+
+    /* draw state for offscreen rendering */
+    sg_draw_state offscreen_draw_state = {
+        .pipeline = offscreen_pip,
+        .vertex_buffers[0] = vbuf,
+        .index_buffer = ibuf
+    };
+
+    /* draw state for default pass */
+    sg_draw_state default_draw_state = {
+        .pipeline = default_pip,
+        .vertex_buffers[0] = vbuf,
+        .index_buffer = ibuf,
+        .fs_images[0] = color_img
+    };
+
+    /* view-projection matrix */
+    hmm_mat4 proj = HMM_Perspective(60.0f, (float)width/(float)height, 0.01f, 10.0f);
+    hmm_mat4 view = HMM_LookAt(HMM_Vec3(0.0f, 1.5f, 6.0f), HMM_Vec3(0.0f, 0.0f, 0.0f), HMM_Vec3(0.0f, 1.0f, 0.0f));
+    hmm_mat4 view_proj = HMM_MultiplyMat4(proj, view);
+
+    /* everything ready, on to the draw loop! */
+    vs_params_t vs_params;
+    float rx = 0.0f, ry = 0.0f;
+    while (d3d11_process_events()) {
+        /* prepare the uniform block with the model-view-projection matrix,
+           we just use the same matrix for the offscreen- and default-pass */
+        rx += 1.0f; ry += 2.0f;
+        hmm_mat4 model = HMM_MultiplyMat4(
+            HMM_Rotate(rx, HMM_Vec3(1.0f, 0.0f, 0.0f)),
+            HMM_Rotate(ry, HMM_Vec3(0.0f, 1.0f, 0.0f)));
+        vs_params.mvp = HMM_MultiplyMat4(view_proj, model);
+
+        /* offscreen pass, rendering an untextured cube */
+        sg_begin_pass(offscreen_pass, &offscreen_pass_action);
+        sg_apply_draw_state(&offscreen_draw_state);
+        sg_apply_uniform_block(SG_SHADERSTAGE_VS, 0, &vs_params, sizeof(vs_params));
+        sg_draw(0, 36, 1);
+        sg_end_pass();
+
+        /* default pass, render cube textured with offscreen render target image */
+        sg_begin_default_pass(&default_pass_action, d3d11_width(), d3d11_height());
+        sg_apply_draw_state(&default_draw_state);
+        sg_apply_uniform_block(SG_SHADERSTAGE_VS, 0, &vs_params, sizeof(vs_params));
+        sg_draw(0, 36, 1);
+        sg_end_pass();
+
+        sg_commit();
+        d3d11_present();
+    }
+    sg_shutdown();
+    d3d11_shutdown();
+    return 0;
+}
