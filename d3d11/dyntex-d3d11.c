3f80ef3ecc630fe260c1ee57e131a1b5ceb592c5
Andre Weissflog <floooh@gmail.com>
2017-10-13 18:15:28 +0200

    new sample: dyntex-d3d11

---
+++ d3d11/dyntex-d3d11.c
@@ -0,0 +1,260 @@
+//------------------------------------------------------------------------------
+//  dyntex-d3d11.c
+//------------------------------------------------------------------------------
+#include "d3d11entry.h"
+#define SOKOL_IMPL
+#define SOKOL_D3D11
+#define SOKOL_D3D11_SHADER_COMPILER
+#define SOKOL_LOG(s) OutputDebugStringA(s)
+#include "sokol_gfx.h"
+#define HANDMADE_MATH_IMPLEMENTATION
+#define HANDMADE_MATH_NO_SSE
+#include "HandmadeMath.h"
+
+typedef struct {
+    hmm_mat4 mvp;
+} vs_params_t;
+
+/* width/height must be 2^N */
+#define IMAGE_WIDTH (64)
+#define IMAGE_HEIGHT (64)
+#define LIVING (0xFFFFFFFF)
+#define DEAD (0xFF000000)
+uint32_t pixels[IMAGE_WIDTH][IMAGE_HEIGHT];
+
+void game_of_life_init();
+void game_of_life_update();
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
+    /* setup d3d11 app wrapper and sokol_gfx */
+    const int width = 800;
+    const int height = 600;
+    const int sample_count = 4;
+    d3d11_init(width, height, sample_count, L"Sokol Array Texture D3D11");
+    sg_setup(&(sg_desc){
+        .d3d11_device = d3d11_device(),
+        .d3d11_device_context = d3d11_device_context(),
+        .d3d11_render_target_view_cb = d3d11_render_target_view,
+        .d3d11_depth_stencil_view_cb = d3d11_depth_stencil_view
+    });
+
+    /* a 128x128 image with streaming-update strategy */
+    sg_image img = sg_make_image(&(sg_image_desc){
+        .width = IMAGE_WIDTH,
+        .height = IMAGE_HEIGHT,
+        .pixel_format = SG_PIXELFORMAT_RGBA8,
+        .usage = SG_USAGE_STREAM,
+        .min_filter = SG_FILTER_LINEAR,
+        .mag_filter = SG_FILTER_LINEAR,
+        .wrap_u = SG_WRAP_CLAMP_TO_EDGE,
+        .wrap_v = SG_WRAP_CLAMP_TO_EDGE
+    });
+
+    /* a cube vertex- and index-buffer */
+    float vertices[] = {
+        /* pos                  color                       uvs */
+        -1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f,     0.0f, 0.0f,
+         1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f,     1.0f, 0.0f,
+         1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f,     1.0f, 1.0f,
+        -1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f,     0.0f, 1.0f,
+
+        -1.0f, -1.0f,  1.0f,    0.0f, 1.0f, 0.0f, 1.0f,     0.0f, 0.0f, 
+         1.0f, -1.0f,  1.0f,    0.0f, 1.0f, 0.0f, 1.0f,     1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f,    0.0f, 1.0f, 0.0f, 1.0f,     1.0f, 1.0f,
+        -1.0f,  1.0f,  1.0f,    0.0f, 1.0f, 0.0f, 1.0f,     0.0f, 1.0f,
+
+        -1.0f, -1.0f, -1.0f,    0.0f, 0.0f, 1.0f, 1.0f,     0.0f, 0.0f,
+        -1.0f,  1.0f, -1.0f,    0.0f, 0.0f, 1.0f, 1.0f,     1.0f, 0.0f,
+        -1.0f,  1.0f,  1.0f,    0.0f, 0.0f, 1.0f, 1.0f,     1.0f, 1.0f,
+        -1.0f, -1.0f,  1.0f,    0.0f, 0.0f, 1.0f, 1.0f,     0.0f, 1.0f,
+
+         1.0f, -1.0f, -1.0f,    1.0f, 0.5f, 0.0f, 1.0f,     0.0f, 0.0f,
+         1.0f,  1.0f, -1.0f,    1.0f, 0.5f, 0.0f, 1.0f,     1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f,    1.0f, 0.5f, 0.0f, 1.0f,     1.0f, 1.0f,
+         1.0f, -1.0f,  1.0f,    1.0f, 0.5f, 0.0f, 1.0f,     0.0f, 1.0f,
+
+        -1.0f, -1.0f, -1.0f,    0.0f, 0.5f, 1.0f, 1.0f,     0.0f, 0.0f,
+        -1.0f, -1.0f,  1.0f,    0.0f, 0.5f, 1.0f, 1.0f,     1.0f, 0.0f,
+         1.0f, -1.0f,  1.0f,    0.0f, 0.5f, 1.0f, 1.0f,     1.0f, 1.0f,
+         1.0f, -1.0f, -1.0f,    0.0f, 0.5f, 1.0f, 1.0f,     0.0f, 1.0f,
+
+        -1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.5f, 1.0f,     0.0f, 0.0f,
+        -1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.5f, 1.0f,     1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.5f, 1.0f,     1.0f, 1.0f,
+         1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.5f, 1.0f,     0.0f, 1.0f
+    };
+    uint16_t indices[] = {
+        0, 1, 2,  0, 2, 3,
+        6, 5, 4,  7, 6, 4,
+        8, 9, 10,  8, 10, 11,
+        14, 13, 12,  15, 14, 12,
+        16, 17, 18,  16, 18, 19,
+        22, 21, 20,  23, 22, 20
+    };
+    sg_buffer vbuf = sg_make_buffer(&(sg_buffer_desc){
+        .size = sizeof(vertices),
+        .content = vertices,
+    });
+    sg_buffer ibuf = sg_make_buffer(&(sg_buffer_desc){
+        .type = SG_BUFFERTYPE_INDEXBUFFER,
+        .size = sizeof(indices),
+        .content = indices,
+    });
+
+    /* a shader to render textured cube */
+    sg_shader shd = sg_make_shader(&(sg_shader_desc){
+        .vs.uniform_blocks[0].size = sizeof(vs_params_t),
+        .fs.images[0].type=SG_IMAGETYPE_2D,
+        .vs.source =
+            "cbuffer params {\n"
+            "  float4x4 mvp;\n"
+            "};\n"
+            "struct vs_in {\n"
+            "  float4 pos: POSITION;\n"
+            "  float4 color: COLOR0;\n"
+            "  float2 uv: TEXCOORD0;\n"
+            "};\n"
+            "struct vs_out {\n"
+            "  float4 color: COLOR0;\n"
+            "  float2 uv: TEXCOORD0;\n"
+            "  float4 pos: SV_Position;\n"
+            "};\n"
+            "vs_out main(vs_in inp) {\n"
+            "  vs_out outp;\n"
+            "  outp.pos = mul(mvp, inp.pos);\n"
+            "  outp.color = inp.color;\n"
+            "  outp.uv = inp.uv;\n"
+            "  return outp;\n"
+            "}\n",
+        .fs.source =
+            "Texture2D<float4> tex: register(t0);\n"
+            "sampler smp: register(s0);\n"
+            "float4 main(float4 color: COLOR0, float2 uv: TEXCOORD0): SV_Target0 {\n"
+            "  return tex.Sample(smp, uv) * color;\n"
+            "}\n"
+    });
+
+    /* a pipeline-state-object for the textured cube */
+    sg_pipeline pip = sg_make_pipeline(&(sg_pipeline_desc){
+        .vertex_layouts[0] = {
+            .stride = 36,
+            .attrs = {
+                [0] = { .sem_name="POSITION",   .offset=0,  .format=SG_VERTEXFORMAT_FLOAT3 },
+                [1] = { .sem_name="COLOR",      .offset=12, .format=SG_VERTEXFORMAT_FLOAT4 },
+                [2] = { .sem_name="TEXCOORD",   .offset=28, .format=SG_VERTEXFORMAT_FLOAT2 }
+            }
+        },
+        .shader = shd,
+        .index_type = SG_INDEXTYPE_UINT16,
+        .depth_stencil = {
+            .depth_compare_func = SG_COMPAREFUNC_LESS_EQUAL,
+            .depth_write_enabled = true
+        },
+        .rasterizer = {
+            .cull_mode = SG_CULLMODE_BACK,
+            .sample_count = sample_count
+        }
+    });
+
+    /* draw state with resource bindings */
+    sg_draw_state draw_state = {
+        .pipeline = pip,
+        .vertex_buffers[0] = vbuf,
+        .index_buffer = ibuf,
+        .fs_images[0] = img
+    };
+
+    /* default pass action (clear to grey) */
+    sg_pass_action pass_action = {0};
+
+    /* view-projection matrix */
+    hmm_mat4 proj = HMM_Perspective(60.0f, (float)width/(float)height, 0.01f, 10.0f);
+    hmm_mat4 view = HMM_LookAt(HMM_Vec3(0.0f, 1.5f, 4.0f), HMM_Vec3(0.0f, 0.0f, 0.0f), HMM_Vec3(0.0f, 1.0f, 0.0f));
+    hmm_mat4 view_proj = HMM_MultiplyMat4(proj, view);
+
+    /* initial game-of-life seed state */
+    game_of_life_init();
+
+    vs_params_t vs_params;
+    float rx = 0.0f, ry = 0.0f;
+    while (d3d11_process_events()) {
+        /* model-view-projection matrix from rotated model matrix */
+        rx += 0.1f; ry += 0.2f;
+        hmm_mat4 rxm = HMM_Rotate(rx, HMM_Vec3(1.0f, 0.0f, 0.0f));
+        hmm_mat4 rym = HMM_Rotate(ry, HMM_Vec3(0.0f, 1.0f, 0.0f));
+        hmm_mat4 model = HMM_MultiplyMat4(rxm, rym);
+        vs_params.mvp = HMM_MultiplyMat4(view_proj, model);
+
+        /* update game-of-life state */
+        game_of_life_update();
+
+        /* update the dynamic image */
+        sg_update_image(img, &(sg_image_content){ 
+            .subimage[0][0] = { 
+                .ptr=pixels, 
+                .size=sizeof(pixels) 
+            } 
+        });
+
+        sg_begin_default_pass(&pass_action, d3d11_width(), d3d11_height());
+        sg_apply_draw_state(&draw_state);
+        sg_apply_uniform_block(SG_SHADERSTAGE_VS, 0, &vs_params, sizeof(vs_params));
+        sg_draw(0, 36, 1);
+        sg_end_pass();
+        sg_commit();
+        d3d11_present();
+    }
+    sg_shutdown();
+    d3d11_shutdown();
+}
+
+void game_of_life_init() {
+    for (int y = 0; y < IMAGE_HEIGHT; y++) {
+        for (int x = 0; x < IMAGE_WIDTH; x++) {
+            if ((rand() & 255) > 230) {
+                pixels[y][x] = LIVING;
+            }
+            else {
+                pixels[y][x] = DEAD;
+            }
+        }
+    }
+}
+
+void game_of_life_update() {
+    static int update_count = 0;
+    for (int y = 0; y < IMAGE_HEIGHT; y++) {
+        for (int x = 0; x < IMAGE_WIDTH; x++) {
+            int num_living_neighbours = 0;
+            for (int ny = -1; ny < 2; ny++) {
+                for (int nx = -1; nx < 2; nx++) {
+                    if ((nx == 0) && (ny == 0)) {
+                        continue;
+                    }
+                    if (pixels[(y+ny)&(IMAGE_HEIGHT-1)][(x+nx)&(IMAGE_WIDTH-1)] == LIVING) {
+                        num_living_neighbours++;
+                    }
+                }
+            }
+            /* any live cell... */
+            if (pixels[y][x] == LIVING) {
+                if (num_living_neighbours < 2) {
+                    /* ... with fewer than 2 living neighbours dies, as if caused by underpopulation */
+                    pixels[y][x] = DEAD;
+                }
+                else if (num_living_neighbours > 3) {
+                    /* ... with more than 3 living neighbours dies, as if caused by overpopulation */
+                    pixels[y][x] = DEAD;
+                }
+            }
+            else if (num_living_neighbours == 3) {
+                /* any dead cell with exactly 3 living neighbours becomes a live cell, as if by reproduction */
+                pixels[y][x] = LIVING;
+            }
+        }
+    }
+    if (update_count++ > 240) {
+        game_of_life_init();
+        update_count = 0;
+    }
+}
