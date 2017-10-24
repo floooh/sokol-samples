96f6f749c55fd022818a34e1a240cf6cc36fe52a
Andre Weissflog <floooh@gmail.com>
2017-10-24 19:15:54 +0200

    D3D11: remove scissor_test_enabled state

--- d3d11/imgui-d3d11.cc
+++ d3d11/imgui-d3d11.cc
@@ -144,7 +144,6 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
     pip_desc.blend.src_factor_rgb = SG_BLENDFACTOR_SRC_ALPHA;
     pip_desc.blend.dst_factor_rgb = SG_BLENDFACTOR_ONE_MINUS_SRC_ALPHA;
     pip_desc.blend.color_write_mask = SG_COLORMASK_RGB;
-    pip_desc.rasterizer.scissor_test_enabled = true;
     draw_state.pipeline = sg_make_pipeline(&pip_desc);
 
     // initial clear color
