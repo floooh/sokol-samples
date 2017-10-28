b9f7bf83504dc34c139518dd458c993281280465
Andre Weissflog <floooh@gmail.com>
2017-10-28 17:55:50 +0200

    GL+Metal: new test/sample for mipmapping

---
+++ glfw/mipmap-glfw.c
@@ -0,0 +1,198 @@
+//------------------------------------------------------------------------------
+//  mipmap-glfw.c
+//------------------------------------------------------------------------------
+#define HANDMADE_MATH_IMPLEMENTATION
+#define HANDMADE_MATH_NO_SSE
+#include "HandmadeMath.h"
+#define GLFW_INCLUDE_NONE
+#include "GLFW/glfw3.h"
+#include "flextgl/flextGL.h"
+#define SOKOL_IMPL
+#define SOKOL_GLCORE33
+#include "sokol_gfx.h"
+
+typedef struct {
+    hmm_mat4 mvp;
+} vs_params_t;
+
+struct {
+    uint32_t mip0[65536];   /* 256x256 */
+    uint32_t mip1[16384];   /* 128x128 */
+    uint32_t mip2[4096];    /* 64*64 */
+    uint32_t mip3[1024];    /* 32*32 */
+    uint32_t mip4[256];     /* 16*16 */
+    uint32_t mip5[64];      /* 8*8 */
+    uint32_t mip6[16];      /* 4*4 */
+    uint32_t mip7[4];       /* 2*2 */
+    uint32_t mip8[1];       /* 1*2 */
+} pixels;
+
+uint32_t mip_colors[9] = {
+    0xFF0000FF,     /* red */
+    0xFF00FF00,     /* green */
+    0xFFFF0000,     /* blue */
+    0xFFFF00FF,     /* magenta */
+    0xFFFFFF00,     /* cyan */
+    0xFF00FFFF,     /* yellow */
+    0xFFFF00A0,     /* violet */
+    0xFFFFA0FF,     /* orange */
+    0xFFA000FF,     /* purple */
+};
+int main() {
+    const int WIDTH = 800;
+    const int HEIGHT = 600;
+    const int MSAA_SAMPLES = 4;
+
+    /* create GLFW window and initialize GL */
+    glfwInit();
+    glfwWindowHint(GLFW_SAMPLES, MSAA_SAMPLES);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* w = glfwCreateWindow(WIDTH, HEIGHT, "Sokol Mipmapping GLFW", 0, 0);
+    glfwMakeContextCurrent(w);
+    glfwSwapInterval(1);
+    flextInit(w);
+
+    /* setup sokol_gfx */
+    sg_desc desc = {0};
+    sg_setup(&desc);
+    assert(sg_isvalid());
+    
+    /* a plane vertex buffer */
+    float vertices[] = {
+        -1.0, -1.0, 0.0,  0.0, 0.0,
+        +1.0, -1.0, 0.0,  1.0, 0.0,
+        -1.0, +1.0, 0.0,  0.0, 1.0,
+        +1.0, +1.0, 0.0,  1.0, 1.0,
+    };
+    sg_buffer vbuf = sg_make_buffer(&(sg_buffer_desc){
+        .size = sizeof(vertices),
+        .content = vertices
+    });
+
+    /* mipmapped images, with different sampling filters */
+    sg_image_content img_content;
+    uint32_t* ptr = pixels.mip0;
+    bool even_odd = false;
+    for (int mip_index = 0; mip_index <= 8; mip_index++) {
+        const int dim = 1<<(8-mip_index);
+        img_content.subimage[0][mip_index].ptr = ptr;
+        img_content.subimage[0][mip_index].size = dim * dim * 4;
+        for (int y = 0; y < dim; y++) {
+            for (int x = 0; x < dim; x++) {
+                *ptr++ = even_odd ? mip_colors[mip_index] : 0xFF000000;
+                even_odd = !even_odd;
+            }
+            even_odd = !even_odd;
+        }
+    }
+    sg_filter min_filter[] = {
+        SG_FILTER_NEAREST_MIPMAP_NEAREST,
+        SG_FILTER_LINEAR_MIPMAP_NEAREST,
+        SG_FILTER_NEAREST_MIPMAP_LINEAR,
+        SG_FILTER_LINEAR_MIPMAP_LINEAR
+    };
+    sg_image img[4];
+    for (int i = 0; i < 4; i++) {
+        img[i] = sg_make_image(&(sg_image_desc){
+            .width = 256,
+            .height = 256,
+            .num_mipmaps = 9,
+            .pixel_format = SG_PIXELFORMAT_RGBA8,
+            .min_filter = min_filter[i],
+            .mag_filter = SG_FILTER_LINEAR,
+            .content = img_content
+        });
+    }
+
+    /* shader */
+    sg_shader shd = sg_make_shader(&(sg_shader_desc){
+        .vs = {
+            .uniform_blocks[0] = {
+                .size = sizeof(vs_params_t),
+                .uniforms = {
+                    [0] = { .name="mvp", .type=SG_UNIFORMTYPE_MAT4 }
+                }
+            },
+            .source =
+                "#version 330\n"
+                "uniform mat4 mvp;\n"
+                "in vec4 position;\n"
+                "in vec2 texcoord0;\n"
+                "out vec2 uv;\n"
+                "void main() {\n"
+                "  gl_Position = mvp * position;\n"
+                "  uv = texcoord0;\n"
+                "}\n"
+        },
+        .fs = {
+            .images[0].type = SG_IMAGETYPE_2D,
+            .source =
+                "#version 330\n"
+                "uniform sampler2D tex;"
+                "in vec2 uv;\n"
+                "out vec4 frag_color;\n"
+                "void main() {\n"
+                "  frag_color = texture(tex, uv);\n"
+                "}\n"
+        }
+    });
+
+    /* pipeline state */
+    sg_pipeline pip = sg_make_pipeline(&(sg_pipeline_desc) {
+        .vertex_layouts[0] = {
+            .stride = 20,
+            .attrs = {
+                [0] = { .name="position", .offset=0,  .format=SG_VERTEXFORMAT_FLOAT3 },
+                [1] = { .name="texcoord0", .offset=12, .format=SG_VERTEXFORMAT_FLOAT2 }
+            } 
+        },
+        .shader = shd,
+        .primitive_type = SG_PRIMITIVETYPE_TRIANGLE_STRIP,
+        .rasterizer.sample_count = MSAA_SAMPLES
+    });
+
+    /* view-projection matrix */
+    hmm_mat4 proj = HMM_Perspective(90.0f, (float)WIDTH/(float)HEIGHT, 0.01f, 10.0f);
+    hmm_mat4 view = HMM_LookAt(HMM_Vec3(0.0f, 0.0f, 5.0f), HMM_Vec3(0.0f, 0.0f, 0.0f), HMM_Vec3(0.0f, 1.0f, 0.0f));
+    hmm_mat4 view_proj = HMM_MultiplyMat4(proj, view);
+    
+    vs_params_t vs_params;
+    float r = 0.0f;
+    while (!glfwWindowShouldClose(w)) {
+        r += 0.1f;
+        hmm_mat4 rm = HMM_Rotate(r, HMM_Vec3(1.0f, 0.0f, 0.0f));
+
+        sg_draw_state draw_state = {
+            .pipeline = pip,
+            .vertex_buffers[0] = vbuf
+        };
+        int cur_width, cur_height;
+        glfwGetWindowSize(w, &cur_width, &cur_height);
+        sg_begin_default_pass(&(sg_pass_action){0}, cur_width, cur_height);
+        for (int i = 0; i < 4; i++) {
+            hmm_vec3 pos;
+            switch (i) {
+                case 0: pos = HMM_Vec3(-3.0f, 0.0f, 0.0f); break;
+                case 1: pos = HMM_Vec3(-1.0f, 0.0f, 0.0f); break;
+                case 2: pos = HMM_Vec3(+1.0f, 0.0f, 0.0f); break;
+                case 3: pos = HMM_Vec3(+3.0f, 0.0f, 0.0f); break;
+            }
+            hmm_mat4 model = HMM_MultiplyMat4(HMM_Translate(pos), rm);
+            vs_params.mvp = HMM_MultiplyMat4(view_proj, model);
+            
+            draw_state.fs_images[0] = img[i];
+            sg_apply_draw_state(&draw_state);
+            sg_apply_uniform_block(SG_SHADERSTAGE_VS, 0, &vs_params, sizeof(vs_params));
+            sg_draw(0, 4, 1);
+        }
+        sg_end_pass();
+        sg_commit();
+        glfwSwapBuffers(w);
+        glfwPollEvents();
+    }
+    sg_shutdown();
+    glfwTerminate();
+}
---
+++ osx-metal/mipmap-metal.c
@@ -0,0 +1,203 @@
+//------------------------------------------------------------------------------
+//  mipmap-metal.c
+//  Texture sampling test.
+//------------------------------------------------------------------------------
+#include "osxentry.h"
+#include "sokol_gfx.h"
+#define HANDMADE_MATH_IMPLEMENTATION
+#define HANDMADE_MATH_NO_SSE
+#include "HandmadeMath.h"
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+const int MSAA_SAMPLES = 4;
+
+sg_pipeline pip;
+sg_buffer vbuf;
+sg_image img[4];
+float r = 0.0f;
+hmm_mat4 view_proj;
+
+typedef struct {
+    hmm_mat4 mvp;
+} vs_params_t;
+
+struct {
+    uint32_t mip0[65536];   /* 256x256 */
+    uint32_t mip1[16384];   /* 128x128 */
+    uint32_t mip2[4096];    /* 64*64 */
+    uint32_t mip3[1024];    /* 32*32 */
+    uint32_t mip4[256];     /* 16*16 */
+    uint32_t mip5[64];      /* 8*8 */
+    uint32_t mip6[16];      /* 4*4 */
+    uint32_t mip7[4];       /* 2*2 */
+    uint32_t mip8[1];       /* 1*2 */
+} pixels;
+
+uint32_t mip_colors[9] = {
+    0xFF0000FF,     /* red */
+    0xFF00FF00,     /* green */
+    0xFFFF0000,     /* blue */
+    0xFFFF00FF,     /* magenta */
+    0xFFFFFF00,     /* cyan */
+    0xFF00FFFF,     /* yellow */
+    0xFFFF00A0,     /* violet */
+    0xFFFFA0FF,     /* orange */
+    0xFFA000FF,     /* purple */
+};
+
+void init(const void* mtl_device) {
+    /* setup sokol */
+    sg_setup(&(sg_desc){
+        .mtl_device = mtl_device,
+        .mtl_renderpass_descriptor_cb = osx_mtk_get_render_pass_descriptor,
+        .mtl_drawable_cb = osx_mtk_get_drawable
+    });
+
+    /* a plane vertex buffer */
+    float vertices[] = {
+        -1.0, -1.0, 0.0,  0.0, 0.0,
+        +1.0, -1.0, 0.0,  1.0, 0.0,
+        -1.0, +1.0, 0.0,  0.0, 1.0,
+        +1.0, +1.0, 0.0,  1.0, 1.0,
+    };
+    vbuf = sg_make_buffer(&(sg_buffer_desc){
+        .size = sizeof(vertices),
+        .content = vertices
+    });
+
+    /* mipmapped images, with different sampling filters */
+    sg_image_content img_content;
+    uint32_t* ptr = pixels.mip0;
+    bool even_odd = false;
+    for (int mip_index = 0; mip_index <= 8; mip_index++) {
+        const int dim = 1<<(8-mip_index);
+        img_content.subimage[0][mip_index].ptr = ptr;
+        img_content.subimage[0][mip_index].size = dim * dim * 4;
+        for (int y = 0; y < dim; y++) {
+            for (int x = 0; x < dim; x++) {
+                *ptr++ = even_odd ? mip_colors[mip_index] : 0xFF000000;
+                even_odd = !even_odd;
+            }
+            even_odd = !even_odd;
+        }
+    }
+    sg_filter min_filter[] = {
+        SG_FILTER_NEAREST_MIPMAP_NEAREST,
+        SG_FILTER_LINEAR_MIPMAP_NEAREST,
+        SG_FILTER_NEAREST_MIPMAP_LINEAR,
+        SG_FILTER_LINEAR_MIPMAP_LINEAR
+    };
+    for (int i = 0; i < 4; i++) {
+        img[i] = sg_make_image(&(sg_image_desc){
+            .width = 256,
+            .height = 256,
+            .num_mipmaps = 9,
+            .pixel_format = SG_PIXELFORMAT_RGBA8,
+            .min_filter = min_filter[i],
+            .mag_filter = SG_FILTER_LINEAR,
+            .content = img_content
+        });
+    }
+
+    /* shader */
+    sg_shader shd = sg_make_shader(&(sg_shader_desc){
+        .vs = {
+            .uniform_blocks[0].size = sizeof(vs_params_t),
+            .entry = "vs_main",
+            .source =
+                "#include <metal_stdlib>\n"
+                "using namespace metal;\n"
+                "struct params_t {\n"
+                "  float4x4 mvp;\n"
+                "};\n"
+                "struct vs_in {\n"
+                "  float4 position [[attribute(0)]];\n"
+                "  float2 uv [[attribute(1)]];\n"
+                "};\n"
+                "struct vs_out {\n"
+                "  float4 pos [[position]];\n"
+                "  float2 uv;\n"
+                "};\n"
+                "vertex vs_out vs_main(vs_in in [[stage_in]], constant params_t& params [[buffer(0)]]) {\n"
+                "  vs_out out;\n"
+                "  out.pos = params.mvp * in.position;\n"
+                "  out.uv = in.uv;\n"
+                "  return out;\n"
+                "}\n"
+        },
+        .fs = {
+            .images[0].type = SG_IMAGETYPE_2D,
+            .entry = "fs_main",
+            .source =
+                "#include <metal_stdlib>\n"
+                "using namespace metal;\n"
+                "struct fs_in {\n"
+                "  float2 uv;\n"
+                "};\n"
+                "fragment float4 fs_main(fs_in in [[stage_in]],\n"
+                "  texture2d<float> tex [[texture(0)]],\n"
+                "  sampler smp [[sampler(0)]])\n"
+                "{\n"
+                "  return float4(tex.sample(smp, in.uv).xyz, 1.0);\n"
+                "};\n"
+        }
+    });
+
+    /* pipeline state */
+    pip = sg_make_pipeline(&(sg_pipeline_desc) {
+        .vertex_layouts[0] = {
+            .stride = 20,
+            .attrs = {
+                [0] = { .offset=0,  .format=SG_VERTEXFORMAT_FLOAT3 },
+                [1] = { .offset=12, .format=SG_VERTEXFORMAT_FLOAT2 }
+            } 
+        },
+        .shader = shd,
+        .primitive_type = SG_PRIMITIVETYPE_TRIANGLE_STRIP,
+        .rasterizer.sample_count = MSAA_SAMPLES
+    });
+
+    /* view-projection matrix */
+    hmm_mat4 proj = HMM_Perspective(90.0f, (float)WIDTH/(float)HEIGHT, 0.01f, 10.0f);
+    hmm_mat4 view = HMM_LookAt(HMM_Vec3(0.0f, 0.0f, 5.0f), HMM_Vec3(0.0f, 0.0f, 0.0f), HMM_Vec3(0.0f, 1.0f, 0.0f));
+    view_proj = HMM_MultiplyMat4(proj, view);
+}
+
+void frame() {
+    vs_params_t vs_params;
+    r += 0.1f;
+    hmm_mat4 rm = HMM_Rotate(r, HMM_Vec3(1.0f, 0.0f, 0.0f));
+
+    sg_draw_state draw_state = {
+        .pipeline = pip,
+        .vertex_buffers[0] = vbuf
+    };
+    sg_begin_default_pass(&(sg_pass_action){0}, osx_width(), osx_height());
+    for (int i = 0; i < 4; i++) {
+        hmm_vec3 pos;
+        switch (i) {
+            case 0: pos = HMM_Vec3(-3.0f, 0.0f, 0.0f); break;
+            case 1: pos = HMM_Vec3(-1.0f, 0.0f, 0.0f); break;
+            case 2: pos = HMM_Vec3(+1.0f, 0.0f, 0.0f); break;
+            case 3: pos = HMM_Vec3(+3.0f, 0.0f, 0.0f); break;
+        }
+        hmm_mat4 model = HMM_MultiplyMat4(HMM_Translate(pos), rm);
+        vs_params.mvp = HMM_MultiplyMat4(view_proj, model);
+        
+        draw_state.fs_images[0] = img[i];
+        sg_apply_draw_state(&draw_state);
+        sg_apply_uniform_block(SG_SHADERSTAGE_VS, 0, &vs_params, sizeof(vs_params));
+        sg_draw(0, 4, 1);
+    }
+    sg_end_pass();
+    sg_commit();
+}
+
+void shutdown() {
+    sg_shutdown();
+}
+
+int main() {
+    osx_start(WIDTH, HEIGHT, MSAA_SAMPLES, "Sokol Mipmapping (Metal)", init, frame, shutdown);
+}
