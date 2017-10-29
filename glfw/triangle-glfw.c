e7fe38e3c4e27a5944b149c4f33422f4919e23f6
Andre Weissflog <floooh@gmail.com>
2017-10-29 11:58:24 +0100

    Clean up the GLFW triangle sample a bit

--- glfw/triangle-glfw.c
+++ glfw/triangle-glfw.c
@@ -11,80 +11,78 @@
 
 int main() {
 
-    const int WIDTH = 640;
-    const int HEIGHT = 480;
-
     /* create window and GL context via GLFW */
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    GLFWwindow* w = glfwCreateWindow(WIDTH, HEIGHT, "Sokol Triangle GLFW", 0, 0);
+    GLFWwindow* w = glfwCreateWindow(640, 480, "Sokol Triangle GLFW", 0, 0);
     glfwMakeContextCurrent(w);
     glfwSwapInterval(1);
     flextInit(w);
 
     /* setup sokol_gfx */
-    sg_desc desc = {0}; 
-    sg_setup(&desc);
-    assert(sg_isvalid());
+    sg_setup(&(sg_desc){0});
 
-    /* default pass action (clears to grey) */
-    sg_pass_action pass_action = {0};
-
-    /* vertex data for the triangle */
+    /* a vertex buffer */
     const float vertices[] = {
         // positions            // colors
          0.0f,  0.5f, 0.5f,     1.0f, 0.0f, 0.0f, 1.0f,
          0.5f, -0.5f, 0.5f,     0.0f, 1.0f, 0.0f, 1.0f,
         -0.5f, -0.5f, 0.5f,     0.0f, 0.0f, 1.0f, 1.0f 
     };
+    sg_buffer vbuf = sg_make_buffer(&(sg_buffer_desc){
+        .size = sizeof(vertices),
+        .content = vertices, 
+    });
 
-    /*
-        Ok, this is maybe a bit ridiculous.
+    /* a shader */
+    sg_shader shd = sg_make_shader(&(sg_shader_desc){
+        .vs.source = 
+            "#version 330\n"
+            "in vec4 position;\n"
+            "in vec4 color0;\n"
+            "out vec4 color;\n"
+            "void main() {\n"
+            "  gl_Position = position;\n"
+            "  color = color0;\n"
+            "}\n",
+        .fs.source =
+            "#version 330\n"
+            "in vec4 color;\n"
+            "out vec4 frag_color;\n"
+            "void main() {\n"
+            "  frag_color = color;\n"
+            "}\n"
+    });
 
-        The following initializes a sg_draw_state struct containing
-        a pipeline and buffer resource, and creates these resources
-        'in-place'.
-    */
+    /* a pipeline state object */
+    sg_pipeline pip = sg_make_pipeline(&(sg_pipeline_desc){
+        .shader = shd,
+        .vertex_layouts[0] = {
+            .stride = 28,
+            .attrs = {
+                [0] = { .name="position", .offset=0, .format=SG_VERTEXFORMAT_FLOAT3 },
+                [1] = { .name="color0", .offset=12, .format=SG_VERTEXFORMAT_FLOAT4 }
+            }
+        }
+    });
+
+    /* a draw state with all the resource binding */
     sg_draw_state draw_state = {
-        .pipeline = sg_make_pipeline(&(sg_pipeline_desc){
-            .vertex_layouts[0] = {
-                .stride = 28,
-                .attrs = {
-                    [0] = { .name="position", .offset=0, .format=SG_VERTEXFORMAT_FLOAT3 },
-                    [1] = { .name="color0", .offset=12, .format=SG_VERTEXFORMAT_FLOAT4 }
-                }
-            },
-            .shader = sg_make_shader(&(sg_shader_desc){
-                .vs.source = 
-                    "#version 330\n"
-                    "in vec4 position;\n"
-                    "in vec4 color0;\n"
-                    "out vec4 color;\n"
-                    "void main() {\n"
-                    "  gl_Position = position;\n"
-                    "  color = color0;\n"
-                    "}\n",
-                .fs.source =
-                    "#version 330\n"
-                    "in vec4 color;\n"
-                    "out vec4 frag_color;\n"
-                    "void main() {\n"
-                    "  frag_color = color;\n"
-                    "}\n"
-            })
-        }),
-        .vertex_buffers[0] = sg_make_buffer(&(sg_buffer_desc){
-            .size = sizeof(vertices),
-            .content = vertices, 
-        })
+        .pipeline = pip,
+        .vertex_buffers[0] = vbuf
     };
+
+    /* default pass action (clear to grey) */
+    sg_pass_action pass_action = {0};
 
     /* draw loop */
     while (!glfwWindowShouldClose(w)) {
-        sg_begin_default_pass(&pass_action, WIDTH, HEIGHT);
+        int cur_width, cur_height;
+        glfwGetWindowSize(w, &cur_width, &cur_height);
+        sg_begin_default_pass(&pass_action, cur_width, cur_height);
         sg_apply_draw_state(&draw_state);
         sg_draw(0, 3, 1);
         sg_end_pass();
