5d5d75e7ff737d1cc6075f6c5c6e7b9fdd6871ce
Andre Weissflog <floooh@gmail.com>
2018-04-13 17:06:52 +0200

    add new sample noninterleaved-metal

---
+++ metal/noninterleaved-metal.c
@@ -0,0 +1,183 @@
+//------------------------------------------------------------------------------
+//  noninterleaved-glfw.c
+//  How to use non-interleaved vertex data (vertex components in 
+//  separate non-interleaved chunks in the same vertex buffers). Note
+//  that only 4 separate chunks are currently possible because there 
+//  are 4 vertex buffer bind slots in sg_draw_state, but you can keep
+//  several related vertex components interleaved in the same chunk.
+//------------------------------------------------------------------------------
+#include "osxentry.h"
+#include "sokol_gfx.h"
+#define HANDMADE_MATH_IMPLEMENTATION
+#define HANDMADE_MATH_NO_SSE
+#include "HandmadeMath.h"
+
+const int WIDTH = 640;
+const int HEIGHT = 480;
+const int MSAA_SAMPLES = 4;
+
+sg_pass_action pass_action;
+sg_draw_state draw_state;
+float rx = 0.0f;
+float ry = 0.0f;
+hmm_mat4 view_proj;
+
+typedef struct {
+    hmm_mat4 mvp;
+} vs_params_t;
+
+void init(const void* mtl_device) {
+    /* setup sokol */
+    sg_desc desc = {
+        .mtl_device = mtl_device,
+        .mtl_renderpass_descriptor_cb = osx_mtk_get_render_pass_descriptor,
+        .mtl_drawable_cb = osx_mtk_get_drawable
+    };
+    sg_setup(&desc);
+
+    /* cube vertex buffer */
+    float vertices[] = {
+        /* positions */
+        -1.0, -1.0, -1.0,   1.0, -1.0, -1.0,   1.0,  1.0, -1.0,  -1.0,  1.0, -1.0,
+        -1.0, -1.0,  1.0,   1.0, -1.0,  1.0,   1.0,  1.0,  1.0,  -1.0,  1.0,  1.0,
+        -1.0, -1.0, -1.0,  -1.0,  1.0, -1.0,  -1.0,  1.0,  1.0,  -1.0, -1.0,  1.0,
+         1.0, -1.0, -1.0,   1.0,  1.0, -1.0,   1.0,  1.0,  1.0,   1.0, -1.0,  1.0, 
+        -1.0, -1.0, -1.0,  -1.0, -1.0,  1.0,   1.0, -1.0,  1.0,   1.0, -1.0, -1.0,
+        -1.0,  1.0, -1.0,  -1.0,  1.0,  1.0,   1.0,  1.0,  1.0,   1.0,  1.0, -1.0,
+
+         /* colors */
+        1.0, 0.5, 0.0, 1.0,  1.0, 0.5, 0.0, 1.0,  1.0, 0.5, 0.0, 1.0,  1.0, 0.5, 0.0, 1.0,
+        0.5, 1.0, 0.0, 1.0,  0.5, 1.0, 0.0, 1.0,  0.5, 1.0, 0.0, 1.0,  0.5, 1.0, 0.0, 1.0,
+        0.5, 0.0, 1.0, 1.0,  0.5, 0.0, 1.0, 1.0,  0.5, 0.0, 1.0, 1.0,  0.5, 0.0, 1.0, 1.0,
+        1.0, 0.5, 1.0, 1.0,  1.0, 0.5, 1.0, 1.0,  1.0, 0.5, 1.0, 1.0,  1.0, 0.5, 1.0, 1.0,
+        0.5, 1.0, 1.0, 1.0,  0.5, 1.0, 1.0, 1.0,  0.5, 1.0, 1.0, 1.0,  0.5, 1.0, 1.0, 1.0,
+        1.0, 1.0, 0.5, 1.0,  1.0, 1.0, 0.5, 1.0,  1.0, 1.0, 0.5, 1.0,  1.0, 1.0, 0.5, 1.0,
+    };
+    sg_buffer vbuf = sg_make_buffer(&(sg_buffer_desc){
+        .size = sizeof(vertices),
+        .content = vertices
+    });
+
+    /* create an index buffer for the cube */
+    uint16_t indices[] = {
+        0, 1, 2,  0, 2, 3,
+        6, 5, 4,  7, 6, 4,
+        8, 9, 10,  8, 10, 11,
+        14, 13, 12,  15, 14, 12,
+        16, 17, 18,  16, 18, 19,
+        22, 21, 20,  23, 22, 20
+    };
+    sg_buffer ibuf = sg_make_buffer(&(sg_buffer_desc){
+        .type = SG_BUFFERTYPE_INDEXBUFFER,
+        .size = sizeof(indices),
+        .content = indices
+    });
+
+    /*
+        a shader, note that Metal only needs to know uniform block sizes, but
+        not their internal layout
+    */
+    sg_shader shd = sg_make_shader(&(sg_shader_desc){
+        .vs.uniform_blocks[0].size = sizeof(vs_params_t),
+        .vs.source =
+            "#include <metal_stdlib>\n"
+            "using namespace metal;\n"
+            "struct params_t {\n"
+            "  float4x4 mvp;\n"
+            "};\n"
+            "struct vs_in {\n"
+            "  float4 position [[attribute(0)]];\n"
+            "  float4 color [[attribute(1)]];\n"
+            "};\n"
+            "struct vs_out {\n"
+            "  float4 pos [[position]];\n"
+            "  float4 color;\n"
+            "};\n"
+            "vertex vs_out _main(vs_in in [[stage_in]], constant params_t& params [[buffer(0)]]) {\n"
+            "  vs_out out;\n"
+            "  out.pos = params.mvp * in.position;\n"
+            "  out.color = in.color;\n"
+            "  return out;\n"
+            "}\n",
+        .fs.source =
+            "#include <metal_stdlib>\n"
+            "using namespace metal;\n"
+            "fragment float4 _main(float4 color [[stage_in]]) {\n"
+            "  return color;\n"
+            "}\n"
+    });
+
+    /* 
+        a pipeline object, note that we need to provide the
+        MSAA sample count of the default framebuffer
+    */
+    sg_pipeline pip = sg_make_pipeline(&(sg_pipeline_desc){
+        .layout = {
+            /* note how the vertex components are pulled from different buffer bind slots */
+            .attrs = {
+                /* positions come from vertex buffer slot 0 */
+                [0] = { .format=SG_VERTEXFORMAT_FLOAT3, .buffer_index=0 },
+                /* colors come from vertex buffer slot 1 */
+                [1] = { .format=SG_VERTEXFORMAT_FLOAT4, .buffer_index=1 }
+            }
+        },        .shader = shd,
+        .index_type = SG_INDEXTYPE_UINT16,
+        .depth_stencil = {
+            .depth_compare_func = SG_COMPAREFUNC_LESS_EQUAL,
+            .depth_write_enabled = true
+        },
+        .rasterizer.cull_mode = SG_CULLMODE_BACK,
+        .rasterizer.sample_count = MSAA_SAMPLES
+    });
+
+    /* fill the draw state struct with resource bindings, note how the same vertex 
+       buffer is bound to the first two slots, and the vertex-buffer-offsets
+       are used to point to the position- and color-components.
+    */
+    draw_state = (sg_draw_state){
+        .pipeline = pip,
+        .vertex_buffers = {
+            [0] = vbuf,
+            [1] = vbuf
+        },
+        .vertex_buffer_offsets = {
+            /* position components are at start of buffer */
+            [0] = 0,
+            /* byte offset of color components in buffer */
+            [1] = 24 * 3 * sizeof(float)
+        },
+        .index_buffer = ibuf
+    };
+
+    /* view-projection matrix */
+    hmm_mat4 proj = HMM_Perspective(60.0f, (float)WIDTH/(float)HEIGHT, 0.01f, 10.0f);
+    hmm_mat4 view = HMM_LookAt(HMM_Vec3(0.0f, 1.5f, 6.0f), HMM_Vec3(0.0f, 0.0f, 0.0f), HMM_Vec3(0.0f, 1.0f, 0.0f));
+    view_proj = HMM_MultiplyMat4(proj, view);
+}
+
+void frame() {
+    /* compute model-view-projection matrix for vertex shader */
+    vs_params_t vs_params;
+    rx += 1.0f; ry += 2.0f;
+    hmm_mat4 rxm = HMM_Rotate(rx, HMM_Vec3(1.0f, 0.0f, 0.0f));
+    hmm_mat4 rym = HMM_Rotate(ry, HMM_Vec3(0.0f, 1.0f, 0.0f));
+    hmm_mat4 model = HMM_MultiplyMat4(rxm, rym);
+    vs_params.mvp = HMM_MultiplyMat4(view_proj, model);
+
+    sg_begin_default_pass(&pass_action, osx_width(), osx_height());
+    sg_apply_draw_state(&draw_state);
+    sg_apply_uniform_block(SG_SHADERSTAGE_VS, 0, &vs_params, sizeof(vs_params));
+    sg_draw(0, 36, 1);
+    sg_end_pass();
+    sg_commit();
+}
+
+void shutdown() {
+    sg_shutdown();
+}
+
+int main() {
+    osx_start(WIDTH, HEIGHT, MSAA_SAMPLES, "Sokol Non-Interleaved Vertex Data (Metal)", init, frame, shutdown);
+    return 0;
+}
+
