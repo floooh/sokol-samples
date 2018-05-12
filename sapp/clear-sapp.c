365cd608ae407a42d9d20aec10183b2b125b7e90
Andre Weissflog <floooh@gmail.com>
2018-05-12 19:45:00 +0200

    clear-sapp sample working (for Metal)

--- sapp/clear-sapp.c
+++ sapp/clear-sapp.c
@@ -4,19 +4,32 @@
 #include "sokol_gfx.h"
 #include "sokol_app.h"
 
-void sokol_enter() {
+sg_pass_action pass_action;
+
+void sokol_init() {
     sapp_setup(&(sapp_desc){
         .width = 400,
         .height = 300,
-        .window_title = "Clear (sapp)"
+        .window_title = "Clear (sokol-app)"
     });
+    sg_setup(&(sg_desc){
+        .mtl_device = sapp_metal_get_device(),
+        .mtl_renderpass_descriptor_cb = sapp_metal_get_renderpass_descriptor,
+        .mtl_drawable_cb = sapp_metal_get_drawable,
+    });
+    pass_action = (sg_pass_action) {
+        .colors[0] = { .action=SG_ACTION_CLEAR, .val={1.0f, 0.0f, 0.0f, 1.0f} }
+    };
 }
 
 void sokol_frame() {
-
+    float g = pass_action.colors[0].val[1] + 0.01f;
+    pass_action.colors[0].val[1] = (g > 1.0f) ? 0.0f : g;
+    sg_begin_default_pass(&pass_action, sapp_width(), sapp_height());
+    sg_end_pass();
+    sg_commit();
 }
 
-int sokol_exit() {
-    sapp_shutdown();
-    return 0;
+void sokol_shutdown() {
+    sg_shutdown();
 }
