945cef5203492073c8deeb63c7c7b83fe5931e55
Andre Weissflog <floooh@gmail.com>
2018-05-14 17:01:35 +0200

    new sample cube-sapp

---
+++ sapp/cube-sapp.c
@@ -0,0 +1,200 @@
+//------------------------------------------------------------------------------
+//  clear-sapp.c
+//------------------------------------------------------------------------------
+#define HANDMADE_MATH_IMPLEMENTATION
+#define HANDMADE_MATH_NO_SSE
+#include "HandmadeMath.h"
+#include "sokol_gfx.h"
+#include "sokol_app.h"
+
+extern const char *vs_src, *fs_src;
+
+const int SAMPLE_COUNT = 1;
+float rx = 0.0f;
+float ry = 0.0f;
+sg_draw_state draw_state;
+
+typedef struct {
+    hmm_mat4 mvp;
+} vs_params_t;
+
+void sokol_init() {
+    sapp_setup(&(sapp_desc){
+        .width = 800,
+        .height = 600,
+        .sample_count = SAMPLE_COUNT,
+        .window_title = "Cube (sokol-app)"
+    });
+    sg_setup(&(sg_desc){
+        .mtl_device = sapp_metal_get_device(),
+        .mtl_renderpass_descriptor_cb = sapp_metal_get_renderpass_descriptor,
+        .mtl_drawable_cb = sapp_metal_get_drawable,
+    });
+
+    /* cube vertex buffer */
+    float vertices[] = {
+        -1.0, -1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
+         1.0, -1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
+         1.0,  1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
+        -1.0,  1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
+
+        -1.0, -1.0,  1.0,   0.0, 1.0, 0.0, 1.0,
+         1.0, -1.0,  1.0,   0.0, 1.0, 0.0, 1.0,
+         1.0,  1.0,  1.0,   0.0, 1.0, 0.0, 1.0,
+        -1.0,  1.0,  1.0,   0.0, 1.0, 0.0, 1.0,
+
+        -1.0, -1.0, -1.0,   0.0, 0.0, 1.0, 1.0,
+        -1.0,  1.0, -1.0,   0.0, 0.0, 1.0, 1.0,
+        -1.0,  1.0,  1.0,   0.0, 0.0, 1.0, 1.0,
+        -1.0, -1.0,  1.0,   0.0, 0.0, 1.0, 1.0,
+
+        1.0, -1.0, -1.0,    1.0, 0.5, 0.0, 1.0,
+        1.0,  1.0, -1.0,    1.0, 0.5, 0.0, 1.0,
+        1.0,  1.0,  1.0,    1.0, 0.5, 0.0, 1.0,
+        1.0, -1.0,  1.0,    1.0, 0.5, 0.0, 1.0,
+
+        -1.0, -1.0, -1.0,   0.0, 0.5, 1.0, 1.0,
+        -1.0, -1.0,  1.0,   0.0, 0.5, 1.0, 1.0,
+         1.0, -1.0,  1.0,   0.0, 0.5, 1.0, 1.0,
+         1.0, -1.0, -1.0,   0.0, 0.5, 1.0, 1.0,
+
+        -1.0,  1.0, -1.0,   1.0, 0.0, 0.5, 1.0,
+        -1.0,  1.0,  1.0,   1.0, 0.0, 0.5, 1.0,
+         1.0,  1.0,  1.0,   1.0, 0.0, 0.5, 1.0,
+         1.0,  1.0, -1.0,   1.0, 0.0, 0.5, 1.0
+    };
+    sg_buffer vbuf = sg_make_buffer(&(sg_buffer_desc){
+        .size = sizeof(vertices),
+        .content = vertices,
+    });
+
+    /* create an index buffer for the cube */
+    uint16_t indices[] = {
+        0, 1, 2,  0, 2, 3,
+        6, 5, 4,  7, 6, 4,
+        8, 9, 10,  8, 10, 11,
+        14, 13, 12,  15, 14, 12,
+        16, 17, 18,  16, 18, 19,
+        22, 21, 20,  23, 22, 20
+    };
+    sg_buffer ibuf = sg_make_buffer(&(sg_buffer_desc){
+        .type = SG_BUFFERTYPE_INDEXBUFFER,
+        .size = sizeof(indices),
+        .content = indices,
+    });
+
+    /* create shader */
+    sg_shader shd = sg_make_shader(&(sg_shader_desc) {
+        .vs.uniform_blocks[0] = {
+            .size = sizeof(vs_params_t),
+            .uniforms = {
+                [0] = { .name="mvp", .type=SG_UNIFORMTYPE_MAT4 }
+            }
+        },
+        .vs.source = vs_src,
+        .fs.source = fs_src
+    });
+
+    /* create pipeline object */
+    sg_pipeline pip = sg_make_pipeline(&(sg_pipeline_desc){
+        .layout = {
+            /* test to provide buffer stride, but no attr offsets */
+            .buffers[0].stride = 28,
+            .attrs = {
+                [0] = { .name="position", .format=SG_VERTEXFORMAT_FLOAT3 },
+                [1] = { .name="color0", .format=SG_VERTEXFORMAT_FLOAT4 }
+            }
+        },
+        .shader = shd,
+        .index_type = SG_INDEXTYPE_UINT16,
+        .depth_stencil = {
+            .depth_compare_func = SG_COMPAREFUNC_LESS_EQUAL,
+            .depth_write_enabled = true,
+        },
+        .rasterizer.cull_mode = SG_CULLMODE_BACK,
+        .rasterizer.sample_count = SAMPLE_COUNT,
+    });
+
+    /* draw state struct with resource bindings */
+    draw_state = (sg_draw_state) {
+        .pipeline = pip,
+        .vertex_buffers[0] = vbuf,
+        .index_buffer = ibuf
+    };
+}
+
+void sokol_frame() {
+    vs_params_t vs_params;
+    const float w = (float) sapp_width();
+    const float h = (float) sapp_height();
+    hmm_mat4 proj = HMM_Perspective(60.0f, w/h, 0.01f, 10.0f);
+    hmm_mat4 view = HMM_LookAt(HMM_Vec3(0.0f, 1.5f, 6.0f), HMM_Vec3(0.0f, 0.0f, 0.0f), HMM_Vec3(0.0f, 1.0f, 0.0f));
+    hmm_mat4 view_proj = HMM_MultiplyMat4(proj, view);
+    rx += 1.0f; ry += 2.0f;
+    hmm_mat4 rxm = HMM_Rotate(rx, HMM_Vec3(1.0f, 0.0f, 0.0f));
+    hmm_mat4 rym = HMM_Rotate(ry, HMM_Vec3(0.0f, 1.0f, 0.0f));
+    hmm_mat4 model = HMM_MultiplyMat4(rxm, rym);
+    vs_params.mvp = HMM_MultiplyMat4(view_proj, model);
+
+    sg_pass_action pass_action = {
+        .colors[0] = { .action = SG_ACTION_CLEAR, .val = { 0.25f, 0.5f, 0.75f, 1.0f } }
+    };
+    sg_begin_default_pass(&pass_action, w, h);
+    sg_apply_draw_state(&draw_state);
+    sg_apply_uniform_block(SG_SHADERSTAGE_VS, 0, &vs_params, sizeof(vs_params));
+    sg_draw(0, 36, 1);
+    sg_end_pass();
+    sg_commit();
+}
+
+void sokol_shutdown() {
+    sg_shutdown();
+    sapp_shutdown();
+}
+
+#if defined(SOKOL_GLCORE33)
+const char* vs_src =
+    "#version 330\n"
+    "uniform mat4 mvp;\n"
+    "in vec4 position;\n"
+    "in vec4 color0;\n"
+    "out vec4 color;\n"
+    "void main() {\n"
+    "  gl_Position = mvp * position;\n"
+    "  color = color0;\n"
+    "}\n";
+const char* fs_src =
+    "#version 330\n"
+    "in vec4 color;\n"
+    "out vec4 frag_color;\n"
+    "void main() {\n"
+    "  frag_color = color;\n"
+    "}\n";
+#elif defined(SOKOL_METAL)
+const char* vs_src =
+    "#include <metal_stdlib>\n"
+    "using namespace metal;\n"
+    "struct params_t {\n"
+    "  float4x4 mvp;\n"
+    "};\n"
+    "struct vs_in {\n"
+    "  float4 position [[attribute(0)]];\n"
+    "  float4 color [[attribute(1)]];\n"
+    "};\n"
+    "struct vs_out {\n"
+    "  float4 pos [[position]];\n"
+    "  float4 color;\n"
+    "};\n"
+    "vertex vs_out _main(vs_in in [[stage_in]], constant params_t& params [[buffer(0)]]) {\n"
+    "  vs_out out;\n"
+    "  out.pos = params.mvp * in.position;\n"
+    "  out.color = in.color;\n"
+    "  return out;\n"
+    "}\n";
+const char* fs_src =
+    "#include <metal_stdlib>\n"
+    "using namespace metal;\n"
+    "fragment float4 _main(float4 color [[stage_in]]) {\n"
+    "  return color;\n"
+    "}\n";
+#endif
