ba9573a91a0c58e1ebd3896a3057146539419e65
Andre Weissflog <floooh@gmail.com>
2018-06-10 14:53:12 +0200

    saokol-app samples: start with Windows GL support

--- sapp/sokol.c
+++ sapp/sokol.c
@@ -1,6 +1,9 @@
 #define SOKOL_IMPL
 #define SOKOL_D3D11_SHADER_COMPILER
 /* sokol 3D-API defines are provided by build options */
+#if defined(_WIN32) && defined(SOKOL_GLCORE33)
+#include "gl.h"
+#endif
 #include "sokol_app.h"
 #include "sokol_gfx.h"
 #include "sokol_time.h"
