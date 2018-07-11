03d98edd5e5690d63d3cccb68cf950d010b3e514
Andre Weissflog <floooh@gmail.com>
2018-07-11 18:23:00 +0200

    Start with sokol-audio sample

---
+++ sapp/saudio-sapp.c
@@ -0,0 +1,56 @@
+//------------------------------------------------------------------------------
+//  saudio-sapp.c
+//  Test sokol-audio
+//------------------------------------------------------------------------------
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+#include "sokol_audio.h"
+
+sg_pass_action pass_action = {
+    .colors[0] = { .action = SG_ACTION_CLEAR, .val = { 1.0f, 0.5f, 0.0f, 1.0f } }
+};
+
+void stream(float* buffer, int num_samples) {
+    for (int i = 0; i < num_samples; i++) {
+        buffer[i] = 0.0f;
+    }
+}
+
+void init(void) {
+    sg_setup(&(sg_desc){
+        .mtl_device = sapp_metal_get_device(),
+        .mtl_renderpass_descriptor_cb = sapp_metal_get_renderpass_descriptor,
+        .mtl_drawable_cb = sapp_metal_get_drawable,
+        .d3d11_device = sapp_d3d11_get_device(),
+        .d3d11_device_context = sapp_d3d11_get_device_context(),
+        .d3d11_render_target_view_cb = sapp_d3d11_get_render_target_view,
+        .d3d11_depth_stencil_view_cb = sapp_d3d11_get_depth_stencil_view
+    });
+    saudio_setup(&(saudio_desc){
+        .sample_rate = 44100,
+        .stream_cb = stream
+    });
+}
+
+void frame(void) {
+    sg_begin_default_pass(&pass_action, sapp_width(), sapp_height());
+    sg_end_pass();
+    sg_commit();
+}
+
+void cleanup(void) {
+    saudio_shutdown();
+    sg_shutdown();
+}
+
+sapp_desc sokol_main(int argc, char* argv[]) {
+    return (sapp_desc){
+        .init_cb = init,
+        .frame_cb = frame,
+        .cleanup_cb = cleanup,
+        .width = 400,
+        .height = 300,
+        .window_title = "Sokol Audio Test",
+    };
+}
+
--- sapp/sokol.c
+++ sapp/sokol.c
@@ -4,3 +4,4 @@
 #include "sokol_app.h"
 #include "sokol_gfx.h"
 #include "sokol_time.h"
+#include "sokol_audio.h"
