c8c728664596d0851f1c915fba9c253342573282
Andre Weissflog <floooh@gmail.com>
2018-09-24 14:15:52 +0200

    modplay-sapp: select between stream callback and push

--- sapp/modplay-sapp.c
+++ sapp/modplay-sapp.c
@@ -9,26 +9,34 @@
 #include "data/mods.h"
 #include <assert.h>
 
+/* select between mono (1) and stereo (2) */
 #define MODPLAY_NUM_CHANNELS (2)
+/* use stream callback (0) or push-from-mainthread (1) model */
+#define MODPLAY_USE_PUSH (1)
+/* big enough for packet_size * num_packets * num_channels */
+#define MODPLAY_SRCBUF_SAMPLES (16*1024)
 
 static bool mpf_valid = false;
 static ModPlugFile* mpf;
 
 /* integer-to-float conversion buffer */
-#define SRCBUF_SAMPLES (8192)
-static int src_buf[SRCBUF_SAMPLES];
+static int int_buf[MODPLAY_SRCBUF_SAMPLES];
+#if MODPLAY_USE_PUSH
+static float flt_buf[MODPLAY_SRCBUF_SAMPLES];
+#endif
 
+#if !MODPLAY_USE_PUSH
 static void stream_cb(float* buffer, int num_samples) {
-    assert(num_samples < SRCBUF_SAMPLES);
+    assert(num_samples < MODPLAY_SRCBUF_SAMPLES);
     if (mpf_valid) {
         /* read sampled from libmodplug, and convert to float, left/right
            channel are interleaved
         */
-        int res = ModPlug_Read(mpf, (void*)src_buf, sizeof(int)*num_samples);
+        int res = ModPlug_Read(mpf, (void*)int_buf, sizeof(int)*num_samples);
         int samples_in_buffer = res / sizeof(int);
         int i;
         for (i = 0; i < samples_in_buffer; i++) {
-            buffer[i] = src_buf[i] / (float)0x7fffffff;
+            buffer[i] = int_buf[i] / (float)0x7fffffff;
         }
         for (; i < num_samples; i++) {
             buffer[i] = 0.0f;
@@ -41,6 +49,7 @@ static void stream_cb(float* buffer, int num_samples) {
         }
     }
 }
+#endif
 
 void init(void) {
     /* setup sokol_gfx */
@@ -57,7 +66,9 @@ void init(void) {
     /* setup sokol_audio (default sample rate is 44100Hz) */
     saudio_setup(&(saudio_desc){
         .num_channels = MODPLAY_NUM_CHANNELS,
+        #if !MODPLAY_USE_PUSH
         .stream_cb = stream_cb
+        #endif
     });
 
     /* setup libmodplug and load mod from embedded C array */
@@ -68,7 +79,7 @@ void init(void) {
     mps.mFrequency = saudio_sample_rate();
     mps.mResamplingMode = MODPLUG_RESAMPLE_LINEAR;
     mps.mMaxMixChannels = 64;
-    mps.mLoopCount = -1;
+    mps.mLoopCount = -1; /* loop play seems to be disabled in current libmodplug */
     mps.mFlags = MODPLUG_ENABLE_OVERSAMPLING;
     ModPlug_SetSettings(&mps);
 
@@ -79,6 +90,30 @@ void init(void) {
 }
 
 void frame(void) {
+    #if MODPLAY_USE_PUSH
+        int num_frames = saudio_expect();
+        if (num_frames > 0) {
+            int num_samples = num_frames * MODPLAY_NUM_CHANNELS;
+            assert(num_samples <= MODPLAY_SRCBUF_SAMPLES);
+            if (mpf_valid) {
+                int res = ModPlug_Read(mpf, (void*)int_buf, sizeof(int)*num_samples);
+                int samples_in_buffer = res / sizeof(int);
+                int i;
+                for (i = 0; i < samples_in_buffer; i++) {
+                    flt_buf[i] = int_buf[i] / (float)0x7fffffff;
+                }
+                for (; i < num_samples; i++) {
+                    flt_buf[i] = 0.0f;
+                }
+            }
+            else {
+                for (int i = 0; i < num_samples; i++) {
+                    flt_buf[i] = 0.0f;
+                }
+            }
+            saudio_push(flt_buf, num_frames);
+        }
+    #endif
     sg_pass_action pass_action = {
         .colors[0] = { .action = SG_ACTION_CLEAR, .val = { 0.5f, 1.0f, 0.0f, 1.0f } }
     };
