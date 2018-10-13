c5a6a8414d950c8c68fe96377d419cc743216fa3
Andre Weissflog <floooh@gmail.com>
2018-10-13 18:45:11 +0200

    more sokol-args tests

--- tests/sokol-args-test.c
+++ tests/sokol-args-test.c
@@ -9,6 +9,8 @@
 
 static const char* argv_0[] = { "exe_name " };
 static const char* argv_1[] = { "exe_name", "kvp0=val0", "kvp1=val1", "kvp2=val2" };
+static const char* argv_2[] = { "exe_name", "kvp0  = val0 ", "  \tkvp1=  val1", "kvp2  = val2   "};
+static const char* argv_3[] = { "exe_name", "kvp0:val0 ", "  kvp1:  val1", "kvp2 \t: val2   "};
 
 static void test_init_shutdown(void) {
     test("sokol-args init shutdown");
@@ -69,11 +71,61 @@ static void test_simple_args(void) {
     sargs_shutdown();
 }
 
+static void test_simple_whitespace(void) {
+    test("sokol-args simple whitespace");
+    sargs_setup(&(sargs_desc){
+        .argc = NUM_ARGS(argv_2),
+        .argv = argv_2
+    });
+    T(sargs_isvalid());
+    T(sargs_num_args() == 3);
+    T(0 == sargs_find("kvp0"));
+    TSTR(sargs_value("kvp0"), "val0");
+    TSTR(sargs_key_at(0), "kvp0");
+    TSTR(sargs_value_at(0), "val0");
+    T(1 == sargs_find("kvp1"));
+    TSTR(sargs_value("kvp1"), "val1");
+    TSTR(sargs_key_at(1), "kvp1");
+    TSTR(sargs_value_at(1), "val1");
+    T(2 == sargs_find("kvp2"));
+    TSTR(sargs_value("kvp2"), "val2");
+    TSTR(sargs_key_at(2), "kvp2");
+    TSTR(sargs_value_at(2), "val2");
+    T(_sargs.buf_pos == 31);
+    sargs_shutdown();
+}
+
+static void test_colon_separator(void) {
+    test("sokol-args colon separator");
+    sargs_setup(&(sargs_desc){
+        .argc = NUM_ARGS(argv_3),
+        .argv = argv_3
+    });
+    T(sargs_isvalid());
+    T(sargs_num_args() == 3);
+    T(0 == sargs_find("kvp0"));
+    TSTR(sargs_value("kvp0"), "val0");
+    TSTR(sargs_key_at(0), "kvp0");
+    TSTR(sargs_value_at(0), "val0");
+    T(1 == sargs_find("kvp1"));
+    TSTR(sargs_value("kvp1"), "val1");
+    TSTR(sargs_key_at(1), "kvp1");
+    TSTR(sargs_value_at(1), "val1");
+    T(2 == sargs_find("kvp2"));
+    TSTR(sargs_value("kvp2"), "val2");
+    TSTR(sargs_key_at(2), "kvp2");
+    TSTR(sargs_value_at(2), "val2");
+    T(_sargs.buf_pos == 31);
+    sargs_shutdown();
+}
+
 int main() {
     test_begin("sokol-args-test");
     test_init_shutdown();
     test_no_args();
     test_simple_args();
+    test_simple_whitespace();
+    test_colon_separator();
     return test_end();
 }
 
