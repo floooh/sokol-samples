9e2a6672e7ff5a0ed6c8cea98d0b85c3cc09904a
Andre Weissflog <floooh@gmail.com>
2018-10-17 13:12:03 +0200

    sokol-args-test: add double-quotes test

--- tests/sokol-args-test.c
+++ tests/sokol-args-test.c
@@ -13,6 +13,7 @@ static const char* argv_2[] = { "exe_name", "kvp0  = val0 ", "  \tkvp1=  val1", 
 static const char* argv_3[] = { "exe_name", "kvp0:val0 ", "  kvp1:  val1", "kvp2 \t: val2   "};
 static const char* argv_4[] = { "exe_name", "kvp0 ", "=val0 ", "  kvp1", "=", "val1", "kvp2 \t", "= val2   "};
 static const char* argv_5[] = { "exe_name", "kvp0='bla bla'", "kvp1=' blub blub'", "kvp2='blob blob '"};
+static const char* argv_6[] = { "exe_name", "kvp0=\"bla bla\"", "kvp1=\" blub blub\"", "kvp2=\"blob blob \""};
 
 static void test_init_shutdown(void) {
     test("sokol-args init shutdown");
@@ -168,6 +169,29 @@ static void test_single_quotes(void) {
     sargs_shutdown();
 }
 
+static void test_double_quotes(void) {
+    test("sokol-args double quotes");
+    sargs_setup(&(sargs_desc){
+        .argc = NUM_ARGS(argv_6),
+        .argv = argv_6
+    });
+    T(sargs_isvalid());
+    T(sargs_num_args() == 3);
+    T(0 == sargs_find("kvp0"));
+    TSTR(sargs_value("kvp0"), "bla bla");
+    TSTR(sargs_key_at(0), "kvp0");
+    TSTR(sargs_value_at(0), "bla bla");
+    T(1 == sargs_find("kvp1"));
+    TSTR(sargs_value("kvp1"), " blub blub");
+    TSTR(sargs_key_at(1), "kvp1");
+    TSTR(sargs_value_at(1), " blub blub");
+    T(2 == sargs_find("kvp2"));
+    TSTR(sargs_value("kvp2"), "blob blob ");
+    TSTR(sargs_key_at(2), "kvp2");
+    TSTR(sargs_value_at(2), "blob blob ");
+    sargs_shutdown();
+}
+
 int main() {
     test_begin("sokol-args-test");
     test_init_shutdown();
@@ -177,6 +201,7 @@ int main() {
     test_colon_separator();
     test_standalone_separator();
     test_single_quotes();
+    test_double_quotes();
     return test_end();
 }
 
