f57dc23ac500290b0724db4860c0fdd02986321d
Andre Weissflog <floooh@gmail.com>
2018-10-18 19:01:47 +0200

    add texcube-sapp sample

---
+++ sapp/texcube-sapp.c
@@ -0,0 +1,314 @@
+//------------------------------------------------------------------------------
+//  texcube-sapp.c
+//  Texture creation and rendering.
+//------------------------------------------------------------------------------
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+#define HANDMADE_MATH_IMPLEMENTATION
+#define HANDMADE_MATH_NO_SSE
+#include "HandmadeMath.h"
+
+extern const char *vs_src, *fs_src;
+
+const int SAMPLE_COUNT = 4;
+float rx = 0.0f;
+float ry = 0.0f;
+sg_draw_state draw_state;
+sg_pass_action pass_action = {
+    .colors[0] = { .action = SG_ACTION_CLEAR, .val = { 0.25f, 0.5f, 0.75f, 1.0f } }
+};
+
+typedef struct {
+    hmm_mat4 mvp;
+} vs_params_t;
+
+void init(void) {
+    sg_setup(&(sg_desc){
+        .gl_force_gles2 = sapp_gles2(),
+        .mtl_device = sapp_metal_get_device(),
+        .mtl_renderpass_descriptor_cb = sapp_metal_get_renderpass_descriptor,
+        .mtl_drawable_cb = sapp_metal_get_drawable,
+        .d3d11_device = sapp_d3d11_get_device(),
+        .d3d11_device_context = sapp_d3d11_get_device_context(),
+        .d3d11_render_target_view_cb = sapp_d3d11_get_render_target_view,
+        .d3d11_depth_stencil_view_cb = sapp_d3d11_get_depth_stencil_view
+    });
+
+    /* cube vertex buffer */
+    float vertices[] = {
+        /* pos                  color                       uvs */
+        -1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f,     0.0f, 0.0f,
+         1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f,     1.0f, 0.0f,
+         1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f,     1.0f, 1.0f,
+        -1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f,     0.0f, 1.0f,
+
+        -1.0f, -1.0f,  1.0f,    0.0f, 1.0f, 0.0f, 1.0f,     0.0f, 0.0f, 
+         1.0f, -1.0f,  1.0f,    0.0f, 1.0f, 0.0f, 1.0f,     1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f,    0.0f, 1.0f, 0.0f, 1.0f,     1.0f, 1.0f,
+        -1.0f,  1.0f,  1.0f,    0.0f, 1.0f, 0.0f, 1.0f,     0.0f, 1.0f,
+
+        -1.0f, -1.0f, -1.0f,    0.0f, 0.0f, 1.0f, 1.0f,     0.0f, 0.0f,
+        -1.0f,  1.0f, -1.0f,    0.0f, 0.0f, 1.0f, 1.0f,     1.0f, 0.0f,
+        -1.0f,  1.0f,  1.0f,    0.0f, 0.0f, 1.0f, 1.0f,     1.0f, 1.0f,
+        -1.0f, -1.0f,  1.0f,    0.0f, 0.0f, 1.0f, 1.0f,     0.0f, 1.0f,
+
+         1.0f, -1.0f, -1.0f,    1.0f, 0.5f, 0.0f, 1.0f,     0.0f, 0.0f,
+         1.0f,  1.0f, -1.0f,    1.0f, 0.5f, 0.0f, 1.0f,     1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f,    1.0f, 0.5f, 0.0f, 1.0f,     1.0f, 1.0f,
+         1.0f, -1.0f,  1.0f,    1.0f, 0.5f, 0.0f, 1.0f,     0.0f, 1.0f,
+
+        -1.0f, -1.0f, -1.0f,    0.0f, 0.5f, 1.0f, 1.0f,     0.0f, 0.0f,
+        -1.0f, -1.0f,  1.0f,    0.0f, 0.5f, 1.0f, 1.0f,     1.0f, 0.0f,
+         1.0f, -1.0f,  1.0f,    0.0f, 0.5f, 1.0f, 1.0f,     1.0f, 1.0f,
+         1.0f, -1.0f, -1.0f,    0.0f, 0.5f, 1.0f, 1.0f,     0.0f, 1.0f,
+
+        -1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.5f, 1.0f,     0.0f, 0.0f,
+        -1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.5f, 1.0f,     1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.5f, 1.0f,     1.0f, 1.0f,
+         1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.5f, 1.0f,     0.0f, 1.0f
+    };
+    draw_state.vertex_buffers[0] = sg_make_buffer(&(sg_buffer_desc){
+        .size = sizeof(vertices),
+        .content = vertices,
+    });
+
+    /* create an index buffer for the cube */
+    uint16_t indices[] = {
+        0, 1, 2,  0, 2, 3,
+        6, 5, 4,  7, 6, 4,
+        8, 9, 10,  8, 10, 11,
+        14, 13, 12,  15, 14, 12,
+        16, 17, 18,  16, 18, 19,
+        22, 21, 20,  23, 22, 20
+    };
+    draw_state.index_buffer = sg_make_buffer(&(sg_buffer_desc){
+        .type = SG_BUFFERTYPE_INDEXBUFFER,
+        .size = sizeof(indices),
+        .content = indices,
+    });
+
+    /* create a checkerboard texture */
+    uint32_t pixels[4*4] = {
+        0xFFFFFFFF, 0xFF000000, 0xFFFFFFFF, 0xFF000000,
+        0xFF000000, 0xFFFFFFFF, 0xFF000000, 0xFFFFFFFF,
+        0xFFFFFFFF, 0xFF000000, 0xFFFFFFFF, 0xFF000000,
+        0xFF000000, 0xFFFFFFFF, 0xFF000000, 0xFFFFFFFF,
+    };
+    draw_state.fs_images[0] = sg_make_image(&(sg_image_desc){
+        .width = 4,
+        .height = 4,
+        .content.subimage[0][0] = {
+            .ptr = pixels,
+            .size = sizeof(pixels)
+        }
+    });
+
+    /* a shader */
+    sg_shader shd = sg_make_shader(&(sg_shader_desc) {
+        .vs = {
+            .uniform_blocks[0] = {
+                .size = sizeof(vs_params_t),
+                .uniforms = {
+                    [0] = { .name="mvp", .type=SG_UNIFORMTYPE_MAT4 }
+                }
+            },
+            .source = vs_src
+        },
+        .fs = {
+            .images[0].type = SG_IMAGETYPE_2D,
+            .source = fs_src
+        }
+    });
+
+    /* a pipeline state object */
+    draw_state.pipeline = sg_make_pipeline(&(sg_pipeline_desc){
+        .layout = {
+            .attrs = {
+                [0] = { .name="position", .sem_name="POS", .format=SG_VERTEXFORMAT_FLOAT3 },
+                [1] = { .name="color0", .sem_name="COLOR", .format=SG_VERTEXFORMAT_FLOAT4 },
+                [2] = { .name="texcoord0", .sem_name="TEXCOORD", .format=SG_VERTEXFORMAT_FLOAT2 }
+            }
+        },
+        .shader = shd,
+        .index_type = SG_INDEXTYPE_UINT16,
+        .depth_stencil = {
+            .depth_compare_func = SG_COMPAREFUNC_LESS_EQUAL,
+            .depth_write_enabled = true
+        },
+        .rasterizer = {
+            .cull_mode = SG_CULLMODE_BACK,
+            .sample_count = SAMPLE_COUNT
+        }
+    });
+}
+
+void frame(void) {
+    /* compute model-view-projection matrix for vertex shader */
+    hmm_mat4 proj = HMM_Perspective(60.0f, (float)sapp_width()/(float)sapp_height(), 0.01f, 10.0f);
+    hmm_mat4 view = HMM_LookAt(HMM_Vec3(0.0f, 1.5f, 6.0f), HMM_Vec3(0.0f, 0.0f, 0.0f), HMM_Vec3(0.0f, 1.0f, 0.0f));
+    hmm_mat4 view_proj = HMM_MultiplyMat4(proj, view);
+    vs_params_t vs_params;
+    rx += 1.0f; ry += 2.0f;
+    hmm_mat4 rxm = HMM_Rotate(rx, HMM_Vec3(1.0f, 0.0f, 0.0f));
+    hmm_mat4 rym = HMM_Rotate(ry, HMM_Vec3(0.0f, 1.0f, 0.0f));
+    hmm_mat4 model = HMM_MultiplyMat4(rxm, rym);
+    vs_params.mvp = HMM_MultiplyMat4(view_proj, model);
+
+    sg_begin_default_pass(&pass_action, sapp_width(), sapp_height());
+    sg_apply_draw_state(&draw_state);
+    sg_apply_uniform_block(SG_SHADERSTAGE_VS, 0, &vs_params, sizeof(vs_params));
+    sg_draw(0, 36, 1);
+    sg_end_pass();
+    sg_commit();
+}
+
+void cleanup(void) {
+    sg_shutdown();
+}
+
+sapp_desc sokol_main(int argc, char* argv[]) {
+    return (sapp_desc){
+        .init_cb = init,
+        .frame_cb = frame,
+        .cleanup_cb = cleanup,
+        .width = 800,
+        .height = 600,
+        .sample_count = SAMPLE_COUNT,
+        .window_title = "Textured Cube (sokol-app)",
+    };
+}
+
+#if defined(SOKOL_GLCORE33)
+const char* vs_src =
+    "#version 330\n"
+    "uniform mat4 mvp;\n"
+    "in vec4 position;\n"
+    "in vec4 color0;\n"
+    "in vec2 texcoord0;\n"
+    "out vec4 color;\n"
+    "out vec2 uv;"
+    "void main() {\n"
+    "  gl_Position = mvp * position;\n"
+    "  color = color0;\n"
+    "  uv = texcoord0 * 5.0;\n"
+    "}\n";
+const char* fs_src =
+    "#version 330\n"
+    "uniform sampler2D tex;"
+    "in vec4 color;\n"
+    "in vec2 uv;\n"
+    "out vec4 frag_color;\n"
+    "void main() {\n"
+    "  frag_color = texture(tex, uv) * color;\n"
+    "}\n";
+#elif defined(SOKOL_GLES2)
+const char* vs_src =
+    "uniform mat4 mvp;\n"
+    "attribute vec4 position;\n"
+    "attribute vec4 color0;\n"
+    "attribute vec2 texcoord0;\n"
+    "varying vec4 color;\n"
+    "varying vec2 uv;"
+    "void main() {\n"
+    "  gl_Position = mvp * position;\n"
+    "  color = color0;\n"
+    "  uv = texcoord0 * 5.0;\n"
+    "}\n";
+const char* fs_src =
+    "precision mediump float;\n"
+    "uniform sampler2D tex;\n"
+    "varying vec4 color;\n"
+    "varying vec2 uv;\n"
+    "void main() {\n"
+    "  gl_FragColor = texture2D(tex, uv) * color;\n"
+    "}\n";
+#elif defined(SOKOL_GLES3)
+const char* vs_src =
+    "#version 300 es\n"
+    "uniform mat4 mvp;\n"
+    "in vec4 position;\n"
+    "in vec4 color0;\n"
+    "in vec2 texcoord0;\n"
+    "out vec4 color;\n"
+    "out vec2 uv;"
+    "void main() {\n"
+    "  gl_Position = mvp * position;\n"
+    "  color = color0;\n"
+    "  uv = texcoord0 * 5.0;\n"
+    "}\n";
+const char* fs_src =
+    "#version 300 es\n"
+    "precision mediump float;"
+    "uniform sampler2D tex;"
+    "in vec4 color;\n"
+    "in vec2 uv;\n"
+    "out vec4 frag_color;\n"
+    "void main() {\n"
+    "  frag_color = texture(tex, uv) * color;\n"
+    "}\n";
+#elif defined(SOKOL_METAL)
+const char* vs_src =
+    "#include <metal_stdlib>\n"
+    "using namespace metal;\n"
+    "struct params_t {\n"
+    "  float4x4 mvp;\n"
+    "};\n"
+    "struct vs_in {\n"
+    "  float4 position [[attribute(0)]];\n"
+    "  float4 color [[attribute(1)]];\n"
+    "  float2 uv [[attribute(2)]];\n"
+    "};\n"
+    "struct vs_out {\n"
+    "  float4 pos [[position]];\n"
+    "  float4 color;\n"
+    "  float2 uv;\n"
+    "};\n"
+    "vertex vs_out _main(vs_in in [[stage_in]], constant params_t& params [[buffer(0)]]) {\n"
+    "  vs_out out;\n"
+    "  out.pos = params.mvp * in.position;\n"
+    "  out.color = in.color;\n"
+    "  out.uv = in.uv * 5.0;\n"
+    "  return out;\n"
+    "}\n";
+const char* fs_src =
+    "#include <metal_stdlib>\n"
+    "using namespace metal;\n"
+    "struct fs_in {\n"
+    "  float4 color;\n"
+    "  float2 uv;\n"
+    "};\n"
+    "fragment float4 _main(fs_in in [[stage_in]],\n"
+    "  texture2d<float> tex [[texture(0)]],\n"
+    "  sampler smp [[sampler(0)]])\n"
+    "{\n"
+    "  return float4(tex.sample(smp, in.uv).xyz, 1.0) * in.color;\n"
+    "}\n";
+#elif defined(SOKOL_D3D11)
+const char* vs_src =
+    "cbuffer params: register(b0) {\n"
+    "  float4x4 mvp;\n"
+    "};\n"
+    "struct vs_in {\n"
+    "  float4 pos: POSITION;\n"
+    "  float4 color: COLOR1;\n"
+    "  float2 uv: TEXCOORD1;\n"
+    "};\n"
+    "struct vs_out {\n"
+    "  float4 color: COLOR0;\n"
+    "  float2 uv: TEXCOORD0;\n"
+    "  float4 pos: SV_Position;\n"
+    "};\n"
+    "vs_out main(vs_in inp) {\n"
+    "  vs_out outp;\n"
+    "  outp.pos = mul(mvp, inp.pos);\n"
+    "  outp.color = inp.color;\n"
+    "  outp.uv = inp.uv * 5.0;\n"
+    "  return outp;\n"
+    "};\n";
+const char* fs_src =
+    "Texture2D<float4> tex: register(t0);\n"
+    "sampler smp: register(s0);\n"
+    "float4 main(float4 color: COLOR0, float2 uv: TEXCOORD0): SV_Target0 {\n"
+    "  return tex.Sample(smp, uv) * color;\n"
+    "}\n";
+#endif
