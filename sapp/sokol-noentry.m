239e537b8bb3c1f46f54debe372895eed71d21eb
Andre Weissflog <floooh@gmail.com>
2019-02-18 19:28:34 +0100

    add a SOKOL_NO_ENTRY sample for sokol_app.h

--- sapp/sokol-noentry.m
+++ sapp/sokol-noentry.m
@@ -1,9 +1,5 @@
 #define SOKOL_IMPL
-#define SOKOL_D3D11_SHADER_COMPILER
-#if defined(_WIN32)
-#include <Windows.h>
-#define SOKOL_LOG(s) OutputDebugStringA(s)
-#endif
+#define SOKOL_NO_ENTRY
 /* sokol 3D-API defines are provided by build options */
 #include "sokol_app.h"
 #include "sokol_gfx.h"
