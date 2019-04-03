bf13c93735dc7c1fa667e74abcf5572978025fdb
Andre Weissflog <floooh@gmail.com>
2019-04-03 18:59:45 +0200

    more cubemaprt stuff

--- sapp/cubemaprt-sapp.c
+++ sapp/cubemaprt-sapp.c
@@ -14,6 +14,7 @@
 #define OFFSCREEN_SAMPLE_COUNT (1)
 #define NUM_SHAPES (32)
 
+/* state struct for the little cubes rotating around the big cube */
 typedef struct {
     hmm_mat4 model;
     hmm_vec4 color;
@@ -23,11 +24,13 @@ typedef struct {
     float angular_velocity;
 } shape_t;
 
+/* vertex (normals for simple point lighting) */
 typedef struct {
     float pos[3];
     float norm[3];
 } vertex_t;
 
+/* shader-uniform block */
 typedef struct {
     hmm_mat4 mvp;
     hmm_mat4 model;
@@ -36,27 +39,46 @@ typedef struct {
     hmm_vec4 eye_pos;
 } shape_uniforms_t;
 
+/* a mesh consists of a vertex- and index-buffer */
 typedef struct {
     sg_buffer vbuf;
     sg_buffer ibuf;
+    int num_elements;
 } mesh_t;
 
+/* the entire application state */
 typedef struct {
     sg_image cubemap;
     sg_pass offscreen_pass[SG_CUBEFACE_NUM];
     sg_pass_action offscreen_pass_action;
     sg_pass_action display_pass_action;
     mesh_t cube;
-    sg_pipeline offscreen_pip;
+    sg_pipeline offscreen_shapes_pip;
+    sg_pipeline display_shapes_pip;
     sg_pipeline display_pip;
     hmm_mat4 offscreen_proj;
+    hmm_vec4 light_dir;
+    float rx, ry;
     shape_t shapes[NUM_SHAPES];
 } app_t;
 static app_t app;
 
 static const char* vs_src, *offscreen_fs_src, *display_fs_src;
 
+static void draw_cubes(sg_pipeline pip, hmm_vec3 eye_pos, hmm_mat4 view_proj);
 static mesh_t make_cube_mesh(void);
+
+static inline uint32_t xorshift32(void) {
+    static uint32_t x = 0x12345678;
+    x ^= x<<13;
+    x ^= x>>17;
+    x ^= x<<5;
+    return x;
+}
+
+static inline float rnd(float min_val, float max_val) {
+    return ((((float)(xorshift32() & 0xFFFF)) / 0x10000) * (max_val - min_val)) + min_val;
+}
 
 void init(void) {
     sg_setup(&(sg_desc){
@@ -105,11 +127,21 @@ void init(void) {
         });
     }
 
+    /* pass action for offscreen pass (clear to black) */
+    app.offscreen_pass_action = (sg_pass_action) {
+        .colors[0] = { .action = SG_ACTION_CLEAR, .val = { 0.5f, 0.5f, 0.5f, 1.0f } }
+    };
+    /* pass action for default pass (clear to grey) */
+    app.display_pass_action = (sg_pass_action) {
+        .colors[0] = { .action = SG_ACTION_CLEAR, .val = { 0.75f, 0.75f, 0.75f, 1.0f } }
+    };
+
     /* vertex- and index-buffers for cube  */
     app.cube = make_cube_mesh();
 
     /* shader and pipeline objects for offscreen-rendering */
     sg_shader_uniform_block_desc ub_desc = {
+        .size = sizeof(shape_uniforms_t),
         .uniforms = {
             [0] = { .name="mvp", .type=SG_UNIFORMTYPE_MAT4 },
             [1] = { .name="model", .type=SG_UNIFORMTYPE_MAT4 },
@@ -132,7 +164,7 @@ void init(void) {
         .fs.source = offscreen_fs_src,
         .label = "offscreen-shader"
     });
-    app.offscreen_pip = sg_make_pipeline(&(sg_pipeline_desc){
+    sg_pipeline_desc pip_desc = {
         .shader = offscreen_shd,
         .layout = layout_desc,
         .index_type = SG_INDEXTYPE_UINT16,
@@ -147,8 +179,13 @@ void init(void) {
             .cull_mode = SG_CULLMODE_BACK,
             .sample_count = OFFSCREEN_SAMPLE_COUNT
         },
-        .label = "offscreen-pipeline"
-    });
+        .label = "offscreen-shapes-pipeline"
+    };
+    app.offscreen_shapes_pip = sg_make_pipeline(&pip_desc);
+    pip_desc.rasterizer.sample_count = DISPLAY_SAMPLE_COUNT;
+    pip_desc.blend.depth_format = 0;
+    pip_desc.label = "display-shapes-pipeline";
+    app.display_shapes_pip = sg_make_pipeline(&pip_desc);
 
     /* shader and pipeline objects for display-rendering */
     sg_shader display_shd = sg_make_shader(&(sg_shader_desc){
@@ -180,10 +217,80 @@ void init(void) {
     
     /* 1:1 aspect ration projection matrix for offscreen rendering */
     app.offscreen_proj = HMM_Perspective(90.0f, 1.0f, 0.01f, 100.0f);
+    app.light_dir = HMM_Vec4v(HMM_NormalizeVec3(HMM_Vec3(-0.75f, 1.0f, 0.0f)), 0.0f);
+
+    /* setup initial state for the orbiting cubes */
+    for (int i = 0; i < NUM_SHAPES; i++) {
+        app.shapes[i].color = HMM_Vec4(rnd(0.0f, 1.0f), rnd(0.0f, 1.0f), rnd(0.0f, 1.0f), 1.0f);
+        app.shapes[i].axis = HMM_NormalizeVec3(HMM_Vec3(rnd(-1.0f, 1.0f), rnd(-1.0f, 1.0f), rnd(-1.0f, 1.0f)));
+        app.shapes[i].radius = rnd(5.0f, 10.0f);
+        app.shapes[i].angle = rnd(0.0f, 360.0f);
+        app.shapes[i].angular_velocity = rnd(15.0f, 50.0f) * (rnd(-1.0f, 1.0f)>0.0f ? 1.0f : -1.0f);
+    }
 }
 
 void frame(void) {
+
+    /* update the little cubes that are reflected in the big cube */
+    for (int i = 0; i < NUM_SHAPES; i++) {
+//        app.shapes[i].angle += app.shapes[i].angular_velocity * (1.0f/60.0f);
+        hmm_mat4 scale = HMM_Scale(HMM_Vec3(0.25f, 0.25f, 0.25f));
+        hmm_mat4 rot = HMM_Rotate(app.shapes[i].angle, app.shapes[i].axis);
+        hmm_mat4 trans = HMM_Translate(HMM_Vec3(0.0f, 0.0f, app.shapes[i].radius));
+        app.shapes[i].model = HMM_MultiplyMat4(rot, HMM_MultiplyMat4(trans, scale));
+    }
+
+    /* offscreen pass which renders the environment cubemap */
+    hmm_vec3 center_and_up[SG_CUBEFACE_NUM][2] = {
+        { { .X=+1.0f, .Y= 0.0f, .Z= 0.0f }, { .X=0.0f, .Y=-1.0f, .Z= 0.0f } },
+        { { .X=-1.0f, .Y= 0.0f, .Z= 0.0f }, { .X=0.0f, .Y=-1.0f, .Z= 0.0f } },
+        { { .X= 0.0f, .Y=+1.0f, .Z= 0.0f }, { .X=0.0f, .Y= 0.0f, .Z= 1.0f } },
+        { { .X= 0.0f, .Y=-1.0f, .Z= 0.0f }, { .X=0.0f, .Y= 0.0f, .Z=-1.0f } },
+        { { .X= 0.0f, .Y= 0.0f, .Z=+1.0f }, { .X=0.0f, .Y=-1.0f, .Z= 0.0f } },
+        { { .X= 0.0f, .Y= 0.0f, .Z=-1.0f }, { .X=0.0f, .Y=-1.0f, .Z= 0.0f } }
+    };
+    for (int face = 0; face < SG_CUBEFACE_NUM; face++) {
+        sg_begin_pass(app.offscreen_pass[face], &app.offscreen_pass_action);
+        hmm_mat4 view = HMM_LookAt(HMM_Vec3(0.0f, 0.0f, 0.0f), center_and_up[face][0], center_and_up[face][1]);
+        hmm_mat4 view_proj = HMM_MultiplyMat4(app.offscreen_proj, view);
+        draw_cubes(app.offscreen_shapes_pip, HMM_Vec3(0.0f, 0.0f, 0.0f), view_proj);
+        sg_end_pass();
+    }
+
+    /* render the default pass */
+    const int w = sapp_width();
+    const int h = sapp_height();
     sg_begin_default_pass(&app.display_pass_action, sapp_width(), sapp_height());
+
+    hmm_vec3 eye_pos = HMM_Vec3(-30.0f, 0.0f, 0.0f);
+    hmm_mat4 proj = HMM_Perspective(45.0f, (float)w/(float)h, 0.01f, 100.0f);
+    hmm_mat4 view = HMM_LookAt(eye_pos, HMM_Vec3(0.0f, 0.0f, 0.0f), HMM_Vec3(0.0f, 1.0f, 0.0f));
+    hmm_mat4 view_proj = HMM_MultiplyMat4(proj, view);
+
+    /* render the orbiting cubes */
+    draw_cubes(app.display_shapes_pip, eye_pos, view_proj);
+
+    /* render a big cube in the middle with environment mapping */
+//    app.rx += 0.1f; app.ry += 0.2f;
+    hmm_mat4 rxm = HMM_Rotate(app.rx, HMM_Vec3(1.0f, 0.0f, 0.0f));
+    hmm_mat4 rym = HMM_Rotate(app.ry, HMM_Vec3(0.0f, 1.0f, 0.0f));
+    hmm_mat4 model = HMM_MultiplyMat4(HMM_MultiplyMat4(rxm, rym), HMM_Scale(HMM_Vec3(2.0f, 2.0f, 2.f)));
+    sg_apply_pipeline(app.display_pip);
+    sg_apply_bindings(&(sg_bindings){
+        .vertex_buffers[0] = app.cube.vbuf,
+        .index_buffer = app.cube.ibuf,
+        .fs_images[0] = app.cubemap
+    });
+    shape_uniforms_t uniforms = {
+        .mvp = HMM_MultiplyMat4(view_proj, model),
+        .model = model,
+        .shape_color = HMM_Vec4(1.0f, 1.0f, 1.0f, 1.0f),
+        .light_dir = app.light_dir,
+        .eye_pos = HMM_Vec4v(eye_pos, 1.0f)
+    };
+    sg_apply_uniforms(SG_SHADERSTAGE_VS, 0, &uniforms, sizeof(uniforms));
+    sg_draw(0, app.cube.num_elements, 1);
+
     __dbgui_draw();
     sg_end_pass();
     sg_commit();
@@ -204,8 +311,28 @@ sapp_desc sokol_main(int argc, char* argv[]) {
         .height = 600,
         .sample_count = DISPLAY_SAMPLE_COUNT,
         .gl_force_gles2 = true,
-        .window_title = "CubemapRenderTarget (sokol-app)",
+        .window_title = "Cubemap Render Target (sokol-app)",
     };
+}
+
+static void draw_cubes(sg_pipeline pip, hmm_vec3 eye_pos, hmm_mat4 view_proj) {
+    sg_apply_pipeline(pip);
+    sg_apply_bindings(&(sg_bindings){
+        .vertex_buffers[0] = app.cube.vbuf,
+        .index_buffer = app.cube.ibuf
+    });
+    for (int i = 0; i < NUM_SHAPES; i++) {
+        const shape_t* shape = &app.shapes[i];
+        shape_uniforms_t uniforms = {
+            .mvp = HMM_MultiplyMat4(view_proj, shape->model),
+            .model = shape->model,
+            .shape_color = shape->color,
+            .light_dir = app.light_dir,
+            .eye_pos = HMM_Vec4v(eye_pos, 1.0f)
+        };
+        sg_apply_uniforms(SG_SHADERSTAGE_VS, 0, &uniforms, sizeof(uniforms));
+        sg_draw(0, app.cube.num_elements, 1);
+    }
 }
 
 static mesh_t make_cube_mesh(void) {
@@ -259,7 +386,8 @@ static mesh_t make_cube_mesh(void) {
             .size = sizeof(indices),
             .content = indices,
             .label = "cube-indices"
-        })
+        }),
+        .num_elements = sizeof(indices) / sizeof(uint16_t)
     };
     return mesh;
 }
@@ -329,7 +457,37 @@ static const char* offscreen_fs_src =
 "  float3 light_dir = normalize(in.world_light_dir);\n"
 "  float3 frag_color = light(in.color.xyz, eye_vec, nrm, light_dir);\n"
 "  return float4(frag_color, 1.0f);\n"
-"};\n";
+"}\n";
+static const char* display_fs_src =
+"#include <metal_stdlib>\n"
+"#include <simd/simd.h>\n"
+"using namespace metal;\n"
+"struct fs_in {\n"
+"  float3 world_position [[user(locn0)]];\n"
+"  float3 world_normal [[user(locn1)]];\n"
+"  float3 world_light_dir [[user(locn2)]];\n"
+"  float3 world_eye_pos [[user(locn3)]];\n"
+"  float4 color [[user(locn4)]];\n"
+"};\n"
+"float3 light(float3 base_color, float3 eye_vec, float3 normal, float3 light_vec) {\n"
+"  float ambient = 0.25f;\n"
+"  float n_dot_l = max(dot(normal, light_vec), 0.0);\n"
+"  float diff = n_dot_l + ambient;\n"
+"  float spec_power = 16.0;\n"
+"  float3 r = reflect(-light_vec, normal);\n"
+"  float r_dot_v = max(dot(r, eye_vec), 0.0);\n"
+"  float spec = pow(r_dot_v, spec_power) * n_dot_l;\n"
+"  return (base_color * (diff + ambient)) + float3(spec, spec, spec);\n"
+"}\n"
+"fragment float4 _main(fs_in in [[stage_in]], texturecube<float> tex [[texture(0)]], sampler smp [[sampler(0)]]) {\n"
+"  float3 eye_vec = normalize(in.world_eye_pos - in.world_position);\n"
+"  float3 nrm = normalize(in.world_normal);\n"
+"  float3 refl_vec = normalize(in.world_position);\n"
+"  float4 refl_color = tex.sample(smp, refl_vec);\n"
+"  float3 light_dir = normalize(in.world_light_dir);\n"
+"  float3 frag_color = light(in.color.xyz * refl_color.xyz, eye_vec, nrm, light_dir);\n"
+"  return float4(frag_color, 1.0f);\n"
+"}\n";
 #elif defined(SOKOL_D3D11)
 #error "FIXME!"
 #endif
