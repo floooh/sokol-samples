aa830c1aa51f4ef433dee5c6f3c12c83e41a3c9b
Andre Weissflog <floooh@gmail.com>
2019-04-23 15:48:06 +0200

    start moving sapp-samples to shader code-generation

---
+++ sapp/triangle-sapp.glsl
@@ -0,0 +1,21 @@
+@vs vs
+layout(location=0) in vec4 position;
+layout(location=1) in vec4 color0;
+layout(location=0) out vec4 color;
+
+void main() {
+    gl_Position = position;
+    color = color0;
+}
+@end
+
+@fs fs
+layout(location=0) in vec4 color;
+layout(location=0) out vec4 frag_color;
+
+void main() {
+    frag_color = color;
+}
+@end
+
+@program triangle vs fs
