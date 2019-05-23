5e98ff8b2498481a15e79d064fbddb3c8da167c0
Andre Weissflog <floooh@gmail.com>
2019-05-23 15:44:46 +0200

    texcube-sapp: demonstrate use of packed integer vertex components

--- sapp/texcube-sapp.c
+++ sapp/texcube-sapp.c
@@ -1,6 +1,6 @@
 //------------------------------------------------------------------------------
 //  texcube-sapp.c
-//  Texture creation and rendering.
+//  Texture creation, rendering with texture, packed vertex components.
 //------------------------------------------------------------------------------
 #include "sokol_app.h"
 #include "sokol_gfx.h"
@@ -18,6 +18,12 @@ static sg_pass_action pass_action = {
 static sg_pipeline pip;
 static sg_bindings bind;
 
+typedef struct {
+    float x, y, z;
+    uint32_t color;
+    int16_t u, v;
+} vertex_t;
+
 void init(void) {
     sg_setup(&(sg_desc){
         .gl_force_gles2 = sapp_gles2(),
@@ -31,38 +37,48 @@ void init(void) {
     });
     __dbgui_setup(SAMPLE_COUNT);
 
-    /* cube vertex buffer */
-    float vertices[] = {
+    /*
+        Cube vertex buffer with packed vertex formats for color and texture coords.
+        Note that a vertex format which must be portable across all 
+        backends must only use the normalized integer formats
+        (BYTE4N, UBYTE4N, SHORT2N, SHORT4N), which can be converted
+        to floating point formats in the vertex shader inputs.
+
+        The reason is that D3D11 cannot convert from non-normalized
+        formats to floating point inputs (only to integer inputs),
+        and WebGL2 / GLES2 don't support integer vertex shader inputs.
+    */
+    vertex_t vertices[] = {
         /* pos                  color                       uvs */
-        -1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f,     0.0f, 0.0f,
-         1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f,     1.0f, 0.0f,
-         1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f,     1.0f, 1.0f,
-        -1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.0f, 1.0f,     0.0f, 1.0f,
+        { -1.0f, -1.0f, -1.0f,  0xFF0000FF,     0,     0 },
+        {  1.0f, -1.0f, -1.0f,  0xFF0000FF, 32767,     0 },
+        {  1.0f,  1.0f, -1.0f,  0xFF0000FF, 32767, 32767 },
+        { -1.0f,  1.0f, -1.0f,  0xFF0000FF,     0, 32767 },
 
-        -1.0f, -1.0f,  1.0f,    0.0f, 1.0f, 0.0f, 1.0f,     0.0f, 0.0f, 
-         1.0f, -1.0f,  1.0f,    0.0f, 1.0f, 0.0f, 1.0f,     1.0f, 0.0f,
-         1.0f,  1.0f,  1.0f,    0.0f, 1.0f, 0.0f, 1.0f,     1.0f, 1.0f,
-        -1.0f,  1.0f,  1.0f,    0.0f, 1.0f, 0.0f, 1.0f,     0.0f, 1.0f,
+        { -1.0f, -1.0f,  1.0f,  0xFF00FF00,     0,     0 }, 
+        {  1.0f, -1.0f,  1.0f,  0xFF00FF00, 32767,     0 },
+        {  1.0f,  1.0f,  1.0f,  0xFF00FF00, 32767, 32767 },
+        { -1.0f,  1.0f,  1.0f,  0xFF00FF00,     0, 32767 },
 
-        -1.0f, -1.0f, -1.0f,    0.0f, 0.0f, 1.0f, 1.0f,     0.0f, 0.0f,
-        -1.0f,  1.0f, -1.0f,    0.0f, 0.0f, 1.0f, 1.0f,     1.0f, 0.0f,
-        -1.0f,  1.0f,  1.0f,    0.0f, 0.0f, 1.0f, 1.0f,     1.0f, 1.0f,
-        -1.0f, -1.0f,  1.0f,    0.0f, 0.0f, 1.0f, 1.0f,     0.0f, 1.0f,
+        { -1.0f, -1.0f, -1.0f,  0xFFFF0000,     0,     0 },
+        { -1.0f,  1.0f, -1.0f,  0xFFFF0000, 32767,     0 },
+        { -1.0f,  1.0f,  1.0f,  0xFFFF0000, 32767, 32767 },
+        { -1.0f, -1.0f,  1.0f,  0xFFFF0000,     0, 32767 },
 
-         1.0f, -1.0f, -1.0f,    1.0f, 0.5f, 0.0f, 1.0f,     0.0f, 0.0f,
-         1.0f,  1.0f, -1.0f,    1.0f, 0.5f, 0.0f, 1.0f,     1.0f, 0.0f,
-         1.0f,  1.0f,  1.0f,    1.0f, 0.5f, 0.0f, 1.0f,     1.0f, 1.0f,
-         1.0f, -1.0f,  1.0f,    1.0f, 0.5f, 0.0f, 1.0f,     0.0f, 1.0f,
+        {  1.0f, -1.0f, -1.0f,  0xFFFF007F,     0,     0 },
+        {  1.0f,  1.0f, -1.0f,  0xFFFF007F, 32767,     0 },
+        {  1.0f,  1.0f,  1.0f,  0xFFFF007F, 32767, 32767 },
+        {  1.0f, -1.0f,  1.0f,  0xFFFF007F,     0, 32767 },
 
-        -1.0f, -1.0f, -1.0f,    0.0f, 0.5f, 1.0f, 1.0f,     0.0f, 0.0f,
-        -1.0f, -1.0f,  1.0f,    0.0f, 0.5f, 1.0f, 1.0f,     1.0f, 0.0f,
-         1.0f, -1.0f,  1.0f,    0.0f, 0.5f, 1.0f, 1.0f,     1.0f, 1.0f,
-         1.0f, -1.0f, -1.0f,    0.0f, 0.5f, 1.0f, 1.0f,     0.0f, 1.0f,
+        { -1.0f, -1.0f, -1.0f,  0xFFFF7F00,     0,     0 },
+        { -1.0f, -1.0f,  1.0f,  0xFFFF7F00, 32767,     0 },
+        {  1.0f, -1.0f,  1.0f,  0xFFFF7F00, 32767, 32767 },
+        {  1.0f, -1.0f, -1.0f,  0xFFFF7F00,     0, 32767 },
 
-        -1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.5f, 1.0f,     0.0f, 0.0f,
-        -1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.5f, 1.0f,     1.0f, 0.0f,
-         1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.5f, 1.0f,     1.0f, 1.0f,
-         1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.5f, 1.0f,     0.0f, 1.0f
+        { -1.0f,  1.0f, -1.0f,  0xFF007FFF,     0,     0 },
+        { -1.0f,  1.0f,  1.0f,  0xFF007FFF, 32767,     0 },
+        {  1.0f,  1.0f,  1.0f,  0xFF007FFF, 32767, 32767 },
+        {  1.0f,  1.0f, -1.0f,  0xFF007FFF,     0, 32767 },
     };
     bind.vertex_buffers[0] = sg_make_buffer(&(sg_buffer_desc){
         .size = sizeof(vertices),
@@ -112,8 +128,8 @@ void init(void) {
         .layout = {
             .attrs = {
                 [ATTR_vs_pos].format = SG_VERTEXFORMAT_FLOAT3,
-                [ATTR_vs_color0].format = SG_VERTEXFORMAT_FLOAT4,
-                [ATTR_vs_texcoord0].format = SG_VERTEXFORMAT_FLOAT2
+                [ATTR_vs_color0].format = SG_VERTEXFORMAT_UBYTE4N,
+                [ATTR_vs_texcoord0].format = SG_VERTEXFORMAT_SHORT2N
             }
         },
         .shader = shd,
