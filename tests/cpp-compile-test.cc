d1da1a38344c0f8055af758afc8980e7aef08e76
Andre Weissflog <floooh@gmail.com>
2019-05-24 18:26:01 +0200

    fix the cpp-compile-test build

--- tests/cpp-compile-test.cc
+++ tests/cpp-compile-test.cc
@@ -2,9 +2,6 @@
 //  cpp-compile-test.cc
 //  Just check whether (most of) the sokol headers also comppile as C++.
 //------------------------------------------------------------------------------
-#if defined(__APPLE__)
-    #error "This is for non-apple platforms only"
-#endif
 #define SOKOL_IMPL
 #define SOKOL_WIN32_FORCE_MAIN
 #include "sokol_app.h"
