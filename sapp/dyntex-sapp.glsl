7c88d15e5908656996a678ebe2f0f71b259c17f7
Andre Weissflog <floooh@gmail.com>
2019-06-11 21:08:36 +0200

    undo the dyntex-sapp.glsl fuckup

--- sapp/dyntex-sapp.glsl
+++ sapp/dyntex-sapp.glsl
@@ -23,26 +23,13 @@ void main() {
 @end
 
 @fs fs
-uniform sampler2D tex_y;
-uniform sampler2D tex_cb;
-uniform sampler2D tex_cr;
+uniform sampler2D tex;
 layout(location=0) in vec4 color;
 layout(location=1) in vec2 uv;
-
 out vec4 frag_color;
 
-mat4 rec601 = mat4(
-    1.16438,  0.00000,  1.59603, -0.87079,
-    1.16438, -0.39176, -0.81297,  0.52959,
-    1.16438,  2.01723,  0.00000, -1.08139,
-    0, 0, 0, 1
-);
-
 void main() {
-    float y = texture(tex_y, uv).r;
-    float cb = texture(tex_cb, uv).r;
-    float cr = texture(tex_cr, uv).r;
-    frag_color = vec4(y, cb, cr, 1.0) * rec601;
+    frag_color = texture(tex, uv) * color;
 }
 @end
 
