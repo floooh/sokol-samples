25b541c79681325d16abb79939412f68a7705db7
Andre Weissflog <floooh@gmail.com>
2019-06-21 20:02:32 +0200

    more sokol-fetch-test impl

--- tests/sokol-fetch-test.c
+++ tests/sokol-fetch-test.c
@@ -58,7 +58,7 @@ UTEST(sokol_fetch, item_init_discard) {
         .user_data = &user_data,
         .user_data_size = sizeof(user_data)
     };
-    _sfetch_item_t item = { };
+    _sfetch_item_t item = {0};
     uint32_t slot_id = _sfetch_make_id(1, 1);
     _sfetch_item_init(&item, slot_id, &request);
     T(item.handle.id == slot_id);
@@ -86,7 +86,7 @@ UTEST(sokol_fetch, item_init_path_overflow) {
     sfetch_request_t request = {
         .path = "012345678901234567890123456789012",
     };
-    _sfetch_item_t item = { };
+    _sfetch_item_t item = {0};
     _sfetch_item_init(&item, _sfetch_make_id(1, 1), &request);
     T(item.path.buf[0] == 0);
 }
@@ -98,14 +98,14 @@ UTEST(sokol_fetch, item_init_userdata_overflow) {
         .user_data = big_data,
         .user_data_size = sizeof(big_data)
     };
-    _sfetch_item_t item = { };
+    _sfetch_item_t item = {0};
     _sfetch_item_init(&item, _sfetch_make_id(1, 1), &request);
     T(item.user.user_data_size == 0);
     T(item.user.user_data[0] == 0);
 }
 
 UTEST(sokol_fetch, pool_init_discard) {
-    _sfetch_pool_t pool = { };
+    _sfetch_pool_t pool = {0};
     const uint32_t num_items = 127;
     T(_sfetch_pool_init(&pool, num_items));
     T(pool.valid);
@@ -122,7 +122,7 @@ UTEST(sokol_fetch, pool_init_discard) {
 
 UTEST(sokol_fetch, pool_alloc_free) {
     uint8_t buf[32];
-    _sfetch_pool_t pool = { };
+    _sfetch_pool_t pool = {0};
     const uint32_t num_items = 16;
     _sfetch_pool_init(&pool, num_items);
     uint32_t slot_id = _sfetch_pool_item_alloc(&pool, &(sfetch_request_t){
@@ -150,7 +150,7 @@ UTEST(sokol_fetch, pool_alloc_free) {
 }
 
 UTEST(sokol_fetch, pool_overflow) {
-    _sfetch_pool_t pool = { };
+    _sfetch_pool_t pool = {0};
     _sfetch_pool_init(&pool, 4);
     uint32_t id0 = _sfetch_pool_item_alloc(&pool, &(sfetch_request_t){ .path="path0" });
     uint32_t id1 = _sfetch_pool_item_alloc(&pool, &(sfetch_request_t){ .path="path1" });
@@ -177,7 +177,7 @@ UTEST(sokol_fetch, pool_overflow) {
 }
 
 UTEST(sokol_fetch, lookup_item) {
-    _sfetch_pool_t pool = { };
+    _sfetch_pool_t pool = {0};
     _sfetch_pool_init(&pool, 4);
     uint32_t id0 = _sfetch_pool_item_alloc(&pool, &(sfetch_request_t){ .path="path0" });
     uint32_t id1 = _sfetch_pool_item_alloc(&pool, &(sfetch_request_t){ .path="path1" });
@@ -194,7 +194,7 @@ UTEST(sokol_fetch, lookup_item) {
 }
 
 UTEST(sokol_fetch, ring_init_discard) {
-    _sfetch_ring_t ring = { };
+    _sfetch_ring_t ring = {0};
     const uint32_t num_slots = 4;
     T(_sfetch_ring_init(&ring, num_slots));
     T(ring.head == 0);
@@ -209,7 +209,7 @@ UTEST(sokol_fetch, ring_init_discard) {
 }
 
 UTEST(sokol_fetch, ring_enqueue_dequeue) {
-    _sfetch_ring_t ring = { };
+    _sfetch_ring_t ring = {0};
     const uint32_t num_slots = 4;
     _sfetch_ring_init(&ring, num_slots);
     T(_sfetch_ring_count(&ring) == 0);
@@ -240,7 +240,7 @@ UTEST(sokol_fetch, ring_enqueue_dequeue) {
 }
 
 UTEST(sokol_fetch, ring_wrap_around) {
-    _sfetch_ring_t ring = { };
+    _sfetch_ring_t ring = {0};
     _sfetch_ring_init(&ring, 4);
     uint32_t i = 0;
     for (i = 0; i < 4; i++) {
@@ -262,7 +262,7 @@ UTEST(sokol_fetch, ring_wrap_around) {
 }
 
 UTEST(sokol_fetch, ring_wrap_count) {
-    _sfetch_ring_t ring = { };
+    _sfetch_ring_t ring = {0};
     _sfetch_ring_init(&ring, 8);
     // add and remove 4 items to move tail to the middle
     for (uint32_t i = 0; i < 4; i++) {
@@ -293,66 +293,24 @@ static void channel_worker(_sfetch_t* ctx, uint32_t slot_id) {
 
 UTEST(sokol_fetch, channel_init_discard) {
     num_processed_items = 0;
-    _sfetch_channel_t chn = { };
+    _sfetch_channel_t chn = {0};
     const int num_slots = 12;
     const int num_lanes = 64;
     _sfetch_channel_init(&chn, 0, num_slots, num_lanes, channel_worker);
     T(chn.valid);
-    T(chn.free_lanes.head == 0);
-    T(chn.free_lanes.tail == 0);
-    T(chn.free_lanes.num == (num_lanes+1));
-    T(chn.free_lanes.buf != 0);
-    T(chn.user_sent.head == 0);
-    T(chn.user_sent.tail == 0);
-    T(chn.user_sent.num == (num_slots+1));
-    T(chn.user_sent.buf != 0);
-    T(chn.user_incoming.head == 0);
-    T(chn.user_incoming.tail == 0);
-    T(chn.user_incoming.num == (num_lanes+1));
-    T(chn.user_incoming.buf != 0);
-    T(chn.thread_incoming.head == 0);
-    T(chn.thread_incoming.tail == 0);
-    T(chn.thread_incoming.num == (num_lanes+1));
-    T(chn.thread_incoming.buf != 0);
-    T(chn.thread_outgoing.head == 0);
-    T(chn.thread_outgoing.tail == 0);
-    T(chn.thread_outgoing.num == (num_lanes+1));
-    T(chn.thread_outgoing.buf != 0);
-    T(chn.user_outgoing.head == 0);
-    T(chn.user_outgoing.tail == 0);
-    T(chn.user_outgoing.num == (num_lanes+1));
-    T(chn.user_outgoing.buf != 0);
+    T(_sfetch_ring_full(&chn.free_lanes));
+    T(_sfetch_ring_empty(&chn.user_sent));
+    T(_sfetch_ring_empty(&chn.user_incoming));
+    T(_sfetch_ring_empty(&chn.thread_incoming));
+    T(_sfetch_ring_empty(&chn.thread_outgoing));
+    T(_sfetch_ring_empty(&chn.user_outgoing));
     _sfetch_channel_discard(&chn);
     T(!chn.valid);
-    T(chn.free_lanes.head == 0);
-    T(chn.free_lanes.tail == 0);
-    T(chn.free_lanes.num == 0);
-    T(chn.free_lanes.buf == 0);
-    T(chn.user_sent.head == 0);
-    T(chn.user_sent.tail == 0);
-    T(chn.user_sent.num == 0);
-    T(chn.user_sent.buf == 0);
-    T(chn.user_incoming.head == 0);
-    T(chn.user_incoming.tail == 0);
-    T(chn.user_incoming.num == 0);
-    T(chn.user_incoming.buf == 0);
-    T(chn.thread_incoming.head == 0);
-    T(chn.thread_incoming.tail == 0);
-    T(chn.thread_incoming.num == 0);
-    T(chn.thread_incoming.buf == 0);
-    T(chn.thread_outgoing.head == 0);
-    T(chn.thread_outgoing.tail == 0);
-    T(chn.thread_outgoing.num == 0);
-    T(chn.thread_outgoing.buf == 0);
-    T(chn.user_outgoing.head == 0);
-    T(chn.user_outgoing.tail == 0);
-    T(chn.user_outgoing.num == 0);
-    T(chn.user_outgoing.buf == 0);
 }
 
 /* public API functions */
 UTEST(sokol_fetch, setup_shutdown) {
-    sfetch_setup(&(sfetch_desc_t){ });
+    sfetch_setup(&(sfetch_desc_t){0});
     T(sfetch_valid());
     // check default values
     T(sfetch_desc().max_requests == 128);
@@ -392,7 +350,7 @@ static void fail_open_callback(sfetch_response_t response) {
 }
 
 UTEST(sokol_fetch, fail_open) {
-    sfetch_setup(&(sfetch_desc_t){ });
+    sfetch_setup(&(sfetch_desc_t){0});
     sfetch_handle_t h = sfetch_send(&(sfetch_request_t){
         .path = "non_existing_file.txt",
         .callback = fail_open_callback,
@@ -407,38 +365,40 @@ UTEST(sokol_fetch, fail_open) {
     sfetch_shutdown();
 }
 
-static bool load_file_passed;
+static bool load_file_fixed_buffer_passed;
 static uint8_t load_file_buf[500000];
+static const uint64_t combatsignal_file_size = 409482;
 
 // The file callback is called from the "current user thread" (the same
 // thread where the sfetch_send() for this request was called). Note that you
 // can call sfetch_setup/shutdown() on multiple threads, each thread will
 // get its own thread-local "sokol-fetch instance" and its own set of
 // IO-channel threads.
-static void load_file_callback(sfetch_response_t response) {
-    const uint64_t combatsignal_file_size = 409482;
+static void load_file_fixed_buffer_callback(sfetch_response_t response) {
     // when loading the whole file at once, the COMPLETED state
     // is the best place to grab/process the data
     if (response.state == SFETCH_STATE_FETCHED) {
         if ((response.content_size == combatsignal_file_size) &&
             (response.chunk_offset == 0) &&
             (response.chunk.ptr == load_file_buf) &&
-            (response.chunk.size == combatsignal_file_size))
+            (response.chunk.size == combatsignal_file_size) &&
+            response.finished)
         {
-            load_file_passed = true;
+            load_file_fixed_buffer_passed = true;
         }
     }
 }
 
 UTEST(sokol_fetch, load_file_fixed_buffer) {
-    sfetch_setup(&(sfetch_desc_t){ });
+    memset(load_file_buf, 0, sizeof(load_file_buf));
+    sfetch_setup(&(sfetch_desc_t){0});
     // send a load-request for a file where we know the max size upfront,
     // so we can provide a buffer right in the fetch request (otherwise
     // the buffer needs to be provided in the callback when the request
     // is in OPENED state, since only then the file size will be known).
     sfetch_handle_t h = sfetch_send(&(sfetch_request_t){
         .path = "comsi.s3m",
-        .callback = load_file_callback,
+        .callback = load_file_fixed_buffer_callback,
         .buffer = {
             .ptr = load_file_buf,
             .size = sizeof(load_file_buf)
@@ -452,6 +412,92 @@ UTEST(sokol_fetch, load_file_fixed_buffer) {
         sfetch_dowork();
         sleep_ms(1);
     }
-    T(load_file_passed);
+    T(load_file_fixed_buffer_passed);
+    sfetch_shutdown();
+}
+
+/* tests whether files with unknown size are processed correctly */
+static bool load_file_unknown_size_opened_passed;
+static bool load_file_unknown_size_fetched_passed;
+static void load_file_unknown_size_callback(sfetch_response_t response) {
+    if (response.state == SFETCH_STATE_OPENED) {
+        if ((response.content_size == combatsignal_file_size) &&
+            (response.chunk_offset == 0) &&
+            (response.chunk.ptr == 0) &&
+            (response.chunk.size == 0) &&
+            !response.finished)
+        {
+            load_file_unknown_size_opened_passed = true;
+            sfetch_set_buffer(response.handle, &(sfetch_buffer_t){
+                .ptr = load_file_buf,
+                .size = sizeof(load_file_buf)
+            });
+        }
+    }
+    else if (response.state == SFETCH_STATE_FETCHED) {
+        if ((response.content_size == combatsignal_file_size) &&
+            (response.chunk_offset == 0) &&
+            (response.chunk.ptr == load_file_buf) &&
+            (response.chunk.size == combatsignal_file_size) &&
+            response.finished)
+        {
+            load_file_unknown_size_fetched_passed = true;
+        }
+    }
+}
+
+UTEST(sokol_fetch, load_file_unknown_size) {
+    memset(load_file_buf, 0, sizeof(load_file_buf));
+    sfetch_setup(&(sfetch_desc_t){0});
+    sfetch_handle_t h = sfetch_send(&(sfetch_request_t){
+        .path = "comsi.s3m",
+        .callback = load_file_unknown_size_callback
+    });
+    int frame_count = 0;
+    while (sfetch_handle_valid(h) && (frame_count++ < 100)) {
+        sfetch_dowork();
+        sleep_ms(1);
+    }
+    T(load_file_unknown_size_opened_passed);
+    T(load_file_unknown_size_fetched_passed);
+    sfetch_shutdown();
+}
+
+/* tests whether not providing a buffer in SFETCH_STATE_OPENED properly fails */
+static bool load_file_no_buffer_opened_passed;
+static bool load_file_no_buffer_failed_passed;
+static void load_file_no_buffer_callback(sfetch_response_t response) {
+    if (response.state == SFETCH_STATE_OPENED) {
+        if ((response.content_size == combatsignal_file_size) &&
+            (response.chunk_offset == 0) &&
+            (response.chunk.ptr == 0) &&
+            (response.chunk.size == 0) &&
+            !response.finished)
+        {
+            /* DO NOT provide a buffer here, see if that properly fails */
+            load_file_no_buffer_opened_passed = true;
+        }
+    }
+    else if (response.state == SFETCH_STATE_FAILED) {
+        if (load_file_no_buffer_opened_passed) {
+            load_file_no_buffer_failed_passed = true;
+        }
+    }
+}
+
+UTEST(sokol_fetch, load_file_no_buffer) {
+    memset(load_file_buf, 0, sizeof(load_file_buf));
+    sfetch_setup(&(sfetch_desc_t){0});
+    sfetch_handle_t h = sfetch_send(&(sfetch_request_t){
+        .path = "comsi.s3m",
+        .callback = load_file_no_buffer_callback
+    });
+    int frame_count = 0;
+    while (sfetch_handle_valid(h) && (frame_count++ < 100)) {
+        sfetch_dowork();
+        sleep_ms(1);
+    }
+    T(load_file_no_buffer_opened_passed);
+    T(load_file_no_buffer_failed_passed);
     sfetch_shutdown();
 }
