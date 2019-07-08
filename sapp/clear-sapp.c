074a1bbc9f7264d85c0cfd38c141f8bbe3eb4148
Oldes <oldes.huhuman@gmail.com>
2019-07-08 22:14:18 +0200

    FIX: clear-sapp: non-trivial designated initializers not supported

--- sapp/clear-sapp.c
+++ sapp/clear-sapp.c
@@ -8,19 +8,25 @@
 static sg_pass_action pass_action;
 
 void init(void) {
-    sg_setup(&(sg_desc){
-        .gl_force_gles2 = true,
-        .mtl_device = sapp_metal_get_device(),
-        .mtl_renderpass_descriptor_cb = sapp_metal_get_renderpass_descriptor,
-        .mtl_drawable_cb = sapp_metal_get_drawable,
-        .d3d11_device = sapp_d3d11_get_device(),
-        .d3d11_device_context = sapp_d3d11_get_device_context(),
-        .d3d11_render_target_view_cb = sapp_d3d11_get_render_target_view,
-        .d3d11_depth_stencil_view_cb = sapp_d3d11_get_depth_stencil_view
-    });
-    pass_action = (sg_pass_action) {
-        .colors[0] = { .action=SG_ACTION_CLEAR, .val={1.0f, 0.0f, 0.0f, 1.0f} }
-    };
+    sg_desc desc = { };
+    desc.gl_force_gles2 = true,
+    desc.mtl_device = sapp_metal_get_device();
+    desc.mtl_renderpass_descriptor_cb = sapp_metal_get_renderpass_descriptor;
+    desc.mtl_drawable_cb = sapp_metal_get_drawable;
+    desc.d3d11_device = sapp_d3d11_get_device();
+    desc.d3d11_device_context = sapp_d3d11_get_device_context();
+    desc.d3d11_render_target_view_cb = sapp_d3d11_get_render_target_view;
+    desc.d3d11_depth_stencil_view_cb = sapp_d3d11_get_depth_stencil_view;
+    desc.gl_force_gles2 = sapp_gles2();
+    sg_setup(&desc);
+
+    // initial clear color
+    pass_action.colors[0].action = SG_ACTION_CLEAR;
+    pass_action.colors[0].val[0] = 1.0f;
+    pass_action.colors[0].val[1] = 0.0f;
+    pass_action.colors[0].val[2] = 0.0f;
+    pass_action.colors[0].val[3] = 1.0f;
+
     __dbgui_setup(1);
 }
 
@@ -39,15 +45,15 @@ void cleanup(void) {
 }
 
 sapp_desc sokol_main(int argc, char* argv[]) {
-    return (sapp_desc){
-        .init_cb = init,
-        .frame_cb = frame,
-        .cleanup_cb = cleanup,
-        .event_cb = __dbgui_event,
-        .width = 400,
-        .height = 300,
-        .gl_force_gles2 = true,
-        .window_title = "Clear (sokol app)",
-    };
+    sapp_desc desc = { };
+    desc.init_cb = init;
+    desc.frame_cb = frame;
+    desc.cleanup_cb = cleanup;
+    desc.event_cb = __dbgui_event;
+    desc.width = 400;
+    desc.height = 300;
+    desc.gl_force_gles2 = true;
+    desc.window_title = "Clear (sokol-app)";
+    return desc;
 }
 
