aebc9dfa04e5bc88a4347a37a2280734902ca471
Andre Weissflog <floooh@gmail.com>
2019-09-04 16:56:02 +0200

    cleanup triangle-sapp.c a bit

--- sapp/triangle-sapp.c
+++ sapp/triangle-sapp.c
@@ -7,13 +7,14 @@
 #include "dbgui/dbgui.h"
 #include "triangle-sapp.glsl.h"
 
-static sg_pass_action pass_action = {
-    .colors[0] = { .action=SG_ACTION_CLEAR, .val={0.0f, 0.0f, 0.0f, 1.0f } }
-};
-static sg_pipeline pip;
-static sg_bindings bind;
+/* application state */
+static struct {
+    sg_pipeline pip;
+    sg_bindings bind;
+    sg_pass_action pass_action;
+} state;
 
-void init(void) {
+static void init(void) {
     sg_setup(&(sg_desc){
         .gl_force_gles2 = true,
         .mtl_device = sapp_metal_get_device(),
@@ -31,9 +32,9 @@ void init(void) {
         // positions            // colors
          0.0f,  0.5f, 0.5f,     1.0f, 0.0f, 0.0f, 1.0f,
          0.5f, -0.5f, 0.5f,     0.0f, 1.0f, 0.0f, 1.0f,
-        -0.5f, -0.5f, 0.5f,     0.0f, 0.0f, 1.0f, 1.0f 
+        -0.5f, -0.5f, 0.5f,     0.0f, 0.0f, 1.0f, 1.0f
     };
-    bind.vertex_buffers[0] = sg_make_buffer(&(sg_buffer_desc){
+    state.bind.vertex_buffers[0] = sg_make_buffer(&(sg_buffer_desc){
         .size = sizeof(vertices),
         .content = vertices,
         .label = "triangle-vertices"
@@ -43,7 +44,7 @@ void init(void) {
     sg_shader shd = sg_make_shader(triangle_shader_desc());
 
     /* create a pipeline object (default render states are fine for triangle) */
-    pip = sg_make_pipeline(&(sg_pipeline_desc){
+    state.pip = sg_make_pipeline(&(sg_pipeline_desc){
         .shader = shd,
         /* if the vertex layout doesn't have gaps, don't need to provide strides and offsets */
         .layout = {
@@ -54,12 +55,17 @@ void init(void) {
         },
         .label = "triangle-pipeline"
     });
+
+    /* a pass action to framebuffer to black */
+    state.pass_action = (sg_pass_action) {
+        .colors[0] = { .action=SG_ACTION_CLEAR, .val={0.0f, 0.0f, 0.0f, 1.0f } }
+    };
 }
 
 void frame(void) {
-    sg_begin_default_pass(&pass_action, sapp_width(), sapp_height());
-    sg_apply_pipeline(pip);
-    sg_apply_bindings(&bind);
+    sg_begin_default_pass(&state.pass_action, sapp_width(), sapp_height());
+    sg_apply_pipeline(state.pip);
+    sg_apply_bindings(&state.bind);
     sg_draw(0, 3, 1);
     __dbgui_draw();
     sg_end_pass();
@@ -77,8 +83,8 @@ sapp_desc sokol_main(int argc, char* argv[]) {
         .frame_cb = frame,
         .cleanup_cb = cleanup,
         .event_cb = __dbgui_event,
-        .width = 640, //800,
-        .height = 480, //600,
+        .width = 640,
+        .height = 480,
         .gl_force_gles2 = true,
         .window_title = "Triangle (sokol-app)",
     };
