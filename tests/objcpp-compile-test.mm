c0832bd7f518f17e511c69ec60d2ff31ef7f9a85
Andre Weissflog <floooh@gmail.com>
2020-05-28 20:02:18 +0200

    add more headers to the C++ compile tests

--- tests/objcpp-compile-test.mm
+++ tests/objcpp-compile-test.mm
@@ -13,10 +13,30 @@
 #include "sokol_time.h"
 #include "sokol_fetch.h"
 #include "sokol_glue.h"
+
 #define SOKOL_GL_IMPL
 #include "sokol_gl.h"
+
 #define SOKOL_DEBUGTEXT_IMPL
 #include "sokol_debugtext.h"
+
+#include "imgui.h"
+#define SOKOL_IMGUI_IMPL
+#include "sokol_imgui.h"
+
+#define SOKOL_GFX_IMGUI_IMPL
+#include "sokol_gfx_imgui.h"
+
+#define FONTSTASH_IMPLEMENTATION
+#if defined(_MSC_VER )
+#pragma warning(disable:4996)   // strncpy use in fontstash.h
+#endif
+#if defined(__GNUC__)
+#pragma GCC diagnostic ignored "-Wunused-function"
+#endif
+#include "fontstash/fontstash.h"
+#define SOKOL_FONTSTASH_IMPL
+#include "sokol_fontstash.h"
 
 static sapp_desc desc;
 
