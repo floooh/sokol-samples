347a8c4574226cab2536c44a7fd9dd014dac18d7
Andre Weissflog <floooh@gmail.com>
2020-06-30 11:51:52 +0200

    fix an ImGui 1.77 related comment

--- d3d11/imgui-d3d11.cc
+++ d3d11/imgui-d3d11.cc
@@ -185,7 +185,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
             ImGui::End();
         }
 
-        // 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
+        // 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowDemoWindow()
         if (show_test_window) {
             ImGui::SetNextWindowPos(ImVec2(460, 20), ImGuiCond_FirstUseEver);
             ImGui::ShowDemoWindow();
