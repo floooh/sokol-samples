711d9da2bd4e59dd98bbd1e350ca3a1e3232e0f3
unknown <d.hilliard@hiltech.com.au>
2020-07-22 19:07:17 +0800

    Added sokol-app Shadow Map Sample

---
+++ sapp/shadows-sapp.c
@@ -0,0 +1,292 @@
+//------------------------------------------------------------------------------
+//  shadows-sapp.c
+//  Render to an offscreen rendertarget texture, and use this texture
+//  for rendering shadows to the screen.
+//------------------------------------------------------------------------------
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+#include "sokol_glue.h"
+#define HANDMADE_MATH_IMPLEMENTATION
+#define HANDMADE_MATH_NO_SSE
+#include "HandmadeMath.h"
+#include "dbgui/dbgui.h"
+#include "shadows-sapp.glsl.h"
+
+#define OFFSCREEN_SAMPLE_COUNT (4)
+
+static struct {
+    struct {
+        sg_pass_action pass_action;
+        sg_pass pass;
+        sg_pipeline pip;
+        sg_bindings bind;
+    } offscreen;
+    struct {
+        sg_pass_action pass_action;
+        sg_pipeline pip;
+        sg_bindings bind;
+    } deflt;
+    float ry;
+} state;
+
+void init(void) {
+    sg_setup(&(sg_desc){
+        .context = sapp_sgcontext()
+    });
+    __dbgui_setup(sapp_sample_count());
+
+    /* default pass action: clear to blue-ish */
+    state.deflt.pass_action = (sg_pass_action) {
+        .colors[0] = { .action = SG_ACTION_CLEAR, .val = { 0.0f, 0.25f, 1.0f, 1.0f } }
+    };
+
+    /* offscreen pass action: clear to black */
+    state.offscreen.pass_action = (sg_pass_action) {
+        .colors[0] = { .action = SG_ACTION_CLEAR, .val = { 1.0f, 1.0f, 1.0f, 1.0f } }
+    };
+
+    /* a render pass with one color- and one depth-attachment image */
+    sg_image_desc img_desc = {
+        .render_target = true,
+        .width = 2048,
+        .height = 2048,
+        .pixel_format = SG_PIXELFORMAT_RGBA8,
+        .min_filter = SG_FILTER_LINEAR,
+        .mag_filter = SG_FILTER_LINEAR,
+        .sample_count = 1,
+        .label = "color-image"
+    };
+    sg_image color_img = sg_make_image(&img_desc);
+    img_desc.pixel_format = SG_PIXELFORMAT_DEPTH;
+    img_desc.label = "depth-image";
+    sg_image depth_img = sg_make_image(&img_desc);
+    state.offscreen.pass = sg_make_pass(&(sg_pass_desc){
+        .color_attachments[0].image = color_img,
+        .depth_stencil_attachment.image = depth_img,
+        .label = "shadow-pass"
+    });
+
+    /* cube vertex buffer with positions, normals, colors and tex coords */
+    float vertices[] = {
+        /* pos                  normals             */
+        -1.0f, -1.0f, -1.0f,    0.0f, 0.0f, -1.0f,  //CUBE BACK FACE
+         1.0f, -1.0f, -1.0f,    0.0f, 0.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,    0.0f, 0.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,    0.0f, 0.0f, -1.0f,
+
+        -1.0f, -1.0f,  1.0f,    0.0f, 0.0f, 1.0f,   //CUBE FRONT FACE
+         1.0f, -1.0f,  1.0f,    0.0f, 0.0f, 1.0f,
+         1.0f,  1.0f,  1.0f,    0.0f, 0.0f, 1.0f,
+        -1.0f,  1.0f,  1.0f,    0.0f, 0.0f, 1.0f,
+
+        -1.0f, -1.0f, -1.0f,    -1.0f, 0.0f, 0.0f,  //CUBE LEFT FACE
+        -1.0f,  1.0f, -1.0f,    -1.0f, 0.0f, 0.0f,
+        -1.0f,  1.0f,  1.0f,    -1.0f, 0.0f, 0.0f,
+        -1.0f, -1.0f,  1.0f,    -1.0f, 0.0f, 0.0f,
+
+         1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 0.0f,   //CUBE RIGHT FACE
+         1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.0f,
+         1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.0f,
+         1.0f, -1.0f,  1.0f,    1.0f, 0.0f, 0.0f,
+
+        -1.0f, -1.0f, -1.0f,    0.0f, -1.0f, 0.0f,  //CUBE BOTTOM FACE
+        -1.0f, -1.0f,  1.0f,    0.0f, -1.0f, 0.0f,
+         1.0f, -1.0f,  1.0f,    0.0f, -1.0f, 0.0f,
+         1.0f, -1.0f, -1.0f,    0.0f, -1.0f, 0.0f,
+
+        -1.0f,  1.0f, -1.0f,    0.0f, 1.0f, 0.0f,   //CUBE TOP FACE
+        -1.0f,  1.0f,  1.0f,    0.0f, 1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f,    0.0f, 1.0f, 0.0f,
+         1.0f,  1.0f, -1.0f,    0.0f, 1.0f, 0.0f,
+
+        -1.0f,  0.0f, -1.0f,    0.0f, 1.0f, 0.0f,   //PLANE GEOMETRY
+        -1.0f,  0.0f,  1.0f,    0.0f, 1.0f, 0.0f,
+         1.0f,  0.0f,  1.0f,    0.0f, 1.0f, 0.0f,
+         1.0f,  0.0f, -1.0f,    0.0f, 1.0f, 0.0f,
+    };
+    sg_buffer vbuf = sg_make_buffer(&(sg_buffer_desc){
+        .size = sizeof(vertices),
+        .content = vertices,
+        .label = "cube-vertices"
+    });
+
+    /* an index buffer for the cube */
+    uint16_t indices[] = {
+        0, 1, 2,  0, 2, 3,
+        6, 5, 4,  7, 6, 4,
+        8, 9, 10,  8, 10, 11,
+        14, 13, 12,  15, 14, 12,
+        16, 17, 18,  16, 18, 19,
+        22, 21, 20,  23, 22, 20,
+        26, 25, 24,  27, 26, 24
+    };
+    sg_buffer ibuf = sg_make_buffer(&(sg_buffer_desc){
+        .type = SG_BUFFERTYPE_INDEXBUFFER,
+        .size = sizeof(indices),
+        .content = indices,
+        .label = "cube-indices"
+    });
+
+    /* pipeline-state-object for offscreen-rendered cube, don't need texture coord here */
+    state.offscreen.pip = sg_make_pipeline(&(sg_pipeline_desc){
+        .layout = {
+            /* need to provide stride, because the buffer's normal vector is skipped */
+            .buffers[0].stride = 6 * sizeof(float),
+            /* but don't need to provide attr offsets, because pos and color are continuous */
+            .attrs = {
+                [ATTR_shadowVS_position].format = SG_VERTEXFORMAT_FLOAT3
+            }
+        },
+        .shader = sg_make_shader(shadow_shader_desc()),
+        .index_type = SG_INDEXTYPE_UINT16,
+        .depth_stencil = {
+            .depth_compare_func = SG_COMPAREFUNC_LESS_EQUAL,
+            .depth_write_enabled = true,
+        },
+        .blend = {
+            .color_format = SG_PIXELFORMAT_RGBA8,
+            .depth_format = SG_PIXELFORMAT_DEPTH
+        },
+        .rasterizer = {
+            .cull_mode = SG_CULLMODE_FRONT,
+            .sample_count = 1
+        },
+        .label = "offscreen-pipeline"
+    });
+
+    /* and another pipeline-state-object for the default pass */
+    state.deflt.pip = sg_make_pipeline(&(sg_pipeline_desc){
+        .layout = {
+            /* don't need to provide buffer stride or attr offsets, no gaps here */
+            .attrs = {
+                [ATTR_colorVS_position].format = SG_VERTEXFORMAT_FLOAT3,
+                [ATTR_colorVS_normal].format = SG_VERTEXFORMAT_FLOAT3
+            }
+        },
+        .shader = sg_make_shader(color_shader_desc()),
+        .index_type = SG_INDEXTYPE_UINT16,
+        .depth_stencil = {
+            .depth_compare_func = SG_COMPAREFUNC_LESS_EQUAL,
+            .depth_write_enabled = true
+        },
+        .rasterizer = {
+            .cull_mode = SG_CULLMODE_BACK,
+        },
+        .label = "default-pipeline"
+    });
+
+    /* the resource bindings for rendering a non-textured cube into offscreen render target */
+    state.offscreen.bind = (sg_bindings){
+        .vertex_buffers[0] = vbuf,
+        .index_buffer = ibuf
+    };
+
+    /* resource bindings to render a textured cube, using the offscreen render target as texture */
+    state.deflt.bind = (sg_bindings){
+        .vertex_buffers[0] = vbuf,
+        .index_buffer = ibuf,
+        .fs_images[SLOT_shadowMap] = color_img
+    };
+
+    state.ry = 0.0f;
+}
+
+void frame(void) {
+
+    state.ry += 0.2f;
+
+    /* Calculate matrices for shadow pass */
+    const hmm_mat4 rym = HMM_Rotate(state.ry, HMM_Vec3(0.0f,1.0f,0.0f));
+    const hmm_vec4 light_dir = HMM_MultiplyMat4ByVec4(rym, HMM_Vec4(50.0f,50.0f,-50.0f,0.0f));
+    const hmm_mat4 light_view = HMM_LookAt(light_dir.XYZ, HMM_Vec3(0.0f, 0.0f, 0.0f), HMM_Vec3(0.0f, 1.0f, 0.0f));
+
+    /* Configure a bias matrix for converting view-space coordinates into uv coordinates */
+    hmm_mat4 light_proj = {
+            0.5f, 0.0f, 0.0f, 0,
+            0.0f, 0.5f, 0.0f, 0,
+            0.0f, 0.0f, 0.5f, 0,
+            0.5f, 0.5f, 0.5f, 1
+    };
+    light_proj = HMM_MultiplyMat4(light_proj, HMM_Orthographic(-4.0f, 4.0f, -4.0f, 4.0f, 0, 200.0f));
+    const hmm_mat4 light_view_proj = HMM_MultiplyMat4(light_proj, light_view);
+
+    /* Calculate matrices for camera pass */
+    const hmm_mat4 proj = HMM_Perspective(60.0f, (float)sapp_width()/(float)sapp_height(), 0.01f, 100.0f);
+    const hmm_mat4 view = HMM_LookAt(HMM_Vec3(5.0f, 5.0f, 5.0f), HMM_Vec3(0.0f, 0.0f, 0.0f), HMM_Vec3(0.0f, 1.0f, 0.0f));
+    const hmm_mat4 view_proj = HMM_MultiplyMat4(proj, view);
+
+    /* Calculate transform matrices for plane and cube */
+    const hmm_mat4 scale = HMM_Scale(HMM_Vec3(5,0,5));
+    const hmm_mat4 translate = HMM_Translate(HMM_Vec3(0,1.5f,0));
+
+    /* Initialise fragment uniforms for light shader */
+    fs_light_params_t fs_light_params;
+    fs_light_params.lightDir = HMM_NormalizeVec3(light_dir.XYZ);
+    fs_light_params.shadowMapSize = HMM_Vec2(2048,2048);
+    fs_light_params.eyePos = HMM_Vec3(5.0f,5.0f,5.0f);
+
+    /* the offscreen pass, render the vertices into the depth image */
+    sg_begin_pass(state.offscreen.pass, &state.offscreen.pass_action);
+    sg_apply_pipeline(state.offscreen.pip);
+    sg_apply_bindings(&state.offscreen.bind);
+    {
+        /* Render the cube into the shadow map */
+        vs_shadow_params_t vs_shadow_params;
+        vs_shadow_params.mvp = HMM_MultiplyMat4(light_view_proj,translate);
+        sg_apply_uniforms(SG_SHADERSTAGE_VS, SLOT_vs_shadow_params, &vs_shadow_params, sizeof(vs_shadow_params));
+        sg_draw(0, 36, 1);
+    }
+    sg_end_pass();
+
+    /* and the display-pass, rendering the scene, using the
+       previously rendered shadow map as a texture */
+    sg_begin_default_pass(&state.deflt.pass_action, sapp_width(), sapp_height());
+    
+    sg_apply_pipeline(state.deflt.pip);
+    sg_apply_bindings(&state.deflt.bind);
+    sg_apply_uniforms(SG_SHADERSTAGE_FS, SLOT_fs_light_params, &fs_light_params, sizeof(fs_light_params));
+    {
+        /* Render the plane in the light pass */
+        vs_light_params_t vs_light_params;
+        vs_light_params.mvp = HMM_MultiplyMat4(view_proj,scale);
+        vs_light_params.lightMVP = HMM_MultiplyMat4(light_view_proj,scale);
+        vs_light_params.model = HMM_Mat4d(1.0f);
+        vs_light_params.diffColor = HMM_Vec3(0.5f,0.5f,0.5f);
+        sg_apply_uniforms(SG_SHADERSTAGE_VS, SLOT_vs_light_params, &vs_light_params, sizeof(vs_light_params));
+        sg_draw(36, 6, 1);
+    }
+    {
+        /* Render the cube in the light pass */
+        vs_light_params_t vs_light_params;
+        vs_light_params.lightMVP = HMM_MultiplyMat4(light_view_proj,translate);
+        vs_light_params.model = translate;
+        vs_light_params.mvp = HMM_MultiplyMat4(view_proj,translate);
+        vs_light_params.diffColor = HMM_Vec3(1.0f,1.0f,1.0f);
+        sg_apply_uniforms(SG_SHADERSTAGE_VS, SLOT_vs_light_params, &vs_light_params, sizeof(vs_light_params));
+        sg_draw(0, 36, 1);
+    }
+
+    __dbgui_draw();
+    sg_end_pass();
+
+    sg_commit();
+}
+
+void cleanup(void) {
+    __dbgui_shutdown();
+    sg_shutdown();
+}
+
+sapp_desc sokol_main(int argc, char* argv[]) {
+    (void)argc; (void)argv;
+    return (sapp_desc){
+        .init_cb = init,
+        .frame_cb = frame,
+        .cleanup_cb = cleanup,
+        .event_cb = __dbgui_event,
+        .width = 1024,
+        .height = 768,
+        .sample_count = 4,
+        .window_title = "Shadow Rendering (sokol-app)",
+    };
+}
---
+++ sapp/shadows-sapp.glsl
@@ -0,0 +1,168 @@
+//------------------------------------------------------------------------------
+//  float/rgba8 encoding/decoding so that we can use an RGBA8
+//  shadow map instead of floating point render targets which might
+//  not be supported everywhere
+//
+//  http://aras-p.info/blog/2009/07/30/encoding-floats-to-rgba-the-final/
+//
+#pragma sokol @ctype mat4 hmm_mat4
+#pragma sokol @ctype vec3 hmm_vec3
+#pragma sokol @ctype vec2 hmm_vec2
+#pragma sokol @block util
+vec4 encodeDepth(float v) {
+    vec4 enc = vec4(1.0, 255.0, 65025.0, 160581375.0) * v;
+    enc = fract(enc);
+    enc -= enc.yzww * vec4(1.0/255.0,1.0/255.0,1.0/255.0,0.0);
+    return enc;
+}
+
+float decodeDepth(vec4 rgba) {
+    return dot(rgba, vec4(1.0, 1.0/255.0, 1.0/65025.0, 1.0/160581375.0));
+}
+
+//------------------------------------------------------------------------------
+//  perform simple shadow map lookup returns 0.0 (unlit) or 1.0 (lit)
+//
+float sampleShadow(sampler2D shadowMap, vec2 uv, float compare) {
+    #if !SOKOL_GLSL
+    uv.y = 1.0-uv.y;
+    #endif
+    float depth = decodeDepth(texture(shadowMap, vec2(uv.x, uv.y)));
+    depth += 0.001;
+    return step(compare, depth);
+}
+
+//------------------------------------------------------------------------------
+//  perform percentage-closer shadow map lookup
+//
+float sampleShadowPCF(sampler2D shadowMap, vec2 uv, vec2 smSize, float compare) {
+    float result = 0.0;
+    for (int x=-2; x<=2; x++) {
+        for (int y=-2; y<=2; y++) {
+            vec2 off = vec2(x,y)/smSize;
+            result += sampleShadow(shadowMap, uv+off, compare);
+        }
+    }
+    return result / 25.0;
+}
+
+//------------------------------------------------------------------------------
+//  perform gamma correction
+//
+vec4 gamma(vec4 c) {
+    float p = 1.0/2.2;
+    return vec4(pow(c.xyz, vec3(p, p, p)), c.w);
+}
+#pragma sokol @end
+
+#pragma sokol @block uniforms
+uniform vs_shadow_params {
+    mat4 mvp;
+};
+#pragma sokol @end
+//------------------------------------------------------------------------------
+//  Shadowmap pass shaders
+//
+#pragma sokol @vs shadowVS
+#pragma sokol @include_block uniforms
+
+in vec4 position;
+out vec2 projZW;
+
+void main() {
+    gl_Position = mvp * position;
+    projZW = gl_Position.zw;
+}
+#pragma sokol @end
+
+#pragma sokol @fs shadowFS
+#pragma sokol @include_block util
+in vec2 projZW;
+out vec4 fragColor;
+
+void main() {
+    float depth = projZW.x / projZW.y;
+    fragColor = encodeDepth(depth);
+}
+#pragma sokol @end
+
+#pragma sokol @program shadow shadowVS shadowFS
+
+//------------------------------------------------------------------------------
+//  Color pass shaders
+//
+#pragma sokol @block colorUniforms
+uniform vs_light_params {
+    mat4 model;
+    mat4 mvp;
+    mat4 lightMVP;
+    vec3 diffColor;
+};
+#pragma sokol @end
+
+#pragma sokol @vs colorVS
+#pragma sokol @include_block colorUniforms
+in vec4 position;
+in vec3 normal;
+out vec3 color;
+out vec4 lightProjPos;
+out vec4 P;
+out vec3 N;
+
+void main() {
+    gl_Position = mvp * position;
+    lightProjPos = lightMVP * position;
+    P = (model * position);
+    N = (model * vec4(normal, 0.0)).xyz;
+    color = diffColor;
+}
+#pragma sokol @end
+
+#pragma sokol @fs colorFS
+#pragma sokol @include_block util
+
+uniform fs_light_params {
+    vec2 shadowMapSize;
+    vec3 lightDir;
+    vec3 eyePos;
+};
+uniform sampler2D shadowMap;
+
+in vec3 color;
+in vec4 lightProjPos;
+in vec4 P;
+in vec3 N;
+out vec4 fragColor;
+
+void main() {
+    float specPower = 2.2;
+    float ambientIntensity = 0.25;
+
+    // diffuse lighting
+    vec3 l = normalize(lightDir);
+    vec3 n = normalize(N);
+    float n_dot_l = dot(n,l);
+    if (n_dot_l > 0.0) {
+
+        vec3 lightPos = lightProjPos.xyz / lightProjPos.w;
+        vec2 smUV = (lightPos.xy+1.0)*0.5;
+        float depth = lightPos.z;
+        float s = sampleShadowPCF(shadowMap, smUV, shadowMapSize, depth);
+
+        float diffIntensity = max(n_dot_l * s, 0.0);
+
+        vec3 v = normalize(eyePos - P.xyz);
+        vec3 r = reflect(-l, n);
+        float r_dot_v = max(dot(r, v), 0.0);
+        float specIntensity = pow(r_dot_v, specPower) * n_dot_l * s;
+
+        fragColor = vec4(vec3(specIntensity, specIntensity, specIntensity) + (diffIntensity+ambientIntensity)*color, 1.0);
+    }
+    else {
+        fragColor = vec4(color * ambientIntensity, 1.0);
+    }
+    fragColor = gamma(fragColor);
+}
+#pragma sokol @end
+
+#pragma sokol @program color colorVS colorFS
