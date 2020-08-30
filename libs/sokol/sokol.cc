de2f038a5ceb1c0dc1125c3207a36153a2d289a3
Andre Weissflog <floooh@gmail.com>
2020-08-30 17:32:06 +0200

    sokol-cpp library to test the new C++ D3D11/DXGI C/C++ calls in sokol_gfx/app.h

---
+++ libs/sokol/sokol.cc
@@ -0,0 +1,15 @@
+// same as sokol.c, but compiled as C++
+#define SOKOL_IMPL
+#if defined(_WIN32)
+#include <Windows.h>
+#define SOKOL_LOG(s) OutputDebugStringA(s)
+#endif
+/* this is only needed for the debug-inspection headers */
+#define SOKOL_TRACE_HOOKS
+/* sokol 3D-API defines are provided by build options */
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+#include "sokol_time.h"
+#include "sokol_audio.h"
+#include "sokol_fetch.h"
+#include "sokol_glue.h"
---
+++ libs/sokol/sokol.mm
@@ -0,0 +1,11 @@
+// same as sokol.m, but compiled as Objective-C++
+#define SOKOL_IMPL
+/* this is only needed for the debug-inspection headers */
+#define SOKOL_TRACE_HOOKS
+/* sokol 3D-API defines are provided by build options */
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+#include "sokol_time.h"
+#include "sokol_audio.h"
+#include "sokol_fetch.h"
+#include "sokol_glue.h"
