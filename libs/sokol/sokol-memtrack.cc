d5f6f3cf424add15707f542791794812ca6e43c7
Andre Weissflog <floooh@gmail.com>
2020-09-08 19:41:45 +0200

    fixes for UWP

---
+++ libs/sokol/sokol-memtrack.cc
@@ -0,0 +1,18 @@
+#define SOKOL_IMPL
+#if defined(_WIN32)
+#define SOKOL_LOG(s) OutputDebugStringA(s)
+#endif
+/* this is only needed for the debug-inspection headers */
+#define SOKOL_TRACE_HOOKS
+/* sokol 3D-API defines are provided by build options */
+#include "sokol_memtrack.h"
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+#include "sokol_time.h"
+//#include "sokol_audio.h"
+//#include "sokol_fetch.h"
+#include "sokol_glue.h"
+#define SOKOL_DEBUGTEXT_IMPL
+#include "sokol_debugtext.h"
+#define SOKOL_GL_IMPL
+#include "sokol_gl.h"
