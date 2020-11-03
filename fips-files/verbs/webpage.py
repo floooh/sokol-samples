570485ad26c6dbc64be2549d21b5ab617f9e7466
Andre Weissflog <floooh@gmail.com>
2020-11-03 20:08:31 +0100

    add droptest-sapp sample to webpage

--- fips-files/verbs/webpage.py
+++ fips-files/verbs/webpage.py
@@ -39,6 +39,7 @@ samples = [
     [ 'debugtext-userfont', 'debugtext-userfont-sapp.c', None],
     [ 'debugtext-context', 'debugtext-context-sapp.c', 'debugtext-context-sapp.glsl'],
     [ 'events', 'events-sapp.cc', None],
+    [ 'droptest', 'droptest-sapp.c', None],
     [ 'pixelformats', 'pixelformats-sapp.c', None],
     [ 'pixelformats-gles2', 'pixelformats-sapp.c', None],
     [ 'saudio', 'saudio-sapp.c', None],
