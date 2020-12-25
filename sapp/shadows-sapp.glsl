732d4e871ea9124f46e812ed8c2dd1e6823e5284
Andre Weissflog <floooh@gmail.com>
2020-12-25 12:16:18 +0100

    shadow-sapp: fix depth encoding (fixes #76)

--- sapp/shadows-sapp.glsl
+++ sapp/shadows-sapp.glsl
@@ -10,14 +10,14 @@
 #pragma sokol @ctype vec2 hmm_vec2
 #pragma sokol @block util
 vec4 encodeDepth(float v) {
-    vec4 enc = vec4(1.0, 255.0, 65025.0, 160581375.0) * v;
+    vec4 enc = vec4(1.0, 255.0, 65025.0, 16581375.0) * v;
     enc = fract(enc);
     enc -= enc.yzww * vec4(1.0/255.0,1.0/255.0,1.0/255.0,0.0);
     return enc;
 }
 
 float decodeDepth(vec4 rgba) {
-    return dot(rgba, vec4(1.0, 1.0/255.0, 1.0/65025.0, 1.0/160581375.0));
+    return dot(rgba, vec4(1.0, 1.0/255.0, 1.0/65025.0, 1.0/16581375.0));
 }
 
 //------------------------------------------------------------------------------
