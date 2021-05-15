c3390af77bcb0cde6b129ae2cf1d304209199899
Andre Weissflog <floooh@gmail.com>
2021-05-15 13:23:02 +0200

    add an Dear ImGui docking branch sample

---
+++ sapp/imgui-dock-sapp.cc
@@ -0,0 +1,98 @@
+//------------------------------------------------------------------------------
+//  imgui-dock-sapp.c
+//
+//  Using the Dear ImGui docking branch.
+//------------------------------------------------------------------------------
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+#include "sokol_time.h"
+#include "sokol_glue.h"
+#include "imgui-dock/imgui.h"
+#define SOKOL_IMGUI_IMPL
+#include "sokol_imgui.h"
+
+static uint64_t last_time = 0;
+static bool show_test_window = true;
+static bool show_another_window = false;
+
+static sg_pass_action pass_action;
+
+void init(void) {
+    // setup sokol-gfx, sokol-time and sokol-imgui
+    sg_desc desc = { };
+    desc.context = sapp_sgcontext();
+    sg_setup(&desc);
+    stm_setup();
+
+    // use sokol-imgui with all default-options (we're not doing
+    // multi-sampled rendering or using non-default pixel formats)
+    simgui_desc_t simgui_desc = { };
+    simgui_setup(&simgui_desc);
+    ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+    // initial clear color
+    pass_action.colors[0].action = SG_ACTION_CLEAR;
+    pass_action.colors[0].value = { 0.3f, 0.7f, 0.5f, 1.0f };
+}
+
+void frame(void) {
+    const int width = sapp_width();
+    const int height = sapp_height();
+    const double delta_time = stm_sec(stm_laptime(&last_time));
+    simgui_new_frame(width, height, delta_time);
+
+    // 1. Show a simple window
+    // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
+    static float f = 0.0f;
+    ImGui::Text("Drag windows over one another!");
+    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+    ImGui::ColorEdit3("clear color", &pass_action.colors[0].value.r);
+    if (ImGui::Button("Test Window")) show_test_window ^= 1;
+    if (ImGui::Button("Another Window")) show_another_window ^= 1;
+    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+
+    // 2. Show another simple window, this time using an explicit Begin/End pair
+    if (show_another_window) {
+        ImGui::SetNextWindowSize(ImVec2(200,100), ImGuiCond_FirstUseEver);
+        ImGui::Begin("Another Window", &show_another_window);
+        ImGui::Text("Hello");
+        ImGui::End();
+    }
+
+    // 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowDemoWindow()
+    if (show_test_window) {
+        ImGui::SetNextWindowPos(ImVec2(460, 20), ImGuiCond_FirstUseEver);
+        ImGui::ShowDemoWindow();
+    }
+
+    // the sokol_gfx draw pass
+    sg_begin_default_pass(&pass_action, width, height);
+    simgui_render();
+    sg_end_pass();
+    sg_commit();
+}
+
+void cleanup(void) {
+    simgui_shutdown();
+    sg_shutdown();
+}
+
+void input(const sapp_event* event) {
+    simgui_handle_event(event);
+}
+
+sapp_desc sokol_main(int argc, char* argv[]) {
+    (void)argc; (void)argv;
+    sapp_desc desc = { };
+    desc.init_cb = init;
+    desc.frame_cb = frame;
+    desc.cleanup_cb = cleanup;
+    desc.event_cb = input;
+    desc.width = 1024;
+    desc.height = 768;
+    desc.gl_force_gles2 = true;
+    desc.window_title = "Dear ImGui Docking (sokol-app)";
+    desc.ios_keyboard_resizes_canvas = false;
+    desc.icon.sokol_default = true;
+    return desc;
+}
