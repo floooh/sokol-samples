5e23fb214ac441a645159fafa0cca311e71782bd
Andre Weissflog <floooh@gmail.com>
2021-06-10 20:02:02 +0200

    ozz-anim-sapp: add UI and camera controls

--- sapp/ozz-anim-sapp.cc
+++ sapp/ozz-anim-sapp.cc
@@ -9,9 +9,20 @@
 #include "sokol_fetch.h"
 #include "sokol_time.h"
 #include "sokol_glue.h"
+
 #define SOKOL_GL_IMPL
 #include "sokol_gl.h"
 
+#include "imgui.h"
+#define SOKOL_IMGUI_IMPL
+#include "sokol_imgui.h"
+
+#define HANDMADE_MATH_IMPLEMENTATION
+#define HANDMADE_MATH_NO_SSE
+#include "HandmadeMath.h"
+#include "util/camera.h"
+
+// ozz-animation headers
 #include "ozz/animation/runtime/animation.h"
 #include "ozz/animation/runtime/skeleton.h"
 #include "ozz/animation/runtime/sampling_job.h"
@@ -23,7 +34,7 @@
 #include "ozz/base/maths/vec_float.h"
 
 #include <memory>   // std::unique_ptr, std::make_unique
-#include <cmath>
+#include <cmath>    // fmodf
 
 // managed ozz-animation C++ objects, this must be freed before shutdown, otherwise
 // ozz-animation will report a memory leak
@@ -36,27 +47,33 @@ typedef struct {
 } ozz_t;
 
 static struct {
-    sg_pass_action pass_action;
     std::unique_ptr<ozz_t> ozz;
+    sg_pass_action pass_action;
+    sgl_pipeline pip;
+    camera_t camera;
     bool skeleton_loaded;
     bool animation_loaded;
     bool load_failed;
+    bool paused;
+    float time_factor;
     uint64_t laptime;
-    double cur_time;
-    sgl_pipeline pip;
+    double cur_frame_time;
+    double cur_abs_time;
 } state;
 
 // io buffers for skeleton and animation data files, we know the max file size upfront
 static uint8_t skel_data_buffer[4 * 1024];
 static uint8_t anim_data_buffer[32 * 1024];
 
+static void eval_animation(void);
+static void draw_scene(void);
+static void draw_ui(void);
 static void skeleton_data_loaded(const sfetch_response_t* response);
 static void animation_data_loaded(const sfetch_response_t* response);
-static void eval_animation(void);
-static void draw_skeleton(void);
 
 static void init(void) {
     state.ozz = std::make_unique<ozz_t>();
+    state.time_factor = 1.0f;
 
     // setup sokol-gfx
     sg_desc sgdesc = { };
@@ -78,6 +95,10 @@ static void init(void) {
     sgldesc.sample_count = sapp_sample_count();
     sgl_setup(&sgldesc);
 
+    // setup sokol-imgui
+    simgui_desc_t imdesc = { };
+    simgui_setup(&imdesc);
+
     // initialize pass action for default-pass
     state.pass_action.colors[0].action = SG_ACTION_CLEAR;
     state.pass_action.colors[0].value = { 0.0f, 0.1f, 0.2f, 1.0f };
@@ -88,6 +109,16 @@ static void init(void) {
     pipdesc.depth.write_enabled = true;
     pipdesc.depth.compare = SG_COMPAREFUNC_LESS_EQUAL;
     state.pip = sgl_make_pipeline(&pipdesc);
+
+    // initialize camera helper
+    cam_init(&state.camera);
+    state.camera.min_dist = 1.0f;
+    state.camera.max_dist = 10.0f;
+    state.camera.min_lat = -85.0f;
+    state.camera.max_lat = 85.0f;
+    state.camera.center.Y = 1.0f;
+    state.camera.dist = 3.0f;
+    state.camera.polar = HMM_Vec2(10.0f, 20.0f);
 
     // start loading the skeleton.ozz and animation.ozz files
     {
@@ -109,30 +140,71 @@ static void init(void) {
 }
 
 static void frame(void) {
-    double frame_time = stm_sec(stm_round_to_common_refresh_rate(stm_laptime(&state.laptime)));
+    const int fb_width = sapp_width();
+    const int fb_height = sapp_height();
+    state.cur_frame_time = stm_sec(stm_round_to_common_refresh_rate(stm_laptime(&state.laptime)));
+    cam_update(&state.camera, fb_width, fb_height);
+
+    simgui_new_frame(fb_width, fb_height, state.cur_frame_time);
+    draw_ui();
 
     sfetch_dowork();
 
     if (state.animation_loaded && state.skeleton_loaded) {
-        state.cur_time += frame_time;
+        if (!state.paused) {
+            state.cur_abs_time += state.cur_frame_time * state.time_factor;
+        }
         eval_animation();
-        draw_skeleton();
+        draw_scene();
     }
 
-    sg_begin_default_pass(&state.pass_action, sapp_width(), sapp_height());
+    sg_begin_default_pass(&state.pass_action, fb_width, fb_height);
     sgl_draw();
+    simgui_render();
     sg_end_pass();
     sg_commit();
 }
 
 static void input(const sapp_event* ev) {
-    (void)ev;
+    // handle user interface input
+    if (simgui_handle_event(ev)) {
+        return;
+    }
+
+    // handle camera controller input
+    switch (ev->type) {
+        case SAPP_EVENTTYPE_MOUSE_DOWN:
+            if (ev->mouse_button == SAPP_MOUSEBUTTON_LEFT) {
+                sapp_lock_mouse(true);
+            }
+            break;
+
+        case SAPP_EVENTTYPE_MOUSE_UP:
+            if (ev->mouse_button == SAPP_MOUSEBUTTON_LEFT) {
+                sapp_lock_mouse(false);
+            }
+            break;
+
+        case SAPP_EVENTTYPE_MOUSE_SCROLL:
+            cam_zoom(&state.camera, ev->scroll_y * 0.5f);
+            break;
+
+        case SAPP_EVENTTYPE_MOUSE_MOVE:
+            if (sapp_mouse_locked()) {
+                cam_orbit(&state.camera, ev->mouse_dx * 0.25f, ev->mouse_dy * 0.25f);
+            }
+            break;
+
+        default:
+            break;
+    }
 }
 
 static void cleanup(void) {
-    // silence ozz leak detection
+    // free C++ objects early, to silence ozz leak detection
     state.ozz = nullptr;
 
+    simgui_shutdown();
     sgl_shutdown();
     sfetch_shutdown();
     sg_shutdown();
@@ -140,7 +212,7 @@ static void cleanup(void) {
 
 static void eval_animation(void) {
     const float anim_duration = state.ozz->animation.duration();
-    const float anim_ratio = fmodf(state.cur_time / anim_duration, 1.0f);   // 0..1
+    const float anim_ratio = fmodf(state.cur_abs_time / anim_duration, 1.0f);   // 0..1
 
     // sample animation
     ozz::animation::SamplingJob sampling_job;
@@ -158,23 +230,26 @@ static void eval_animation(void) {
     ltm_job.Run();
 }
 
-static void draw_skeleton(void) {
+static void draw_scene(void) {
     const float aspect = sapp_widthf() / sapp_heightf();
-    const float eye_x = 1.0f;
-    const float eye_y = 2.0f;
-    const float eye_z = 2.0f;
-    const float center_x = 0.0f;
-    const float center_y = 1.0f;
-    const float center_z = 0.0f;
+    const float eye_x = state.camera.eye_pos.X;
+    const float eye_y = state.camera.eye_pos.Y;
+    const float eye_z = state.camera.eye_pos.Z;
+    const float center_x = state.camera.center.X;
+    const float center_y = state.camera.center.Y;
+    const float center_z = state.camera.center.Z;
+    const float up_x = 0.0f;
+    const float up_y = 1.0f;
+    const float up_z = 0.0f;
     const int num_joints = state.ozz->skeleton.num_joints();
     ozz::span<const int16_t> joint_parents = state.ozz->skeleton.joint_parents();
 
     sgl_defaults();
     sgl_load_pipeline(state.pip);
     sgl_matrix_mode_projection();
-    sgl_perspective(sgl_rad(45.0f), aspect, 0.1f, 10.0f);
+    sgl_perspective(sgl_rad(60.0f), aspect, 0.1f, 100.0f);
     sgl_matrix_mode_modelview();
-    sgl_lookat(eye_x, eye_y, eye_z, center_x, center_y, center_z, 0.0f, 1.0f, 0.0f);
+    sgl_lookat(eye_x, eye_y, eye_z, center_x, center_y, center_z, up_x, up_y, up_z);
     sgl_begin_lines();
     sgl_c3b(0, 255, 0);
     for (int joint_index = 0; joint_index < num_joints; joint_index++) {
@@ -195,6 +270,25 @@ static void draw_skeleton(void) {
     sgl_end();
 }
 
+static void draw_ui(void) {
+    ImGui::SetNextWindowPos({ 20, 20 }, ImGuiCond_Once);
+    ImGui::SetNextWindowSize({ 220, 150 }, ImGuiCond_Once);
+    ImGui::SetNextWindowBgAlpha(0.35f);
+    if (ImGui::Begin("Controls", nullptr, ImGuiWindowFlags_NoDecoration|ImGuiWindowFlags_AlwaysAutoResize)) {
+        ImGui::Text("Camera Controls:");
+        ImGui::Text("  LMB + Mouse Move: Look");
+        ImGui::Text("  Mouse Wheel: Zoom");
+        ImGui::SliderFloat("Distance", &state.camera.dist, state.camera.min_dist, state.camera.max_dist, "%.1f", 1.0f);
+        ImGui::SliderFloat("Theta", &state.camera.polar.X, state.camera.min_lat, state.camera.max_lat, "%.1f", 1.0f);
+        ImGui::SliderFloat("Phi", &state.camera.polar.Y, 0.0f, 360.0f, "%.1f", 1.0f);
+        ImGui::Separator();
+        ImGui::Text("Time Controls:");
+        ImGui::Checkbox("Paused", &state.paused);
+        ImGui::SliderFloat("Factor", &state.time_factor, 0.0f, 5.0f, "%.1f", 1.0f);
+    }
+    ImGui::End();
+}
+
 static void skeleton_data_loaded(const sfetch_response_t* response) {
     if (response->fetched) {
         ozz::io::MemoryStream stream;
@@ -209,6 +303,9 @@ static void skeleton_data_loaded(const sfetch_response_t* response) {
             state.ozz->locals.resize(num_soa_joints);
             state.ozz->models.resize(num_joints);
             state.ozz->cache.Resize(num_joints);
+        }
+        else {
+            state.load_failed = true;
         }
     }
     else if (response->failed) {
@@ -225,6 +322,9 @@ static void animation_data_loaded(const sfetch_response_t* response) {
         if (archive.TestTag<ozz::animation::Animation>()) {
             archive >> state.ozz->animation;
             state.animation_loaded = true;
+        }
+        else {
+            state.load_failed = true;
         }
     }
     else if (response->failed) {
