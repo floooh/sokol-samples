23a662227358d10870ca70aa936c5af053056be4
Andre Weissflog <floooh@gmail.com>
2021-06-16 17:05:11 +0200

    ozz-skin-sapp finished

--- sapp/ozz-skin-sapp.cc
+++ sapp/ozz-skin-sapp.cc
@@ -2,7 +2,17 @@
 //  ozz-skin-sapp.c
 //
 //  Ozz-animation with GPU skinning.
-//  FIXME: explain how it works
+//
+//  http://guillaumeblanc.github.io/ozz-animation/
+//
+//  Joint palette data for vertex skinning is uploaded each frame to a dynamic
+//  RGBA32F texture and sampled in the vertex shader to perform weighted
+//  skinning with up to 4 influence joints per vertex.
+//
+//  Character instance matrices are stored in a vertex buffer.
+//
+//  Together this enables rendering many independently animated and positioned
+//  characters in a single draw call via hardware instancing.
 //------------------------------------------------------------------------------
 #include "sokol_app.h"
 #include "sokol_gfx.h"
@@ -36,19 +46,23 @@
 #include <memory>   // std::unique_ptr, std::make_unique
 #include <cmath>    // fmodf
 
-#define MAX_PALETTE_JOINTS (256)
-#define MAX_INSTANCES (64)
+// the upper limit for joint palette size is 256 (because the mesh joint indices
+// are stored in an UBYTE4), but the example mesh only needs less than 64
+#define MAX_PALETTE_JOINTS (64)
+
+// this defines the size of the instance-buffer and height of the joint-texture
+#define MAX_INSTANCES (512)
 
 // wrapper struct for managed ozz-animation C++ objects, must be deleted
 // before shutdown, otherwise ozz-animation will report a memory leak
 typedef struct {
     ozz::animation::Skeleton skeleton;
     ozz::animation::Animation animation;
-    ozz::animation::SamplingCache cache;
     ozz::vector<uint16_t> joint_remaps;
     ozz::vector<ozz::math::Float4x4> mesh_inverse_bindposes;
     ozz::vector<ozz::math::SoaTransform> local_matrices;
     ozz::vector<ozz::math::Float4x4> model_matrices;
+    ozz::animation::SamplingCache cache;
 } ozz_t;
 
 // a skinned-mesh vertex, we don't need the texcoords and tangent
@@ -65,8 +79,10 @@ typedef struct {
 // transposed 4x3 model-to-world matrix, and information where the
 // joint palette is found in the joint texture
 typedef struct {
-    float model[3][4];
-    float skin_info[4];
+    float xxxx[4];
+    float yyyy[4];
+    float zzzz[4];
+    float joint_uv[2];
 } instance_t;
 
 static struct {
@@ -76,13 +92,15 @@ static struct {
     sg_image joint_texture;
     sg_buffer instance_buffer;
     sg_bindings bind;
-    vs_params_t vs_params;
+    int num_instances;          // current number of character instances
     int num_triangle_indices;
+    int num_skeleton_joints;    // number of joints in the skeleton
     int num_skin_joints;        // number of joints actually used by skinned mesh
     int joint_texture_width;    // in number of pixels
     int joint_texture_height;   // in number of pixels
     int joint_texture_pitch;    // in number of floats
     camera_t camera;
+    bool draw_enabled;
     struct {
         bool skeleton;
         bool animation;
@@ -90,12 +108,12 @@ static struct {
         bool failed;
     } loaded;
     struct {
-        double frame;
-        double absolute;
+        double frame_time_ms;
+        double frame_time_sec;
+        double abs_time_sec;
         uint64_t laptime;
+        uint64_t anim_eval_time;
         float factor;
-        float anim_ratio;
-        bool anim_ratio_ui_override;
         bool paused;
     } time;
     struct {
@@ -109,12 +127,13 @@ static uint8_t skel_io_buffer[32 * 1024];
 static uint8_t anim_io_buffer[96 * 1024];
 static uint8_t mesh_io_buffer[3 * 1024 * 1024];
 
-// instance data upload buffer
-instance_t instance_upload_buffer[MAX_INSTANCES];
+// instance data buffer;
+static instance_t instance_data[MAX_INSTANCES];
 
 // joint-matrix upload buffer, each joint consists of transposed 4x3 matrix
-float joint_upload_buffer[MAX_INSTANCES][MAX_PALETTE_JOINTS][3][4];
+static float joint_upload_buffer[MAX_INSTANCES][MAX_PALETTE_JOINTS][3][4];
 
+static void init_instance_data(void);
 static void draw_ui(void);
 static void skel_data_loaded(const sfetch_response_t* respone);
 static void anim_data_loaded(const sfetch_response_t* respone);
@@ -122,6 +141,8 @@ static void mesh_data_loaded(const sfetch_response_t* respone);
 
 static void init(void) {
     state.ozz = std::make_unique<ozz_t>();
+    state.num_instances = 1;
+    state.draw_enabled = true;
     state.time.factor = 1.0f;
     state.ui.joint_texture_scale = 4;
 
@@ -150,15 +171,16 @@ static void init(void) {
 
     // initialize camera controller
     camera_desc_t camdesc = { };
-    camdesc.min_dist = 1.0f;
-    camdesc.max_dist = 10.0f;
+    camdesc.min_dist = 2.0f;
+    camdesc.max_dist = 40.0f;
     camdesc.center.Y = 1.1f;
     camdesc.distance = 3.0f;
-    camdesc.latitude = 10.0f;
+    camdesc.latitude = 20.0f;
     camdesc.longitude = 20.0f;
     cam_init(&state.camera, &camdesc);
 
-    // vertex-skinning shader and pipeline object for 3d rendering
+    // vertex-skinning shader and pipeline object for 3d rendering, note
+    // the hardware-instanced vertex layout
     sg_pipeline_desc pip_desc = { };
     pip_desc.shader = sg_make_shader(skinned_shader_desc(sg_query_backend()));
     pip_desc.layout.buffers[0].stride = sizeof(vertex_t);
@@ -174,8 +196,8 @@ static void init(void) {
     pip_desc.layout.attrs[ATTR_vs_inst_yyyy].buffer_index = 1;
     pip_desc.layout.attrs[ATTR_vs_inst_zzzz].format = SG_VERTEXFORMAT_FLOAT4;
     pip_desc.layout.attrs[ATTR_vs_inst_zzzz].buffer_index = 1;
-    pip_desc.layout.attrs[ATTR_vs_inst_skin_info].format = SG_VERTEXFORMAT_FLOAT4;
-    pip_desc.layout.attrs[ATTR_vs_inst_skin_info].buffer_index = 1;
+    pip_desc.layout.attrs[ATTR_vs_inst_joint_uv].format = SG_VERTEXFORMAT_FLOAT2;
+    pip_desc.layout.attrs[ATTR_vs_inst_joint_uv].buffer_index = 1;
     pip_desc.index_type = SG_INDEXTYPE_UINT16;
     // ozz mesh data appears to have counter-clock-wise face winding
     pip_desc.face_winding = SG_FACEWINDING_CCW;
@@ -201,11 +223,14 @@ static void init(void) {
     state.joint_texture = sg_make_image(&img_desc);
     state.bind.vs_images[SLOT_joint_tex] = state.joint_texture;
 
-    // create a dynamic instance-data buffer
+    // create a static instance-data buffer, in this demo, character instances
+    // don't move around and also are not clipped against the view volume,
+    // so we can just initialize a static instance data buffer upfront
+    init_instance_data();
     sg_buffer_desc buf_desc = { };
     buf_desc.type = SG_BUFFERTYPE_VERTEXBUFFER;
     buf_desc.size = MAX_INSTANCES * sizeof(instance_t);
-    buf_desc.usage = SG_USAGE_STREAM;
+    buf_desc.data = SG_RANGE(instance_data);
     state.instance_buffer = sg_make_buffer(&buf_desc);
     state.bind.vertex_buffers[1] = state.instance_buffer;
 
@@ -236,61 +261,91 @@ static void init(void) {
     }
 }
 
-static void eval_animation(void) {
-    // convert current time to animation ration (0.0 .. 1.0)
-    const float anim_duration = state.ozz->animation.duration();
-    if (!state.time.anim_ratio_ui_override) {
-        state.time.anim_ratio = fmodf(state.time.absolute / anim_duration, 1.0f);
+// initialize the static instance data, since the character instances don't
+// move around or are clipped against the view volume in this demo, the instance
+// data is initialized once and lives in an immutable instance buffer
+static void init_instance_data(void) {
+    assert((state.joint_texture_width > 0) && (state.joint_texture_height > 0));
+
+    // initialize the character instance model-to-world matrices
+    for (int i=0, x=0, y=0, dx=0, dy=0; i < MAX_INSTANCES; i++, x+=dx, y+=dy) {
+        instance_t* inst = &instance_data[i];
+
+        // a 3x4 transposed model-to-world matrix (only the x/z position is set)
+        inst->xxxx[0]=1.0f; inst->xxxx[1]=0.0f; inst->xxxx[2]=0.0f; inst->xxxx[3] = (float)x * 1.5f;
+        inst->yyyy[0]=0.0f; inst->yyyy[1]=1.0f; inst->yyyy[2]=0.0f; inst->yyyy[3] = 0.0f;
+        inst->zzzz[0]=0.0f; inst->zzzz[1]=0.0f; inst->zzzz[2]=1.0f; inst->zzzz[3] = (float)y * 1.5f;
+
+        // at a corner?
+        if (abs(x) == abs(y)) {
+            if (x >= 0) {
+                // top-right corner: start a new ring
+                if (y >= 0) { x+=1; y+=1; dx=0; dy=-1; }
+                // bottom-right corner
+                else { dx=-1; dy=0; }
+            }
+            else {
+                // top-left corner
+                if (y >= 0) { dx=+1; dy=0; }
+                // bottom-left corner
+                else { dx=0; dy=+1; }
+            }
+        }
     }
 
-    // sample animation
-    ozz::animation::SamplingJob sampling_job;
-    sampling_job.animation = &state.ozz->animation;
-    sampling_job.cache = &state.ozz->cache;
-    sampling_job.ratio = state.time.anim_ratio;
-    sampling_job.output = make_span(state.ozz->local_matrices);
-    sampling_job.Run();
-
-    // convert joint matrices from local to model space
-    ozz::animation::LocalToModelJob ltm_job;
-    ltm_job.skeleton = &state.ozz->skeleton;
-    ltm_job.input = make_span(state.ozz->local_matrices);
-    ltm_job.output = make_span(state.ozz->model_matrices);
-    ltm_job.Run();
-}
-
-// update the instance-data buffer with the model-to-world matrices and
-// joint-palette-texture infos for each character instance
-static void update_instance_buffer(int num_instances) {
-    // FIXME: fill model-to-world matrices
+    // the skin_info vertex component contains information about where to find
+    // the joint palette for this character instance in the joint texture
     const float half_pixel_x = 0.5f / (float)state.joint_texture_width;
     const float half_pixel_y = 0.5f / (float)state.joint_texture_height;
-    for (int i = 0; i < num_instances; i++) {
-        instance_t* inst = &instance_upload_buffer[i];
-        inst->skin_info[0] = half_pixel_x;
-        inst->skin_info[1] = half_pixel_y + (i / (float)state.joint_texture_height);
-        inst->skin_info[2] = (float)state.joint_texture_width;
-        inst->skin_info[3] = (float)state.joint_texture_height;
+    for (int i = 0; i < MAX_INSTANCES; i++) {
+        instance_t* inst = &instance_data[i];
+        inst->joint_uv[0] = half_pixel_x;
+        inst->joint_uv[1] = half_pixel_y + (i / (float)state.joint_texture_height);
     }
-    sg_range data = { instance_upload_buffer, num_instances * sizeof(instance_t) };
-    sg_update_buffer(state.instance_buffer, &data);
 }
 
 // compute skinning matrices, and upload into joint texture
 static void update_joint_texture(void) {
-    const int instance = 0;
-    for (int i = 0; i < state.num_skin_joints; i++) {
-        ozz::math::Float4x4 skin_matrix = state.ozz->model_matrices[state.ozz->joint_remaps[i]] * state.ozz->mesh_inverse_bindposes[i];
-        const ozz::math::SimdFloat4& c0 = skin_matrix.cols[0];
-        const ozz::math::SimdFloat4& c1 = skin_matrix.cols[1];
-        const ozz::math::SimdFloat4& c2 = skin_matrix.cols[2];
-        const ozz::math::SimdFloat4& c3 = skin_matrix.cols[3];
 
-        float* ptr = &joint_upload_buffer[instance][i][0][0];
-        *ptr++ = ozz::math::GetX(c0); *ptr++ = ozz::math::GetX(c1); *ptr++ = ozz::math::GetX(c2); *ptr++ = ozz::math::GetX(c3);
-        *ptr++ = ozz::math::GetY(c0); *ptr++ = ozz::math::GetY(c1); *ptr++ = ozz::math::GetY(c2); *ptr++ = ozz::math::GetY(c3);
-        *ptr++ = ozz::math::GetZ(c0); *ptr++ = ozz::math::GetZ(c1); *ptr++ = ozz::math::GetZ(c2); *ptr++ = ozz::math::GetZ(c3);
+    uint64_t start_time = stm_now();
+    const float anim_duration = state.ozz->animation.duration();
+    for (int instance = 0; instance < state.num_instances; instance++) {
+
+        // each character instance evaluates its own animation
+        const float anim_ratio = fmodf((state.time.abs_time_sec + (instance*0.1f)) / anim_duration, 1.0f);
+
+        // sample animation
+        // NOTE: using one cache per instance versus one cache per animation
+        // makes a small difference, but not much
+        ozz::animation::SamplingJob sampling_job;
+        sampling_job.animation = &state.ozz->animation;
+        sampling_job.cache = &state.ozz->cache;
+        sampling_job.ratio = anim_ratio;
+        sampling_job.output = make_span(state.ozz->local_matrices);
+        sampling_job.Run();
+
+        // convert joint matrices from local to model space
+        ozz::animation::LocalToModelJob ltm_job;
+        ltm_job.skeleton = &state.ozz->skeleton;
+        ltm_job.input = make_span(state.ozz->local_matrices);
+        ltm_job.output = make_span(state.ozz->model_matrices);
+        ltm_job.Run();
+
+        // compute skinning matrices and write to joint texture upload buffer
+        for (int i = 0; i < state.num_skin_joints; i++) {
+            ozz::math::Float4x4 skin_matrix = state.ozz->model_matrices[state.ozz->joint_remaps[i]] * state.ozz->mesh_inverse_bindposes[i];
+            const ozz::math::SimdFloat4& c0 = skin_matrix.cols[0];
+            const ozz::math::SimdFloat4& c1 = skin_matrix.cols[1];
+            const ozz::math::SimdFloat4& c2 = skin_matrix.cols[2];
+            const ozz::math::SimdFloat4& c3 = skin_matrix.cols[3];
+
+            float* ptr = &joint_upload_buffer[instance][i][0][0];
+            *ptr++ = ozz::math::GetX(c0); *ptr++ = ozz::math::GetX(c1); *ptr++ = ozz::math::GetX(c2); *ptr++ = ozz::math::GetX(c3);
+            *ptr++ = ozz::math::GetY(c0); *ptr++ = ozz::math::GetY(c1); *ptr++ = ozz::math::GetY(c2); *ptr++ = ozz::math::GetY(c3);
+            *ptr++ = ozz::math::GetZ(c0); *ptr++ = ozz::math::GetZ(c1); *ptr++ = ozz::math::GetZ(c2); *ptr++ = ozz::math::GetZ(c3);
+        }
     }
+    state.time.anim_eval_time = stm_since(start_time);
 
     sg_image_data img_data = { };
     // FIXME: upload partial texture? (needs sokol-gfx fixes)
@@ -303,26 +358,30 @@ static void frame(void) {
 
     const int fb_width = sapp_width();
     const int fb_height = sapp_height();
-    state.time.frame = stm_sec(stm_round_to_common_refresh_rate(stm_laptime(&state.time.laptime)));
+    uint64_t frame_ticks = stm_round_to_common_refresh_rate(stm_laptime(&state.time.laptime));
+    state.time.frame_time_ms = stm_ms(frame_ticks);
+    state.time.frame_time_sec = stm_sec(frame_ticks);
     cam_update(&state.camera, fb_width, fb_height);
 
-    simgui_new_frame(fb_width, fb_height, state.time.frame);
+    simgui_new_frame(fb_width, fb_height, state.time.frame_time_sec);
     draw_ui();
 
     sg_begin_default_pass(&state.pass_action, fb_width, fb_height);
     if (state.loaded.animation && state.loaded.skeleton && state.loaded.mesh) {
         if (!state.time.paused) {
-            state.time.absolute += state.time.frame * state.time.factor;
+            state.time.abs_time_sec += state.time.frame_time_sec * state.time.factor;
         }
-        eval_animation();
         update_joint_texture();
-        update_instance_buffer(1);
 
-        state.vs_params.view_proj = state.camera.view_proj;
+        vs_params_t vs_params = { };
+        vs_params.view_proj = state.camera.view_proj;
+        vs_params.joint_pixel_width = 1.0f / (float)state.joint_texture_width;
         sg_apply_pipeline(state.pip);
         sg_apply_bindings(&state.bind);
-        sg_apply_uniforms(SG_SHADERSTAGE_VS, SLOT_vs_params, SG_RANGE_REF(state.vs_params));
-        sg_draw(0, state.num_triangle_indices, 1);
+        sg_apply_uniforms(SG_SHADERSTAGE_VS, SLOT_vs_params, SG_RANGE_REF(vs_params));
+        if (state.draw_enabled) {
+            sg_draw(0, state.num_triangle_indices, state.num_instances);
+        }
     }
     simgui_render();
     sg_end_pass();
@@ -354,6 +413,17 @@ static void draw_ui(void) {
             ImGui::Text("Failed loading character data!");
         }
         else {
+            if (ImGui::SliderInt("Num Instances", &state.num_instances, 1, MAX_INSTANCES)) {
+                float dist_step = (state.camera.max_dist - state.camera.min_dist) / MAX_INSTANCES;
+                state.camera.distance = state.camera.min_dist + dist_step * state.num_instances;
+            }
+            ImGui::Checkbox("Enable Mesh Drawing", &state.draw_enabled);
+            ImGui::Text("Frame Time: %.3fms\n", state.time.frame_time_ms);
+            ImGui::Text("Anim Eval Time: %.3fms\n", stm_ms(state.time.anim_eval_time));
+            ImGui::Text("Num Triangles: %d\n", (state.num_triangle_indices/3) * state.num_instances);
+            ImGui::Text("Num Animated Joints: %d\n", state.num_skeleton_joints * state.num_instances);
+            ImGui::Text("Num Skinning Joints: %d\n", state.num_skin_joints * state.num_instances);
+            ImGui::Separator();
             ImGui::Text("Camera Controls:");
             ImGui::Text("  LMB + Mouse Move: Look");
             ImGui::Text("  Mouse Wheel: Zoom");
@@ -364,12 +434,6 @@ static void draw_ui(void) {
             ImGui::Text("Time Controls:");
             ImGui::Checkbox("Paused", &state.time.paused);
             ImGui::SliderFloat("Factor", &state.time.factor, 0.0f, 10.0f, "%.1f", 1.0f);
-            if (ImGui::SliderFloat("Ratio", &state.time.anim_ratio, 0.0f, 1.0f)) {
-                state.time.anim_ratio_ui_override = true;
-            }
-            if (ImGui::IsItemDeactivatedAfterEdit()) {
-                state.time.anim_ratio_ui_override = false;
-            }
             ImGui::Separator();
             if (ImGui::Button("Toggle Joint Texture")) {
                 state.ui.joint_texture_shown = !state.ui.joint_texture_shown;
@@ -413,6 +477,7 @@ static void skel_data_loaded(const sfetch_response_t* response) {
             const int num_joints = state.ozz->skeleton.num_joints();
             state.ozz->local_matrices.resize(num_soa_joints);
             state.ozz->model_matrices.resize(num_joints);
+            state.num_skeleton_joints = num_joints;
             state.ozz->cache.Resize(num_joints);
         }
         else {
--- sapp/ozz-skin-sapp.glsl
+++ sapp/ozz-skin-sapp.glsl
@@ -2,15 +2,15 @@
 @ctype vec4 hmm_vec4
 
 @block skin_utils
-void skinned_pos_nrm(in vec4 pos, in vec4 nrm, in vec4 skin_weights, in vec4 skin_indices, in vec4 skin_info, out vec4 skin_pos, out vec4 skin_nrm) {
+void skinned_pos_nrm(in vec4 pos, in vec4 nrm, in vec4 skin_weights, in vec4 skin_indices, in vec2 joint_uv, out vec4 skin_pos, out vec4 skin_nrm) {
     skin_pos = vec4(0.0, 0.0, 0.0, 1.0);
     skin_nrm = vec4(0.0, 0.0, 0.0, 0.0);    
     vec4 weights = skin_weights / dot(skin_weights, vec4(1.0));
-    vec2 step = vec2(1.0 / skin_info.z, 0.0);
+    vec2 step = vec2(joint_pixel_width, 0.0);
     vec2 uv;
     vec4 xxxx, yyyy, zzzz;
     if (weights.x > 0.0) {
-        uv = vec2(skin_info.x + (3.0 * skin_indices.x)/skin_info.z, skin_info.y);
+        uv = vec2(joint_uv.x + (3.0 * skin_indices.x)*joint_pixel_width, joint_uv.y);
         xxxx = textureLod(joint_tex, uv, 0.0);
         yyyy = textureLod(joint_tex, uv + step, 0.0);
         zzzz = textureLod(joint_tex, uv + 2.0 * step, 0.0);
@@ -18,7 +18,7 @@ void skinned_pos_nrm(in vec4 pos, in vec4 nrm, in vec4 skin_weights, in vec4 ski
         skin_nrm.xyz += vec3(dot(nrm,xxxx), dot(nrm,yyyy), dot(nrm,zzzz)) * weights.x;
     }
     if (weights.y > 0.0) {
-        uv = vec2(skin_info.x + (3.0 * skin_indices.y)/skin_info.z, skin_info.y);
+        uv = vec2(joint_uv.x + (3.0 * skin_indices.y)*joint_pixel_width, joint_uv.y);
         xxxx = textureLod(joint_tex, uv, 0.0);
         yyyy = textureLod(joint_tex, uv + step, 0.0);
         zzzz = textureLod(joint_tex, uv + 2.0 * step, 0.0);
@@ -26,7 +26,7 @@ void skinned_pos_nrm(in vec4 pos, in vec4 nrm, in vec4 skin_weights, in vec4 ski
         skin_nrm.xyz += vec3(dot(nrm,xxxx), dot(nrm,yyyy), dot(nrm,zzzz)) * weights.y;
     }
     if (weights.z > 0.0) {
-        uv = vec2(skin_info.x + (3.0 * skin_indices.z)/skin_info.z, skin_info.y);
+        uv = vec2(joint_uv.x + (3.0 * skin_indices.z)*joint_pixel_width, joint_uv.y);
         xxxx = textureLod(joint_tex, uv, 0.0);
         yyyy = textureLod(joint_tex, uv + step, 0.0);
         zzzz = textureLod(joint_tex, uv + 2.0 * step, 0.0);
@@ -34,7 +34,7 @@ void skinned_pos_nrm(in vec4 pos, in vec4 nrm, in vec4 skin_weights, in vec4 ski
         skin_nrm.xyz += vec3(dot(nrm,xxxx), dot(nrm,yyyy), dot(nrm,zzzz)) * weights.z;
     }
     if (weights.w > 0.0) {
-        uv = vec2(skin_info.x + (3.0 * skin_indices.w)/skin_info.z, skin_info.y);
+        uv = vec2(joint_uv.x + (3.0 * skin_indices.w)*joint_pixel_width, joint_uv.y);
         xxxx = textureLod(joint_tex, uv, 0.0);
         yyyy = textureLod(joint_tex, uv + step, 0.0);
         zzzz = textureLod(joint_tex, uv + 2.0 * step, 0.0);
@@ -47,6 +47,7 @@ void skinned_pos_nrm(in vec4 pos, in vec4 nrm, in vec4 skin_weights, in vec4 ski
 @vs vs
 uniform vs_params {
     mat4 view_proj;
+    float joint_pixel_width;
 };
 
 uniform sampler2D joint_tex;
@@ -58,16 +59,21 @@ in vec4 jweights;
 in vec4 inst_xxxx;
 in vec4 inst_yyyy;
 in vec4 inst_zzzz;
-in vec4 inst_skin_info;
+in vec2 inst_joint_uv;
 
 out vec3 color;
 
 @include_block skin_utils
 
 void main() {
+    // compute skinned model-space position and normal
     vec4 pos, nrm;
-    skinned_pos_nrm(position, normal, jweights, jindices, inst_skin_info, pos, nrm);
-    // FIXME: compute model-view-proj matrix
+    skinned_pos_nrm(position, normal, jweights, jindices, inst_joint_uv, pos, nrm);
+
+    // transform pos and normal to world space
+    pos = vec4(dot(pos,inst_xxxx), dot(pos,inst_yyyy), dot(pos,inst_zzzz), 1.0);
+    nrm = vec4(dot(nrm,inst_xxxx), dot(nrm,inst_yyyy), dot(nrm,inst_zzzz), 0.0);
+
     gl_Position = view_proj * pos;
     color = (nrm.xyz + 1.0) * 0.5;
 }
