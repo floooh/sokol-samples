2fb9e6cc86bca9815d373f3ff8a4ce1b43b355a0
Andre Weissflog <floooh@gmail.com>
2022-01-02 20:14:35 +0100

    finish uniformtypes-sapp.c

--- sapp/uniformtypes-sapp.glsl
+++ sapp/uniformtypes-sapp.glsl
@@ -1,12 +1,57 @@
 @vs vs
-in vec4 position;
-in vec4 color0;
 
+// NOTE: mixing float and int base types in one uniform block is fairly
+// inefficient in the sokol-gfx GL backend, because the uniform
+// block will not be flattened into a single float or int array!
+uniform vs_params {
+    vec2 offset;
+    vec2 scale;
+    int sel;
+    int i1;
+    ivec2 i2;
+    ivec3 i3;
+    ivec4 i4;
+    vec4 pal[10];
+};
+
+in vec2 position;
 out vec4 color;
 
 void main() {
-    gl_Position = position;
-    color = color0;
+    gl_Position = vec4(position * scale + offset, 0.0, 1.0);
+    if (sel == 0) {
+        color = pal[i1];
+    }
+    else if (sel == 1) {
+        color = pal[i2.x];
+    }
+    else if (sel == 2) {
+        color = pal[i2.y];
+    }
+    else if (sel == 3) {
+        color = pal[i3.x];
+    }
+    else if (sel == 4) {
+        color = pal[i3.y];
+    }
+    else if (sel == 5) {
+        color = pal[i3.z];
+    }
+    else if (sel == 6) {
+        color = pal[i4.x];
+    }
+    else if (sel == 7) {
+        color = pal[i4.y];
+    }
+    else if (sel == 8) {
+        color = pal[i4.z];
+    }
+    else if (sel == 9) {
+        color = pal[i4.w];
+    }
+    else {
+        color = vec4(1.0, 1.0, 1.0, 1.0);
+    }
 }
 @end
 
