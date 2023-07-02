f2db3a7b1fff8f415bffa60a15b702fb0b8eddae
Andre Weissflog <floooh@gmail.com>
2023-07-02 16:32:50 +0200

    fix new shadows-depthtex-sapp sample for Metal

--- sapp/shadows-depthtex-sapp.c
+++ sapp/shadows-depthtex-sapp.c
@@ -42,7 +42,6 @@ static void init(void) {
     });
     __dbgui_setup(sapp_sample_count());
 
-    // FIXME: replace with sokol-shape:
     // cube vertex buffer with positions & normals
     const float vertices[] = {
         // pos                  normals
@@ -76,10 +75,10 @@ static void init(void) {
          1.0f,  1.0f,  1.0f,    0.0f, 1.0f, 0.0f,
          1.0f,  1.0f, -1.0f,    0.0f, 1.0f, 0.0f,
 
-        -1.0f,  0.0f, -1.0f,    0.0f, 1.0f, 0.0f,   //PLANE GEOMETRY
-        -1.0f,  0.0f,  1.0f,    0.0f, 1.0f, 0.0f,
-         1.0f,  0.0f,  1.0f,    0.0f, 1.0f, 0.0f,
-         1.0f,  0.0f, -1.0f,    0.0f, 1.0f, 0.0f,
+        -5.0f,  0.0f, -5.0f,    0.0f, 1.0f, 0.0f,   //PLANE GEOMETRY
+        -5.0f,  0.0f,  5.0f,    0.0f, 1.0f, 0.0f,
+         5.0f,  0.0f,  5.0f,    0.0f, 1.0f, 0.0f,
+         5.0f,  0.0f, -5.0f,    0.0f, 1.0f, 0.0f,
     };
     state.vbuf = sg_make_buffer(&(sg_buffer_desc){
         .data = SG_RANGE(vertices),
@@ -112,11 +111,19 @@ static void init(void) {
         },
     };
 
-    // a shadow map render target which will serve as depth buffer
+    // display pass action
+    state.display.pass_action = (sg_pass_action){
+        .colors[0] = {
+            .load_action = SG_LOADACTION_CLEAR,
+            .clear_value = { 0.25f, 0.25f, 0.5f, 1.0f}
+        },
+    };
+
+    // a shadow map render target which will serve as depth buffer in the shadow pass
     state.shadow_map = sg_make_image(&(sg_image_desc){
         .render_target = true,
-        .width = 1024,
-        .height = 1024,
+        .width = 2048,
+        .height = 2048,
         .pixel_format = SG_PIXELFORMAT_DEPTH,
         .sample_count = 1,
         .label = "shadow-map",
@@ -124,11 +131,11 @@ static void init(void) {
 
     // a comparison sampler which is used to sample the shadow map texture in the display pass
     state.shadow_sampler = sg_make_sampler(&(sg_sampler_desc){
-        .min_filter = SG_FILTER_NEAREST,
-        .mag_filter = SG_FILTER_NEAREST,
+        .min_filter = SG_FILTER_LINEAR,
+        .mag_filter = SG_FILTER_LINEAR,
         .wrap_u = SG_WRAP_CLAMP_TO_EDGE,
         .wrap_v = SG_WRAP_CLAMP_TO_EDGE,
-        .compare = SG_COMPAREFUNC_GREATER,
+        .compare = SG_COMPAREFUNC_LESS,
         .label = "shadow-sampler",
     });
 
@@ -149,17 +156,16 @@ static void init(void) {
         },
         .shader = sg_make_shader(shadow_shader_desc(sg_query_backend())),
         .index_type = SG_INDEXTYPE_UINT16,
-        // FIXME!
-        .cull_mode = SG_CULLMODE_NONE,
+        // render back-faces in shadow pass to prevent shadow acne on front-faces
+        .cull_mode = SG_CULLMODE_FRONT,
         .sample_count = 1,
-        // important: 'deactivate' the default color target
-        .colors[0].pixel_format = SG_PIXELFORMAT_NONE,
         .depth = {
             .pixel_format = SG_PIXELFORMAT_DEPTH,
             .compare = SG_COMPAREFUNC_LESS_EQUAL,
             .write_enabled = true,
         },
-        // NOTE: no color targets
+        // important: 'deactivate' the default color target for 'depth-only-rendering'
+        .colors[0].pixel_format = SG_PIXELFORMAT_NONE,
         .label = "shadow-pipeline"
     });
 
@@ -173,8 +179,7 @@ static void init(void) {
         },
         .shader = sg_make_shader(display_shader_desc(sg_query_backend())),
         .index_type = SG_INDEXTYPE_UINT16,
-        // FIXME
-        .cull_mode = SG_CULLMODE_NONE,
+        .cull_mode = SG_CULLMODE_BACK,
         .depth = {
             .compare = SG_COMPAREFUNC_LESS_EQUAL,
             .write_enabled = true,
@@ -203,44 +208,66 @@ static void frame(void) {
     const float t = (float)(sapp_frame_duration() * 60.0);
     state.ry += 0.2f * t;
 
-    // calculate transform matrices for plane and cube
-    const hmm_mat4 scale = HMM_Scale(HMM_Vec3(5.0f, 0.0f, 5.0f));
-    const hmm_mat4 translate = HMM_Translate(HMM_Vec3(0.0f, 1.5f, 0.0f));
+    const hmm_vec3 eye_pos = HMM_Vec3(5.0f, 5.0f, 5.0f);
+    const hmm_mat4 plane_model = HMM_Mat4d(1.0f);
+    const hmm_mat4 cube_model = HMM_Translate(HMM_Vec3(0.0f, 1.5f, 0.0f));
+    const hmm_vec3 plane_color = HMM_Vec3(1.0f, 0.5f, 0.0f);
+    const hmm_vec3 cube_color = HMM_Vec3(0.5f, 1.0f, 0.5f);
 
     // calculate matrices for shadow pass
     const hmm_mat4 rym = HMM_Rotate(state.ry, HMM_Vec3(0.0f, 1.0f, 0.0f));
-    const hmm_vec4 light_dir = HMM_MultiplyMat4ByVec4(rym, HMM_Vec4(50.0f, 50.0f, -50.0f, 0.0f));
-    const hmm_mat4 light_view = HMM_LookAt(light_dir.XYZ, HMM_Vec3(0.0f, 0.0f, 0.0f), HMM_Vec3(0.0f, 1.0f, 0.0f));
-
-    // configure a bias matrix for converting view-space coordinates into uv coordinates
-    hmm_mat4 light_proj = { {
-        { 0.5f, 0.0f, 0.0f, 0 },
-        { 0.0f, 0.5f, 0.0f, 0 },
-        { 0.0f, 0.0f, 0.5f, 0 },
-        { 0.5f, 0.5f, 0.5f, 1 }
-    } };
-    light_proj = HMM_MultiplyMat4(light_proj, HMM_Orthographic(-4.0f, 4.0f, -4.0f, 4.0f, 0, 200.0f));
+    const hmm_vec4 light_pos = HMM_MultiplyMat4ByVec4(rym, HMM_Vec4(50.0f, 50.0f, -50.0f, 1.0f));
+    const hmm_mat4 light_view = HMM_LookAt(light_pos.XYZ, HMM_Vec3(0.0f, 1.5f, 0.0f), HMM_Vec3(0.0f, 1.0f, 0.0f));
+    const hmm_mat4 light_proj = HMM_Orthographic(-5.0f, 5.0f, -5.0f, 5.0f, 0, 100.0f);
     const hmm_mat4 light_view_proj = HMM_MultiplyMat4(light_proj, light_view);
 
-    const vs_shadow_params_t vs_shadow_params = {
-        .mvp = HMM_MultiplyMat4(light_view_proj, translate)
+    const vs_shadow_params_t cube_vs_shadow_params = {
+        .mvp = HMM_MultiplyMat4(light_view_proj, cube_model)
     };
 
     // calculate matrices for display pass
     const hmm_mat4 proj = HMM_Perspective(60.0f, sapp_widthf()/sapp_heightf(), 0.01f, 100.0f);
-    const hmm_mat4 view = HMM_LookAt(HMM_Vec3(5.0f, 5.0f, 5.0f), HMM_Vec3(0.0f, 0.0f, 0.0f), HMM_Vec3(0.0f, 1.0f, 0.0f));
+    const hmm_mat4 view = HMM_LookAt(eye_pos, HMM_Vec3(0.0f, 0.0f, 0.0f), HMM_Vec3(0.0f, 1.0f, 0.0f));
     const hmm_mat4 view_proj = HMM_MultiplyMat4(proj, view);
+
+    const fs_display_params_t fs_display_params = {
+        .light_dir = HMM_NormalizeVec3(light_pos.XYZ),
+        .eye_pos = eye_pos,
+    };
+
+    const vs_display_params_t plane_vs_display_params = {
+        .mvp = HMM_MultiplyMat4(view_proj, plane_model),
+        .model = plane_model,
+        .light_mvp = HMM_MultiplyMat4(light_view_proj, plane_model),
+        .diff_color = plane_color,
+    };
+
+    const vs_display_params_t cube_vs_display_params = {
+        .mvp = HMM_MultiplyMat4(view_proj, cube_model),
+        .model = cube_model,
+        .light_mvp = HMM_MultiplyMat4(light_view_proj, cube_model),
+        .diff_color = cube_color,
+    };
 
     // the shadow map pass, render scene from light source into shadow map texture
     sg_begin_pass(state.shadow.pass, &state.shadow.pass_action);
     sg_apply_pipeline(state.shadow.pip);
     sg_apply_bindings(&state.shadow.bind);
-    sg_apply_uniforms(SG_SHADERSTAGE_VS, SLOT_vs_shadow_params, &SG_RANGE(vs_shadow_params));
+    sg_apply_uniforms(SG_SHADERSTAGE_VS, SLOT_vs_shadow_params, &SG_RANGE(cube_vs_shadow_params));
     sg_draw(0, 36, 1);
     sg_end_pass();
 
     // the display pass, render scene from camera, compare-sample shadow map texture
     sg_begin_default_pass(&state.display.pass_action, sapp_width(), sapp_height());
+    sg_apply_pipeline(state.display.pip);
+    sg_apply_bindings(&state.display.bind);
+    sg_apply_uniforms(SG_SHADERSTAGE_FS, SLOT_fs_display_params, &SG_RANGE(fs_display_params));
+    // render plane
+    sg_apply_uniforms(SG_SHADERSTAGE_VS, SLOT_vs_display_params, &SG_RANGE(plane_vs_display_params));
+    sg_draw(36, 6, 1);
+    // render cube
+    sg_apply_uniforms(SG_SHADERSTAGE_VS, SLOT_vs_shadow_params, &SG_RANGE(cube_vs_display_params));
+    sg_draw(0, 36, 1);
     __dbgui_draw();
     sg_end_pass();
     sg_commit();
--- sapp/shadows-depthtex-sapp.glsl
+++ sapp/shadows-depthtex-sapp.glsl
@@ -25,26 +25,77 @@ void main() { }
 
 uniform vs_display_params {
     mat4 mvp;
+    mat4 model;
+    mat4 light_mvp;
+    vec3 diff_color;
 };
 
 in vec4 pos;
 in vec3 norm;
+out vec3 color;
+out vec4 light_proj_pos;
+out vec4 world_pos;
+out vec3 world_norm;
 
 void main() {
     gl_Position = mvp * pos;
+    light_proj_pos = light_mvp * pos;
+    #if !SOKOL_GLSL
+    light_proj_pos.y = -light_proj_pos.y;
+    #endif
+    world_pos = model * pos;
+    world_norm = normalize((model * vec4(norm, 0.0)).xyz);
+    color = diff_color;
 }
 @end
 
 @fs fs_display
 
+uniform fs_display_params {
+    vec3 light_dir;
+    vec3 eye_pos;
+};
+
 uniform texture2D shadow_map;
 uniform sampler shadow_sampler;
 
+in vec3 color;
+in vec4 light_proj_pos;
+in vec4 world_pos;
+in vec3 world_norm;
+
 out vec4 frag_color;
 
+vec4 gamma(vec4 c) {
+    float p = 1.0 / 2.2;
+    return vec4(pow(c.xyz, vec3(p)), c.w);
+}
+
 void main() {
-    float s = texture(sampler2DShadow(shadow_map, shadow_sampler), vec3(0, 0, 1));
-    frag_color = vec4(s, s, s, 1.0);
+    float spec_power = 2.2;
+    float ambient_intensity = 0.25;
+
+    // diffuse lighting
+    vec3 l = normalize(light_dir);
+    vec3 n = normalize(world_norm);
+    float n_dot_l = dot(n, l);
+    if (n_dot_l > 0.0) {
+
+        vec3 light_pos = light_proj_pos.xyz / light_proj_pos.w;
+        vec3 sm_pos = vec3((light_pos.xy + 1.0) * 0.5, light_pos.z);
+        float s = texture(sampler2DShadow(shadow_map, shadow_sampler), sm_pos);
+        float diff_intensity = max(n_dot_l * s, 0.0);
+
+        vec3 v = normalize(eye_pos - world_pos.xyz);
+        vec3 r = reflect(-l, n);
+        float r_dot_v = max(dot(r, v), 0.0);
+        float spec_intensity = pow(r_dot_v, spec_power) * n_dot_l * s;
+
+        frag_color = vec4(vec3(spec_intensity) + (diff_intensity + ambient_intensity) * color, 1.0);
+    } else {
+        frag_color = vec4(color * ambient_intensity, 1.0);
+    }
+    frag_color = gamma(frag_color);
 }
 @end
 
