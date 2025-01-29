bfecc185838d8dbe98b837da4e699b346cce396e
Andre Weissflog <floooh@gmail.com>
2025-01-29 20:20:40 +0100

    instancingcompute-metal.c generally working

--- metal/instancingcompute-metal.c
+++ metal/instancingcompute-metal.c
@@ -11,14 +11,20 @@
 #include <stdlib.h> // calloc, free
 
 #define MAX_PARTICLES (512 * 1024)
+#define NUM_PARTICLES_EMITTED_PER_FRAME (10)
 
 static struct {
+    uint64_t last_time;
+    int num_particles;
+    float ry;
     struct {
         sg_buffer buf;
         sg_pipeline pip;
     } compute;
-    uint64_t last_time;
     struct {
+        sg_buffer vbuf;
+        sg_buffer ibuf;
+        sg_pipeline pip;
         sg_pass_action pass_action;
     } display;
 } state = {
@@ -31,7 +37,11 @@ static struct {
 
 typedef struct {
     float dt;
-} params_t;
+} cs_params_t;
+
+typedef struct {
+    hmm_mat4 mvp;
+} vs_params_t;
 
 typedef struct {
     float pos[4];
@@ -73,7 +83,7 @@ static void init(void) {
     }
 
     // create compute shader
-    sg_shader shd = sg_make_shader(&(sg_shader_desc){
+    sg_shader compute_shd = sg_make_shader(&(sg_shader_desc){
         .compute_func.source =
             "#include <metal_stdlib>\n"
             "using namespace metal;\n"
@@ -106,40 +116,157 @@ static void init(void) {
             "}\n",
         .uniform_blocks[0] = {
             .stage = SG_SHADERSTAGE_COMPUTE,
-            .size = sizeof(params_t),
+            .size = sizeof(cs_params_t),
             .msl_buffer_n = 0,
         },
-        .storage_buffers = {
-            [0] = {
-                .stage = SG_SHADERSTAGE_COMPUTE,
-                .readonly = false,
-                .msl_buffer_n = 8,
-            },
+        .storage_buffers[0] = {
+            .stage = SG_SHADERSTAGE_COMPUTE,
+            .readonly = false,
+            .msl_buffer_n = 8,
         },
     });
 
     // create a compute pipeline object
     state.compute.pip = sg_make_pipeline(&(sg_pipeline_desc){
         .compute = true,
-        .shader = shd,
+        .shader = compute_shd,
+    });
+
+    // vertex buffer for static geometry, goes into vertex-buffer-slot 0
+    const float r = 0.05f;
+    const float vertices[] = {
+        // positions            colors
+        0.0f,   -r, 0.0f,       1.0f, 0.0f, 0.0f, 1.0f,
+           r, 0.0f, r,          0.0f, 1.0f, 0.0f, 1.0f,
+           r, 0.0f, -r,         0.0f, 0.0f, 1.0f, 1.0f,
+          -r, 0.0f, -r,         1.0f, 1.0f, 0.0f, 1.0f,
+          -r, 0.0f, r,          0.0f, 1.0f, 1.0f, 1.0f,
+        0.0f,    r, 0.0f,       1.0f, 0.0f, 1.0f, 1.0f
+    };
+    state.display.vbuf = sg_make_buffer(&(sg_buffer_desc){
+        .data = SG_RANGE(vertices)
+    });
+
+    // index buffer for static geometry
+    const uint16_t indices[] = {
+        0, 1, 2,    0, 2, 3,    0, 3, 4,    0, 4, 1,
+        5, 1, 2,    5, 2, 3,    5, 3, 4,    5, 4, 1
+    };
+    state.display.ibuf = sg_make_buffer(&(sg_buffer_desc){
+        .type = SG_BUFFERTYPE_INDEXBUFFER,
+        .data = SG_RANGE(indices)
+    });
+
+    // shader for rendering with instance position pulled from the storage buffer
+    sg_shader display_shd = sg_make_shader(&(sg_shader_desc){
+        .vertex_func.source =
+            "#include <metal_stdlib>\n"
+            "using namespace metal;\n"
+            "struct params_t {\n"
+            "  float4x4 mvp;\n"
+            "};\n"
+            "struct vs_in {\n"
+            "  float3 pos [[attribute(0)]];\n"
+            "  float4 color [[attribute(1)]];\n"
+            "};\n"
+            "struct particle_t {\n"
+            "  float4 pos;\n"
+            "  float4 vel;\n"
+            "};\n"
+            "struct ssbo_t {\n"
+            "  particle_t prt[1];\n"
+            "};\n"
+            "struct vs_out {\n"
+            "  float4 pos [[position]];\n"
+            "  float4 color;\n"
+            "};\n"
+            "vertex vs_out _main(\n"
+            "  vs_in in [[stage_in]],\n"
+            "  const device ssbo_t& sbuf [[buffer(8)]],\n"
+            "  constant params_t& params [[buffer(0)]],\n"
+            "  uint inst_idx [[instance_id]])\n"
+            "{\n"
+            "  vs_out out;\n"
+            "  float4 pos = float4(in.pos + sbuf.prt[inst_idx].pos.xyz, 1.0);\n"
+            "  out.pos = params.mvp * pos;\n"
+            "  out.color = in.color;\n"
+            "  return out;\n"
+            "}\n",
+        .fragment_func.source =
+            "#include <metal_stdlib>\n"
+            "using namespace metal;\n"
+            "fragment float4 _main(float4 color [[stage_in]]) {\n"
+            "  return color;\n"
+            "}\n",
+        .uniform_blocks[0] = {
+            .stage = SG_SHADERSTAGE_VERTEX,
+            .size = sizeof(vs_params_t),
+            .msl_buffer_n = 0,
+        },
+        .storage_buffers[0] = {
+            .stage = SG_SHADERSTAGE_VERTEX,
+            .readonly = true,
+            .msl_buffer_n = 8,
+        },
+    });
+
+    // a pipeline object for rendering, this uses geometry from
+    // traditional vertex buffer, but pull per-instance positions
+    // from the compute-update storage buffer
+    state.display.pip = sg_make_pipeline(&(sg_pipeline_desc){
+        .layout = {
+            .attrs = {
+                [0] = { .format=SG_VERTEXFORMAT_FLOAT3, .buffer_index=0 },  // position
+                [1] = { .format=SG_VERTEXFORMAT_FLOAT4, .buffer_index=0 },  // color
+            }
+        },
+        .shader = display_shd,
+        .index_type = SG_INDEXTYPE_UINT16,
+        .depth = {
+            .compare = SG_COMPAREFUNC_LESS_EQUAL,
+            .write_enabled = true,
+        },
+        .cull_mode = SG_CULLMODE_BACK,
     });
 }
 
 static void frame(void) {
-    const params_t params = {
-        .dt = (float)stm_sec(stm_laptime(&state.last_time)),
-    };
+    state.num_particles += NUM_PARTICLES_EMITTED_PER_FRAME;
+    if (state.num_particles > MAX_PARTICLES) {
+        state.num_particles = MAX_PARTICLES;
+    }
 
     // compute pass to update the particle positions
+    const cs_params_t cs_params = {
+        .dt = (float)stm_sec(stm_laptime(&state.last_time)),
+    };
     sg_begin_pass(&(sg_pass){ .compute = true });
     sg_apply_pipeline(state.compute.pip);
-    sg_apply_bindings(&(sg_bindings){ .storage_buffers[0] = state.compute.buf });
-    sg_apply_uniforms(0, &SG_RANGE(params));
-    sg_dispatch(128, 1, 1);
+    sg_apply_bindings(&(sg_bindings){
+        .storage_buffers[0] = state.compute.buf
+    });
+    sg_apply_uniforms(0, &SG_RANGE(cs_params));
+    sg_dispatch(state.num_particles, 1, 1);
     sg_end_pass();
 
-
+    // render pass to render instanced geometry, with the instance-positions
+    // pulled from the compute-updated storage buffer
+    state.ry += 1;
+    const hmm_mat4 proj = HMM_Perspective(60.0f, (float)osx_width()/(float)osx_height(), 0.01f, 50.0f);
+    const hmm_mat4 view = HMM_LookAt(HMM_Vec3(0.0f, 1.5f, 12.0f), HMM_Vec3(0.0f, 0.0f, 0.0f), HMM_Vec3(0.0f, 1.0f, 0.0f));
+    const hmm_mat4 view_proj = HMM_MultiplyMat4(proj, view);
+    const vs_params_t vs_params = {
+        .mvp = HMM_MultiplyMat4(view_proj, HMM_Rotate(state.ry, HMM_Vec3(0.0f, 1.0f, 0.0f)))
+    };
     sg_begin_pass(&(sg_pass){ .action = state.display.pass_action, .swapchain = osx_swapchain() });
+    sg_apply_pipeline(state.display.pip);
+    sg_apply_bindings(&(sg_bindings){
+        .vertex_buffers[0] = state.display.vbuf,
+        .index_buffer = state.display.ibuf,
+        .storage_buffers[0] = state.compute.buf,
+    });
+    sg_apply_uniforms(0, &SG_RANGE(vs_params));
+    sg_draw(0, 24, state.num_particles);
     sg_end_pass();
     sg_commit();
 }
@@ -149,5 +276,5 @@ static void shutdown(void) {
 }
 
 int main() {
-    osx_start(640, 480, 1, SG_PIXELFORMAT_DEPTH, "computeboids-metal", init, frame, shutdown);
+    osx_start(640, 480, 1, SG_PIXELFORMAT_DEPTH, "instancingcompute-metal", init, frame, shutdown);
 }
