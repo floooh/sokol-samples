e9a95ca9b40fb44b86c1c9f09fe8689d46b968e3
Andre Weissflog <floooh@gmail.com>
2025-02-22 15:19:01 +0100

    computeboids sample finished

--- sapp/computeboids-sapp.c
+++ sapp/computeboids-sapp.c
@@ -13,23 +13,41 @@
 #include "sokol_imgui.h"
 #include "computeboids-sapp.glsl.h"
 
-// keep this in sync with the same constant in the GLSL
-#define NUM_PARTICLES (1500)
+#define MAX_PARTICLES (10000)
 
 static struct {
-    sg_buffer particle_buffer[2];
+    sim_params_t sim_params;
     struct {
+        sg_buffer buf[2];
+        sg_pipeline pip;
+    } compute;
+    struct {
+        sg_pipeline pip;
         sg_pass_action pass_action;
     } display;
 } state = {
+    .sim_params = {
+        .dt = 0.04f,
+        .rule1_distance = 0.1f,
+        .rule2_distance = 0.025f,
+        .rule3_distance = 0.025f,
+        .rule1_scale = 0.02f,
+        .rule2_scale = 0.05f,
+        .rule3_scale = 0.005f,
+        .num_particles = 1500,
+    },
     .display = {
         .pass_action = {
-            .colors[0] = { .load_action = SG_LOADACTION_CLEAR, .clear_value = { 0, 0, 0, 1} },
+            .colors[0] = {
+                .load_action = SG_LOADACTION_CLEAR,
+                .clear_value = { 0.0f, 0.15f, 0.3f, 1}
+            },
         }
     }
 };
 
 static void draw_ui(void);
+static void draw_fallback(void);
 
 static inline uint32_t xorshift32(void) {
     static uint32_t x = 0x12345678;
@@ -39,7 +57,7 @@ static inline uint32_t xorshift32(void) {
     return x;
 }
 
-// return a random float value between -1.0f and +1.0
+// return a pseudo-random float between -1.0f and +1.0
 static inline float rnd(void) {
     return (((float)(xorshift32() & 0xFFFF) / (float)0xFFFF) - 0.5f) * 2.0f;
 }
@@ -52,39 +70,89 @@ static void init(void) {
     simgui_setup(&(simgui_desc_t){
         .logger.func = slog_func,
     });
+    igGetStyle()->WindowBorderSize = 0;
+    igGetStyle()->WindowRounding = 8.0f;
     if (!sg_query_features().compute) {
         // compute not supported: early out
         return;
     }
 
+    // two storage buffers with pre-initialized positions and velocities
     {
-        const size_t initial_data_size = NUM_PARTICLES * sizeof(particle_t);
+        const size_t initial_data_size = MAX_PARTICLES * sizeof(particle_t);
         particle_t* initial_data = calloc(1, initial_data_size);
-        for (size_t i = 0; i < NUM_PARTICLES; i++) {
+        for (size_t i = 0; i < MAX_PARTICLES; i++) {
             initial_data[i] = (particle_t){
                 .pos = { rnd(), rnd() },
                 .vel = { rnd() * 0.1f, rnd() * 0.1f },
             };
         }
         for (size_t i = 0; i < 2; i++) {
-            state.particle_buffer[i] = sg_make_buffer(&(sg_buffer_desc){
+            state.compute.buf[i] = sg_make_buffer(&(sg_buffer_desc){
                 .type = SG_BUFFERTYPE_STORAGEBUFFER,
                 .data = {
                     .ptr = initial_data,
                     .size = initial_data_size
                 },
+                .label = (i == 0) ? "particle-buffer-0" : "particle-buffer-1",
             });
         }
         free(initial_data);
     }
+
+    // compute shader and pipeline
+    state.compute.pip = sg_make_pipeline(&(sg_pipeline_desc){
+        .compute = true,
+        .shader = sg_make_shader(compute_shader_desc(sg_query_backend())),
+        .label = "compute-pipeline",
+    });
+
+    // a render pipeline and shader, note that vertices for the boids will be
+    // synthesized by the vertex shader, so there's no separate vertex buffer,
+    // we also don't need any non-default render state since the boids are just 2D triangles
+    state.display.pip = sg_make_pipeline(&(sg_pipeline_desc){
+        .shader = sg_make_shader(display_shader_desc(sg_query_backend())),
+        .label = "render-pipeline",
+    });
 }
 
 static void frame(void) {
     draw_ui();
+
+    // fallback if compute is not supported
+    if (!sg_query_features().compute) {
+        draw_fallback();
+        return;
+    }
+
+    // input- and output- storage-buffers for this frame
+    const sg_buffer in_buf = state.compute.buf[sapp_frame_count() & 1];
+    const sg_buffer out_buf = state.compute.buf[(sapp_frame_count() + 1) & 1];
+
+    // compute pass to update boid positions and velocities, this works with buffer-ping-ponging,
+    // since the compute shader needs random access on the input parameters
+    sg_begin_pass(&(sg_pass){ .compute = true, .label = "compute-pass" });
+    sg_apply_pipeline(state.compute.pip);
+    sg_apply_bindings(&(sg_bindings){
+        .storage_buffers = {
+            [SBUF_cs_ssbo_in] = in_buf,
+            [SBUF_cs_ssbo_out] = out_buf,
+        },
+    });
+    sg_apply_uniforms(UB_sim_params, &SG_RANGE(state.sim_params));
+    sg_dispatch((state.sim_params.num_particles+63)/64, 1, 1);
+    sg_end_pass();
+
+    // render pass for rendering the boids, instanced by the current output storage buffer
     sg_begin_pass(&(sg_pass){
         .action = state.display.pass_action,
         .swapchain = sglue_swapchain(),
     });
+    sg_apply_pipeline(state.display.pip);
+    sg_apply_bindings(&(sg_bindings){
+        .storage_buffers[SBUF_vs_ssbo] = out_buf,
+    });
+    sg_draw(0, 3, state.sim_params.num_particles);
     simgui_render();
     sg_end_pass();
     sg_commit();
@@ -100,6 +168,13 @@ static void input(const sapp_event* ev) {
     simgui_handle_event(ev);
 }
 
+static void draw_fallback(void) {
+    sg_begin_pass(&(sg_pass){ .action = state.display.pass_action, .swapchain = sglue_swapchain() });
+    simgui_render();
+    sg_end_pass();
+    sg_commit();
+}
+
 static void draw_ui(void) {
     simgui_new_frame(&(simgui_frame_desc_t){
         .width = sapp_width(),
@@ -107,18 +182,25 @@ static void draw_ui(void) {
         .delta_time = sapp_frame_duration(),
         .dpi_scale = sapp_dpi_scale(),
     });
-    igSetNextWindowPos((ImVec2){20,20}, ImGuiCond_Once);
+    igSetNextWindowBgAlpha(0.8f);
+    igSetNextWindowPos((ImVec2){10,10}, ImGuiCond_Once);
     const ImGuiWindowFlags flags =
         ImGuiWindowFlags_NoDecoration |
         ImGuiWindowFlags_AlwaysAutoResize |
-        ImGuiWindowFlags_NoBackground |
         ImGuiWindowFlags_NoBringToFrontOnFocus |
         ImGuiWindowFlags_NoFocusOnAppearing;
     if (igBegin("controls", 0, flags)) {
         if (!sg_query_features().compute) {
-            igText("Compute not supported!");
+            igText("Compute shaders not supported!");
         } else {
-            igText("F I X M E !!!");
+            igSliderFloat("Delta T", &state.sim_params.dt, 0.01, 0.1);
+            igSliderFloat("Rule1 Distance", &state.sim_params.rule1_distance, 0.0f, 0.2f);
+            igSliderFloat("Rule2 Distance", &state.sim_params.rule2_distance, 0.0f, 0.1f);
+            igSliderFloat("Rule3 Distance", &state.sim_params.rule3_distance, 0.0f, 0.1f);
+            igSliderFloat("Rule1 Scale", &state.sim_params.rule1_scale, 0.0f, 0.1f);
+            igSliderFloat("Rule2 Scale", &state.sim_params.rule2_scale, 0.0f, 0.1f);
+            igSliderFloat("Rule3 Scale", &state.sim_params.rule3_scale, 0.0f, 0.1f);
+            igSliderInt("Num Boids", &state.sim_params.num_particles, 0, MAX_PARTICLES);
         }
     }
     igEnd();
