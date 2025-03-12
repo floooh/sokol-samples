ae4d7257bbd1efa3423adab6f0374c0b4dc6a01a
Andre Weissflog <floooh@gmail.com>
2025-03-12 18:18:21 +0100

    ozz-skin-sapp: use UBYTE4 for joint indices, use texelFetch to load joint matrices

--- sapp/ozz-skin-sapp.glsl
+++ sapp/ozz-skin-sapp.glsl
@@ -2,42 +2,41 @@
 @ctype vec4 hmm_vec4
 
 @block skin_utils
-void skinned_pos_nrm(in vec4 pos, in vec4 nrm, in vec4 skin_weights, in vec4 skin_indices, in vec2 joint_uv, out vec4 skin_pos, out vec4 skin_nrm) {
+void skinned_pos_nrm(in vec4 pos, in vec4 nrm, in vec4 skin_weights, in uvec4 skin_indices, out vec4 skin_pos, out vec4 skin_nrm) {
     skin_pos = vec4(0.0, 0.0, 0.0, 1.0);
     skin_nrm = vec4(0.0, 0.0, 0.0, 0.0);
     vec4 weights = skin_weights / dot(skin_weights, vec4(1.0));
-    vec2 step = vec2(joint_pixel_width, 0.0);
-    vec2 uv;
+    ivec2 uv;
     vec4 xxxx, yyyy, zzzz;
     if (weights.x > 0.0) {
-        uv = vec2(joint_uv.x + (3.0 * skin_indices.x)*joint_pixel_width, joint_uv.y);
-        xxxx = textureLod(sampler2D(joint_tex, smp), uv, 0.0);
-        yyyy = textureLod(sampler2D(joint_tex, smp), uv + step, 0.0);
-        zzzz = textureLod(sampler2D(joint_tex, smp), uv + 2.0 * step, 0.0);
+        uv = ivec2(3 * skin_indices.x, gl_InstanceIndex);
+        xxxx = texelFetch(sampler2D(joint_tex, smp), uv, 0);
+        yyyy = texelFetch(sampler2D(joint_tex, smp), uv + ivec2(1,0), 0);
+        zzzz = texelFetch(sampler2D(joint_tex, smp), uv + ivec2(2,0), 0);
         skin_pos.xyz += vec3(dot(pos,xxxx), dot(pos,yyyy), dot(pos,zzzz)) * weights.x;
         skin_nrm.xyz += vec3(dot(nrm,xxxx), dot(nrm,yyyy), dot(nrm,zzzz)) * weights.x;
     }
     if (weights.y > 0.0) {
-        uv = vec2(joint_uv.x + (3.0 * skin_indices.y)*joint_pixel_width, joint_uv.y);
-        xxxx = textureLod(sampler2D(joint_tex, smp), uv, 0.0);
-        yyyy = textureLod(sampler2D(joint_tex, smp), uv + step, 0.0);
-        zzzz = textureLod(sampler2D(joint_tex, smp), uv + 2.0 * step, 0.0);
+        uv = ivec2(3 * skin_indices.y, gl_InstanceIndex);
+        xxxx = texelFetch(sampler2D(joint_tex, smp), uv, 0);
+        yyyy = texelFetch(sampler2D(joint_tex, smp), uv + ivec2(1,0), 0);
+        zzzz = texelFetch(sampler2D(joint_tex, smp), uv + ivec2(2,0), 0);
         skin_pos.xyz += vec3(dot(pos,xxxx), dot(pos,yyyy), dot(pos,zzzz)) * weights.y;
         skin_nrm.xyz += vec3(dot(nrm,xxxx), dot(nrm,yyyy), dot(nrm,zzzz)) * weights.y;
     }
     if (weights.z > 0.0) {
-        uv = vec2(joint_uv.x + (3.0 * skin_indices.z)*joint_pixel_width, joint_uv.y);
-        xxxx = textureLod(sampler2D(joint_tex, smp), uv, 0.0);
-        yyyy = textureLod(sampler2D(joint_tex, smp), uv + step, 0.0);
-        zzzz = textureLod(sampler2D(joint_tex, smp), uv + 2.0 * step, 0.0);
+        uv = ivec2(3 * skin_indices.z, gl_InstanceIndex);
+        xxxx = texelFetch(sampler2D(joint_tex, smp), uv, 0);
+        yyyy = texelFetch(sampler2D(joint_tex, smp), uv + ivec2(1,0), 0);
+        zzzz = texelFetch(sampler2D(joint_tex, smp), uv + ivec2(2,0), 0);
         skin_pos.xyz += vec3(dot(pos,xxxx), dot(pos,yyyy), dot(pos,zzzz)) * weights.z;
         skin_nrm.xyz += vec3(dot(nrm,xxxx), dot(nrm,yyyy), dot(nrm,zzzz)) * weights.z;
     }
     if (weights.w > 0.0) {
-        uv = vec2(joint_uv.x + (3.0 * skin_indices.w)*joint_pixel_width, joint_uv.y);
-        xxxx = textureLod(sampler2D(joint_tex, smp), uv, 0.0);
-        yyyy = textureLod(sampler2D(joint_tex, smp), uv + step, 0.0);
-        zzzz = textureLod(sampler2D(joint_tex, smp), uv + 2.0 * step, 0.0);
+        uv = ivec2(3 * skin_indices.w, gl_InstanceIndex);
+        xxxx = texelFetch(sampler2D(joint_tex, smp), uv, 0);
+        yyyy = texelFetch(sampler2D(joint_tex, smp), uv + ivec2(1,0), 0);
+        zzzz = texelFetch(sampler2D(joint_tex, smp), uv + ivec2(2,0), 0);
         skin_pos.xyz += vec3(dot(pos,xxxx), dot(pos,yyyy), dot(pos,zzzz)) * weights.w;
         skin_nrm.xyz += vec3(dot(nrm,xxxx), dot(nrm,yyyy), dot(nrm,zzzz)) * weights.w;
     }
@@ -47,7 +46,6 @@ void skinned_pos_nrm(in vec4 pos, in vec4 nrm, in vec4 skin_weights, in vec4 ski
 @vs vs
 layout(binding=0) uniform vs_params {
     mat4 view_proj;
-    float joint_pixel_width;
 };
 
 @image_sample_type joint_tex unfilterable_float
@@ -57,12 +55,11 @@ layout(binding=0) uniform sampler smp;
 
 in vec4 position;
 in vec4 normal;
-in vec4 jindices;
+in uvec4 jindices;
 in vec4 jweights;
 in vec4 inst_xxxx;
 in vec4 inst_yyyy;
 in vec4 inst_zzzz;
-in vec2 inst_joint_uv;
 
 out vec3 color;
 
@@ -71,7 +68,7 @@ out vec3 color;
 void main() {
     // compute skinned model-space position and normal
     vec4 pos, nrm;
-    skinned_pos_nrm(position, normal, jweights, jindices * 255.0, inst_joint_uv, pos, nrm);
+    skinned_pos_nrm(position, normal, jweights, jindices, pos, nrm);
 
     // transform pos and normal to world space
     pos = vec4(dot(pos,inst_xxxx), dot(pos,inst_yyyy), dot(pos,inst_zzzz), 1.0);
