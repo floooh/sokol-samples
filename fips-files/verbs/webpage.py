a44ad083c22360990ed9b4f2fc6674c15385b0ac
Andre Weissflog <floooh@gmail.com>
2025-08-23 18:57:22 +0200

    remove sbufoffset-sapp sample from WebGL2 webpage (needs compute shaders)

--- fips-files/verbs/webpage.py
+++ fips-files/verbs/webpage.py
@@ -53,7 +53,6 @@ samples = [
     [ 'vertexindexbuffer', 'vertexindexbuffer-sapp.c', 'vertexindexbuffer-sapp.glsl'],
     [ 'vertextexture', 'vertextexture-sapp.c', 'vertextexture-sapp.glsl'],
     [ 'sbuftex', 'sbuftex-sapp.c', 'sbuftex-sapp.glsl' ],
-    [ 'sbufoffset', 'sbufoffset-sapp.c', 'sbufoffset-sapp.glsl'],
     [ 'shapes', 'shapes-sapp.c', 'shapes-sapp.glsl'],
     [ 'shapes-transform', 'shapes-transform-sapp.c', 'shapes-transform-sapp.glsl'],
     [ 'offscreen', 'offscreen-sapp.c', 'offscreen-sapp.glsl' ],
@@ -127,6 +126,7 @@ samples = [
 ]
 
 compute_samples = [
+    [ 'sbufoffset', 'sbufoffset-sapp.c', 'sbufoffset-sapp.glsl'],
     [ 'instancing-compute', 'instancing-compute-sapp.c', 'instancing-compute-sapp.glsl' ],
     [ 'computeboids', 'computeboids-sapp.c', 'computeboids-sapp.glsl' ],
     [ 'write-storageimage', 'write-storageimage-sapp.c', 'write-storageimage-sapp.glsl' ],
