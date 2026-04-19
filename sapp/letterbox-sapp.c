72b1763c107146dea6ac90ecb34adf01e0d3fb97
Andre Weissflog <floooh@gmail.com>
2026-04-19 17:50:27 +0200

    new sample letterbox-sapp wip

---
+++ sapp/letterbox-sapp.c
@@ -0,0 +1,86 @@
+//------------------------------------------------------------------------------
+//  letterbox-sapp.c
+//
+//  Demonstrate/test sokol_letterbox.h
+//------------------------------------------------------------------------------
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+#include "sokol_log.h"
+#include "sokol_glue.h"
+#define SOKOL_LETTERBOX_IMPL
+#include "sokol_letterbox.h"
+#include "cimgui.h"
+#define SOKOL_IMGUI_IMPL
+#include "sokol_imgui.h"
+
+static void drawui(void);
+
+static struct {
+    sg_pass_action pass_action;
+} state;
+
+static void init(void) {
+    sg_setup(&(sg_desc){
+        .environment = sglue_environment(),
+        .logger.func = slog_func,
+    });
+    simgui_setup(&(simgui_desc_t){
+        .logger.func = slog_func,
+    });
+
+    state.pass_action = (sg_pass_action){
+        .colors[0] = {
+            .load_action = SG_LOADACTION_CLEAR,
+            .clear_value = { 0, 0, 0, 1 },
+        },
+    };
+}
+
+static void frame(void) {
+    drawui();
+
+    sg_begin_pass(&(sg_pass){ .action = state.pass_action, .swapchain = sglue_swapchain() });
+    simgui_render();
+    sg_end_pass();
+    sg_commit();
+}
+
+static void cleanup(void) {
+    simgui_shutdown();
+    sg_shutdown();
+}
+
+static void input(const sapp_event* ev) {
+    simgui_handle_event(ev);
+}
+
+static void drawui(void) {
+    simgui_new_frame(&(simgui_frame_desc_t){
+        .width = sapp_width(),
+        .height = sapp_height(),
+        .delta_time = sapp_frame_duration(),
+        .dpi_scale = sapp_dpi_scale(),
+    });
+    igSetNextWindowPos((ImVec2){ 30, 50 }, ImGuiCond_Once);
+    igSetNextWindowBgAlpha(0.75f);
+    if (igBegin("Controls", 0, ImGuiWindowFlags_NoDecoration|ImGuiWindowFlags_AlwaysAutoResize)) {
+        igText("FIXME!");
+    }
+    igEnd();
+}
+
+sapp_desc sokol_main(int argc, char* argv[]) {
+    (void)argc;
+    (void)argv;
+    return (sapp_desc){
+        .init_cb = init,
+        .frame_cb = frame,
+        .cleanup_cb = cleanup,
+        .event_cb = input,
+        .width = 800,
+        .height = 600,
+        .window_title = "letterbox-sapp.c",
+        .icon.sokol_default = true,
+        .logger.func = slog_func,
+    };
+}
